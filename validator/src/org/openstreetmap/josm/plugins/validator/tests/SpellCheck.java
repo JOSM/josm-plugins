9912345f67d97006e7253530e80cf74dc5a69b5e
frsantos <frsantos@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-05-06 17:50:55 +0000

    Read spellcheck data from user file
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@2790 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- validator/src/org/openstreetmap/josm/plugins/validator/tests/SpellCheck.java
+++ validator/src/org/openstreetmap/josm/plugins/validator/tests/SpellCheck.java
@@ -2,13 +2,15 @@ package org.openstreetmap.josm.plugins.validator.tests;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
 import java.util.Map.Entry;
 
-import javax.swing.JCheckBox;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.command.*;
@@ -18,6 +20,7 @@ import org.openstreetmap.josm.gui.annotation.AnnotationPreset.*;
 import org.openstreetmap.josm.gui.preferences.AnnotationPresetPreference;
 import org.openstreetmap.josm.plugins.validator.*;
 import org.openstreetmap.josm.plugins.validator.util.Bag;
+import org.openstreetmap.josm.plugins.validator.util.Util;
 import org.openstreetmap.josm.tools.GBC;
 import org.openstreetmap.josm.tools.XmlObjectParser;
 import org.xml.sax.SAXException;
@@ -29,20 +32,47 @@ import org.xml.sax.SAXException;
  */
 public class SpellCheck extends Test 
 {
-	/** The spell check key substitutions: the key should be substituted by the value */
+	/** The default spellcheck data file */
+    public static final String SPELLCHECK_DATA_FILE = "http://svn.openstreetmap.org/applications/utils/planet.osm/java/speller/words.cfg";
+
+    /** The spell check key substitutions: the key should be substituted by the value */
 	protected static Map<String, String> spellCheckKeyData;
 
 	/** The spell check preset values */
 	protected static Bag<String, String> spellCheckValueData;
 	
-	/** Preference name for checking values */
-	public static final String PREF_CHECK_VALUES = "tests." + SpellCheck.class.getSimpleName() + ".checkValues";
+    /** Preference name for checking values */
+    public static final String PREF_CHECK_VALUES = "tests." + SpellCheck.class.getSimpleName() + ".checkValues";
+    /** Preference name for checking values */
+    public static final String PREF_CHECK_KEYS = "tests." + SpellCheck.class.getSimpleName() + ".checkKeys";
+    /** Preference name for sources */
+    public static final String PREF_SOURCES = "tests." + SpellCheck.class.getSimpleName() + ".sources";
+    /** Preference name for global upload check */
+    public static final String PREF_CHECK_BEFORE_UPLOAD = "tests." + SpellCheck.class.getSimpleName() + ".checkBeforeUpload";
+    /** Preference name for keys upload check */
+    public static final String PREF_CHECK_KEYS_BEFORE_UPLOAD = "tests." + SpellCheck.class.getSimpleName() + ".checkKeysBeforeUpload";
+    /** Preference name for values upload check */
+    public static final String PREF_CHECK_VALUES_BEFORE_UPLOAD = "tests." + SpellCheck.class.getSimpleName() + ".checkValuesBeforeUpload";
 	
-	/** Whether to check values too */
-	protected boolean checkValues = false;
+    /** Whether to check keys */
+    protected boolean checkKeys = false;
+    /** Whether to check values */
+    protected boolean checkValues = false;
 
-	/** Preferences checkbox */
-	protected JCheckBox prefCheckValues;
+    /** Preferences checkbox for keys */
+    protected JCheckBox prefCheckKeys;
+    /** Preferences checkbox for values */
+    protected JCheckBox prefCheckValues;
+    /** The preferences checkbox for validation of keys on upload */
+    protected JCheckBox prefCheckKeysBeforeUpload;
+    /** The preferences checkbox for validation of values on upload */
+    protected JCheckBox prefCheckValuesBeforeUpload;
+    /** The add button */
+    protected JButton addSrcButton;
+    /** The edit button */
+    protected JButton editSrcButton;
+    /** The delete button */
+    protected JButton deleteSrcButton;
 
 	/** Empty values error */
 	protected static int EMPTY_VALUES 	= 0;
@@ -51,12 +81,20 @@ public class SpellCheck extends Test
 	/** Invalid value error */
 	protected static int INVALID_VALUE 	= 2;
 	
+    /** List of sources for spellcheck data */
+    protected JList spellcheckSources;
+
+    /** Whether this test must check the keys before upload. Used by peferences */
+    protected boolean testKeysBeforeUpload;
+    /** Whether this test must check the values before upload. Used by peferences */
+    protected boolean testValuesBeforeUpload;
+    
 	/**
 	 * Constructor
 	 */
 	public SpellCheck() 
 	{
-		super(tr("Spell checker."),
+		super(tr("Properties spell checker."),
 			  tr("This plugin checks misspelled property keys and values."));
 	}
 
@@ -79,31 +117,42 @@ public class SpellCheck extends Test
 	 */
 	private static void initializeSpellCheck(OSMValidatorPlugin plugin) throws FileNotFoundException, IOException 
 	{
-		plugin.copy("/resources/spellCheck.data", "spellCheck.data");
-		BufferedReader reader = new BufferedReader( new FileReader(plugin.getPluginDir() + "/spellCheck.data") );
-		
 		spellCheckKeyData = new HashMap<String, String>();
-		String okValue = null;
-		do
-		{
-			String line = reader.readLine();
-			if( line == null || line.length() == 0 )
-				break;
+        String sources = Main.pref.get( PREF_SOURCES );
+        if( sources == null || sources.length() == 0)
+            sources = SPELLCHECK_DATA_FILE;
+        
+        StringTokenizer st = new StringTokenizer(sources, ";");
+        while (st.hasMoreTokens())
+        {
+            String source = st.nextToken();
+            File sourceFile = Util.mirror(new URL(source), plugin.getPluginDir());
+            BufferedReader reader = new BufferedReader(new FileReader(sourceFile));
 
-			if( line.charAt(0) == '+' )
-			{
-				okValue = line.substring(1);
-			}
-			else if( line.charAt(0) == '-' && okValue != null )
-			{
-				spellCheckKeyData.put(line.substring(1), okValue);
-			}
-			else
-			{
-				System.err.println("Invalid spellcheck line:" + line);
-			}
-		}
-		while( true );
+    		String okValue = null;
+    		do
+    		{
+    			String line = reader.readLine();
+                if( line == null || line.length() == 0 )
+                    break;
+                if( line.startsWith("#") )
+                    continue;
+    
+    			if( line.charAt(0) == '+' )
+    			{
+    				okValue = line.substring(1);
+    			}
+    			else if( line.charAt(0) == '-' && okValue != null )
+    			{
+    				spellCheckKeyData.put(line.substring(1), okValue);
+    			}
+    			else
+    			{
+    				System.err.println("Invalid spellcheck line:" + line);
+    			}
+    		}
+    		while( true );
+        }
 	}
 	
 	/**
@@ -126,8 +175,6 @@ public class SpellCheck extends Test
 		
 		spellCheckValueData = new Bag<String, String>();
 		readPresetFromPreferences();
-		
-		// TODO: allow per user word definitions
 	}
 	
 	
@@ -165,12 +212,12 @@ public class SpellCheck extends Test
 		{
 			String key = prop.getKey();
 			String value = prop.getValue();
-			if( (value==null || value.trim().length() == 0) && !withErrors.contains(p, "EV"))
+			if( checkValues && (value==null || value.trim().length() == 0) && !withErrors.contains(p, "EV"))
 			{
 				errors.add( new TestError(this, Severity.WARNING, tr("Tags with empty values"), p, EMPTY_VALUES) );
 				withErrors.add(p, "EV");
 			}
-			if( spellCheckKeyData.containsKey(key) && !withErrors.contains(p, "IPK"))
+			if( checkKeys && spellCheckKeyData.containsKey(key) && !withErrors.contains(p, "IPK"))
 			{
 				errors.add( new TestError(this, Severity.WARNING, tr("Invalid property keys"), p, INVALID_KEY) );
 				withErrors.add(p, "IPK");
@@ -262,24 +309,148 @@ public class SpellCheck extends Test
 	@Override
 	public void startTest() 
 	{
-		checkValues = Main.pref.getBoolean(PREF_CHECK_VALUES);
+        checkKeys = Main.pref.getBoolean(PREF_CHECK_KEYS);
+        if( isBeforeUpload )
+            checkKeys = checkKeys && Main.pref.getBoolean(PREF_CHECK_KEYS_BEFORE_UPLOAD);
+
+        checkValues = Main.pref.getBoolean(PREF_CHECK_VALUES);
+        if( isBeforeUpload )
+            checkValues = checkValues && Main.pref.getBoolean(PREF_CHECK_VALUES_BEFORE_UPLOAD);
 	}
 
+    @Override
+    public void visit(Collection<OsmPrimitive> selection) 
+    {
+        if( checkKeys || checkValues)
+            super.visit(selection);
+    }
+    
 	@Override
 	public void addGui(JPanel testPanel)
 	{
-		boolean checkValues = Main.pref.getBoolean(PREF_CHECK_VALUES);
-		
-		String text = tr("Check also property values from presets");
-		prefCheckValues = new JCheckBox(text, checkValues);
-		prefCheckValues.setToolTipText(text);
-		testPanel.add(prefCheckValues, GBC.eop().insets(40,0,0,0));
+        testPanel.add( new JLabel(), GBC.eol());
+        
+        boolean checkKeys = Main.pref.getBoolean(PREF_CHECK_KEYS);
+        prefCheckKeys = new JCheckBox(tr("Check property keys."), checkKeys);
+        prefCheckKeys .setToolTipText(tr("Validate that property keys are valid checking against list of words."));
+        testPanel.add(prefCheckKeys, GBC.std().insets(40,0,0,0));
+
+        prefCheckKeysBeforeUpload = new JCheckBox();
+        prefCheckKeysBeforeUpload.setSelected(Main.pref.getBoolean(PREF_CHECK_KEYS_BEFORE_UPLOAD));
+        testPanel.add(prefCheckKeysBeforeUpload, GBC.eop().insets(20,0,0,0));
+        
+        spellcheckSources = new JList(new DefaultListModel());
+        if( !Main.pref.hasKey(PREF_SOURCES))
+            Main.pref.put(PREF_SOURCES, SPELLCHECK_DATA_FILE);
+        
+        String sources = Main.pref.get( PREF_SOURCES );
+        StringTokenizer st = new StringTokenizer(sources, ";");
+        while (st.hasMoreTokens())
+            ((DefaultListModel)spellcheckSources.getModel()).addElement(st.nextToken());
+        
+        addSrcButton = new JButton(tr("Add"));
+        addSrcButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                String source = JOptionPane.showInputDialog(Main.parent, tr("Spellcheck source"));
+                if (source == null)
+                    return;
+                ((DefaultListModel)spellcheckSources.getModel()).addElement(source);
+            }
+        });
+
+        editSrcButton = new JButton(tr("Edit"));
+        editSrcButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                if (spellcheckSources.getSelectedIndex() == -1)
+                    JOptionPane.showMessageDialog(Main.parent, tr("Please select the row to edit."));
+                else {
+                    String source = JOptionPane.showInputDialog(Main.parent, tr("Spellcheck source"), spellcheckSources.getSelectedValue());
+                    if (source == null)
+                        return;
+                    ((DefaultListModel)spellcheckSources.getModel()).setElementAt(source, spellcheckSources.getSelectedIndex());
+                }
+            }
+        });
+
+        deleteSrcButton = new JButton(tr("Delete"));
+        deleteSrcButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                if (spellcheckSources.getSelectedIndex() == -1)
+                    JOptionPane.showMessageDialog(Main.parent, tr("Please select the row to delete."));
+                else {
+                    ((DefaultListModel)spellcheckSources.getModel()).remove(spellcheckSources.getSelectedIndex());
+                }
+            }
+        });
+        spellcheckSources.setVisibleRowCount(3);
+
+        spellcheckSources.setToolTipText(tr("The sources (url or filename) of spell check data files. See http://wiki.openstreetmap.org/index.php/User:JLS/speller for help."));
+        addSrcButton.setToolTipText(tr("Add a new spellcheck source to the list."));
+        editSrcButton.setToolTipText(tr("Edit the selected source."));
+        deleteSrcButton.setToolTipText(tr("Delete the selected source from the list."));
+
+        testPanel.add(new JLabel(tr("Spellcheck data sources")), GBC.eol().insets(40,0,0,0));
+        testPanel.add(new JScrollPane(spellcheckSources), GBC.eol().insets(40,0,0,0));
+        final JPanel buttonPanel = new JPanel(new GridBagLayout());
+        testPanel.add(buttonPanel, GBC.eol().fill(GBC.HORIZONTAL));
+        buttonPanel.add(addSrcButton, GBC.std().insets(0,5,0,0));
+        buttonPanel.add(editSrcButton, GBC.std().insets(5,5,5,0));
+        buttonPanel.add(deleteSrcButton, GBC.std().insets(0,5,0,0));
+        
+        prefCheckKeys.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                boolean selected = prefCheckKeys.isSelected();
+                spellcheckSources.setEnabled( selected );
+                addSrcButton.setEnabled(selected);
+                editSrcButton.setEnabled(selected);
+                deleteSrcButton.setEnabled(selected);
+            }
+        });
+        
+        spellcheckSources.setEnabled( checkKeys );
+        buttonPanel.setEnabled( checkKeys );
+        
+        boolean checkValues = Main.pref.getBoolean(PREF_CHECK_VALUES);
+        prefCheckValues = new JCheckBox(tr("Check property values."), checkValues);
+        prefCheckValues .setToolTipText(tr("Validate that property values are valid checking against presets."));
+		testPanel.add(prefCheckValues, GBC.std().insets(40,0,0,0));
+
+        prefCheckValuesBeforeUpload = new JCheckBox();
+        prefCheckValuesBeforeUpload.setSelected(Main.pref.getBoolean(PREF_CHECK_VALUES_BEFORE_UPLOAD));
+        testPanel.add(prefCheckValuesBeforeUpload, GBC.eop().insets(20,0,0,0));
+        
 	}
 
+    public void setGuiEnabled(boolean enabled)
+    {
+        prefCheckKeys.setEnabled(enabled);
+        prefCheckKeysBeforeUpload.setEnabled(enabled);
+        spellcheckSources.setEnabled( enabled );
+        addSrcButton.setEnabled(enabled);
+        editSrcButton.setEnabled(enabled);
+        deleteSrcButton.setEnabled(enabled);
+        prefCheckValues.setEnabled(enabled);
+        prefCheckValuesBeforeUpload.setEnabled(enabled);
+    } 
+    
 	@Override
 	public void ok() 
 	{
-		Main.pref.put(PREF_CHECK_VALUES, prefCheckValues.isSelected());
+        Main.pref.put(PREF_CHECK_VALUES, prefCheckValues.isSelected());
+        Main.pref.put(PREF_CHECK_KEYS, prefCheckKeys.isSelected());
+        Main.pref.put(PREF_CHECK_VALUES_BEFORE_UPLOAD, prefCheckValuesBeforeUpload.isSelected());
+        Main.pref.put(PREF_CHECK_KEYS_BEFORE_UPLOAD, prefCheckKeysBeforeUpload.isSelected());
+        Main.pref.put(PREF_CHECK_BEFORE_UPLOAD, prefCheckKeysBeforeUpload.isSelected() || prefCheckValuesBeforeUpload.isSelected());            
+        String sources = "";
+        if( spellcheckSources.getModel().getSize() > 0 )
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < spellcheckSources.getModel().getSize(); ++i)
+                sb.append(";"+spellcheckSources.getModel().getElementAt(i));
+            sources = sb.substring(1);
+        }
+        Main.pref.put(PREF_SOURCES, sources );
+
 	}
 	
 	@Override
