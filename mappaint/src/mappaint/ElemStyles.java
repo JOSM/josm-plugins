39da4148ca3c8f93c8bf2e6c7e731bd10f41e0d9
ulf <ulf@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-07-19 00:03:17 +0000

    - add an experimental way to show unknown key/value combinations (currently commented out)
    - don't add duplicate rules into the internal hashtable
    
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@3619 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- mappaint/src/mappaint/ElemStyles.java
+++ mappaint/src/mappaint/ElemStyles.java
@@ -7,6 +7,8 @@ import org.openstreetmap.josm.data.osm.OsmPrimitive;
 public class ElemStyles
 {
 	HashMap<String, ElemStyle> styles;
+	static int nr = 0;
+
 
 	public ElemStyles()
 	{
@@ -15,6 +17,7 @@ public class ElemStyles
 
 	public void add (String k, String v, ElemStyle style)
 	{
+		ElemStyle  old_style;
 		String key = k + "=" + v;
 		
 		/* unfortunately, there don't seem to be an efficient way to */
@@ -29,7 +32,18 @@ public class ElemStyles
 		else if(style instanceof IconElemStyle) {
 			key = key + "node";
 		}
-		styles.put(key, style);
+		/* avoid duplicates - for now */
+		old_style = styles.get(key);
+		if(old_style == null) {
+			/* new key/value, insert */
+			styles.put(key, style);
+		} else {
+			if(style.getMaxScale() < old_style.getMaxScale()) {
+				/* existing larger scale key/value, replace */
+				styles.remove(old_style);
+				styles.put(key, style);
+			}
+		}
 	}
 
 	public ElemStyle getStyle (OsmPrimitive p)
@@ -37,6 +51,7 @@ public class ElemStyles
 		if(p.keys!=null)
 		{
 			String classname;
+			String kv = null;
 			
 			if(p instanceof org.openstreetmap.josm.data.osm.Node) {
 				classname = "node";
@@ -47,13 +62,48 @@ public class ElemStyles
 			while(iterator.hasNext())	
 			{
 				String key = iterator.next();
-				String kv = key + "=" + p.keys.get(key) + classname;
+				kv = key + "=" + p.keys.get(key) + classname;
 				if(styles.containsKey(kv))
 				{
 					return styles.get(kv);
 				}
 			}
+
+            // not a known key/value combination
+			boolean first_line = true;
+
+            // filter out trivial tags and show the rest
+			iterator = p.keys.keySet().iterator();
+			while(iterator.hasNext())	
+			{
+				String key = iterator.next();
+				kv = key + "=" + p.keys.get(key);
+				if(	!kv.startsWith("created_by=") &&
+					!kv.startsWith("converted_by=") &&
+					!kv.startsWith("source=") &&
+					!kv.startsWith("note=") &&
+					!kv.startsWith("layer=") &&
+					!kv.startsWith("bridge=") &&
+					!kv.startsWith("tunnel=") &&
+					!kv.startsWith("oneway=") &&
+					!kv.startsWith("speedlimit=") &&
+					!kv.startsWith("motorcar=") &&
+					!kv.startsWith("horse=") &&
+					!kv.startsWith("bicycle=") &&
+					!kv.startsWith("foot=")
+					) {
+						
+					if (first_line) {
+						nr++;
+						//System.out.println("mappaint - rule not found[" + nr + "]: " + kv + " id:" + p.id);
+					} else {
+						//System.out.println("mappaint - rule not found[" + nr + "]: " + kv);
+					}
+					first_line=false;
+				}
+			}
 		}
+		
 		return null;
 	}
 
