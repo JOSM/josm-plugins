635b4ef444f04787994064ec60fde7e71b04e98e
christofd <christofd@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-07-28 23:02:05 +0000

    add description to route
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@3830 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- navigator/src/at/dallermassl/josm/plugin/navigator/NavigatorModel.java
+++ navigator/src/at/dallermassl/josm/plugin/navigator/NavigatorModel.java
@@ -16,6 +16,7 @@ import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Segment;
+import org.openstreetmap.josm.data.osm.Way;
 
 /**
  * @author cdaller
@@ -26,6 +27,7 @@ public class NavigatorModel {
     private List<Node> nodes;
     private int selectionChangedCalls;
     List<Segment> segmentPath;
+    List<SegmentEdge> edgePath;
 
     public NavigatorModel() {
         nodes = new ArrayList<Node>();
@@ -73,21 +75,66 @@ public class NavigatorModel {
             fullPath.addAll(path);
             fullWeight += routing.getPathLength();
         }
-        segmentPath = new LinkedList<Segment>();
-        synchronized(segmentPath) {
-            for (SegmentEdge edge : fullPath) {
-                segmentPath.add(edge.getSegment());
-            }
-        }
-        Main.ds.setSelected(segmentPath);
-        Main.map.mapView.repaint();
-        System.out.println("shortest path found: " + fullPath + " weight: " + fullWeight);
         
-        double weight2 = 0;
-        for(Segment seg : segmentPath) {
-            weight2 += Math.sqrt(seg.from.coor.distance(seg.to.coor)) * 111000;
+        edgePath = new ArrayList<SegmentEdge>();
+        edgePath.addAll(fullPath);
+        
+        System.out.println("shortest path found: " + fullPath + " weight: " + fullWeight);
+        System.out.println(getPathDescription());
+//        double weight2 = 0;
+//        for(Segment seg : getSegmentPath()) {
+//            weight2 += Math.sqrt(seg.from.coor.distance(seg.to.coor)) * 111000;
+//        }
+//        System.out.println("all added: " + weight2);
+    }
+    
+    public String getPathDescription() {
+        List<PathDescription> pathDescriptions = getPathDescriptions();
+        
+        // create text representation from description:
+        StringBuilder builder = new StringBuilder();
+        for(PathDescription desc : pathDescriptions) {
+            builder.append("follow ");
+            String tmp = desc.getWay().get("name");
+            if(tmp == null) {
+                builder.append("unkown street ");
+            } else {
+                builder.append(tmp).append(" ");
+            }
+            tmp = desc.getWay().get("highway");
+            if(tmp != null) {
+                builder.append("(").append(tmp).append(") ");
+            }
+            builder.append("for ").append((int)desc.getLength()).append(" meters, then\n");
         }
-        System.out.println("all added: " + weight2);
+        builder.delete(builder.length() - ", then ".length(), builder.length());
+        return builder.toString();
+    }
+
+    /**
+     * @return
+     */
+    private List<PathDescription> getPathDescriptions() {
+        List<PathDescription> pathDescriptions = new LinkedList<PathDescription>();
+        PathDescription description;
+        double length = 0;
+        Way oldWay = null;
+        Way way = null;
+        for(SegmentEdge edge : edgePath) {
+            way = edge.getWay();
+            length += edge.getLengthInM();
+            if(oldWay != null && !oldWay.equals(way)) {
+                description = new PathDescription(oldWay, length);
+                pathDescriptions.add(description);
+                length = 0;
+            }
+            oldWay = way;
+        }
+        if(way != null) {
+            description = new PathDescription(way, length);
+            pathDescriptions.add(description);
+        }
+        return pathDescriptions;
     }
 
 
@@ -100,10 +147,18 @@ public class NavigatorModel {
 
 
     /**
-     * @return the segmentPath
+     * @return the segmentPath or <code>null</code>.
      */
     public List<Segment> getSegmentPath() {
-        return this.segmentPath;
+        if(segmentPath == null && edgePath != null) {
+            segmentPath = new LinkedList<Segment>();
+            synchronized(segmentPath) {
+                for (SegmentEdge edge : edgePath) {
+                    segmentPath.add(edge.getSegment());
+                }
+            }
+        }
+        return segmentPath;
     }
 
 
@@ -114,12 +169,21 @@ public class NavigatorModel {
     public void addNode(Node node) {
         nodes.add(node);
     }
+
+    /**
+     * @return the edgePath
+     */
+    public List<SegmentEdge> getEdgePath() {
+        return this.edgePath;
+    }
     
     /**
-     * Clear all nodes to navigate. 
+     * Resets all data (nodes, edges, segments).
      */
-    public void clearNodes() {
-        nodes.clear();        
+    public void reset() {
+        segmentPath = null;
+        edgePath = null;
+        nodes.clear();
     }
 
 
