7a2dc5e163e4863590cad080379dd65781999457
christofd <christofd@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-07-28 23:03:37 +0000

    add way and length
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@3834 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- navigator/src/at/dallermassl/josm/plugin/navigator/SegmentEdge.java
+++ navigator/src/at/dallermassl/josm/plugin/navigator/SegmentEdge.java
@@ -5,32 +5,75 @@ package at.dallermassl.josm.plugin.navigator;
 
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.openstreetmap.josm.data.osm.Segment;
+import org.openstreetmap.josm.data.osm.Way;
 
 /**
  * @author cdaller
- *
+ * 
  */
 public class SegmentEdge extends DefaultWeightedEdge {
-  private Segment segment;
-  
-  public SegmentEdge() {
-    super();
-    System.out.println("warning: edge without segment!");
-  }
+    private Segment segment;
+    private boolean inverted;
+    private Way way;
 
-  /**
-   * @param segment
-   */
-  public SegmentEdge(Segment segment) {
-    super();
-    this.segment = segment;
-  }
+    public SegmentEdge() {
+        super();
+        System.out.println("warning: edge without segment!");
+    }
 
-  /**
-   * @return the segment
-   */
-  public Segment getSegment() {
-    return this.segment;
-  }
+    /**
+     * @param segment
+     */
+    public SegmentEdge(Segment segment) {
+        this(segment, false);
+    }
+
+    /**
+     * @param segment
+     * @param inverted
+     *            if <code>true</code> the edge is the other direction as the contained segment.
+     */
+    public SegmentEdge(Segment segment, boolean inverted) {
+        super();
+        this.segment = segment;
+        this.inverted = inverted;
+    }
+
+    /**
+     * @return the segment
+     */
+    public Segment getSegment() {
+        return this.segment;
+    }
+
+    /**
+     * @return the way
+     */
+    public Way getWay() {
+        return this.way;
+    }
+
+    /**
+     * @param way
+     *            the way to set
+     */
+    public void setWay(Way way) {
+        this.way = way;
+    }
+
+    /**
+     * @return the inverted
+     */
+    public boolean isInverted() {
+        return this.inverted;
+    }
+
+    /**
+     * Returns length of segment in meters (correct only on equator!)
+     * @return length of semgent in meters.
+     */
+    public double getLengthInM() {
+        return Math.sqrt(segment.from.coor.distance(segment.to.coor)) * 111000;
+    }
 
 }
