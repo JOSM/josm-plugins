608a5e7e4765dae06516bd7519260328512d1b27
david <david@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-08-12 11:08:46 +0000

    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@4088 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- nearclick/src/nearclick/NearClickPlugin.java
+++ nearclick/src/nearclick/NearClickPlugin.java
@@ -7,15 +7,26 @@ import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.event.AWTEventListener;
 import java.awt.event.MouseEvent;
+import org.openstreetmap.josm.Main;
 
 public class NearClickPlugin implements AWTEventListener {
 
     private int mouseDownX = -1;
     private int mouseDownY = -1;
     private long mouseDownTime = -1;
+    private int radiussquared = 49;
+    private int delay = 250;
 
     public NearClickPlugin() {
         Toolkit.getDefaultToolkit().addAWTEventListener(this, AWTEvent.MOUSE_EVENT_MASK | AWTEvent.MOUSE_MOTION_EVENT_MASK);
+	try {
+	    int radius = Integer.parseInt(Main.pref.get("nearclick.radius", "7"));
+	    radiussquared = radius * radius;
+	    delay = Integer.parseInt(Main.pref.get("nearclick.delay", "250"));
+	} catch (NumberFormatException ex) {
+	    delay = 250;
+	    radiussquared = 7 * 7;
+	}
     }
 
     public void eventDispatched(AWTEvent event) {
@@ -23,12 +34,15 @@ public class NearClickPlugin implements AWTEventListener {
             MouseEvent e = (MouseEvent)event;
             if ( e.getButton() != MouseEvent.BUTTON1 )
                 return;
+	    int xdiff = e.getPoint().x - mouseDownX;
+	    int ydiff = e.getPoint().y - mouseDownY;
+	    
             if (e.getID() == MouseEvent.MOUSE_RELEASED) {
-                if ( e.getWhen() - mouseDownTime < 250 &&
-                        e.getPoint().x - mouseDownX < 7 &&
-                        e.getPoint().y - mouseDownY < 7 &&
-                        e.getPoint().x != mouseDownX &&
-                        e.getPoint().y != mouseDownY ) {
+                if ( e.getWhen() - mouseDownTime < delay &&
+		     ( e.getPoint().x != mouseDownX ||
+		       e.getPoint().y != mouseDownY) &&
+		     xdiff * xdiff + ydiff * ydiff < radiussquared
+		   ) {
                     try {
                         Robot r = new Robot(GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice());
                         r.mousePress(MouseEvent.BUTTON1_MASK);
