5901852c29d95fa2ddd0b40fd5135c1db6ee808d
ulf <ulf@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-11-10 01:05:09 +0000

    move the two remaining UtilsPlugin functions from the toolbar to the Tools menu, as they are much more in common with that menu
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@5417 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- utilsplugin/src/UtilsPlugin/UtilsPlugin.java
+++ utilsplugin/src/UtilsPlugin/UtilsPlugin.java
@@ -2,7 +2,6 @@ package UtilsPlugin;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
-import org.openstreetmap.josm.gui.IconToggleButton;
 
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
@@ -17,15 +16,24 @@ import javax.swing.BoxLayout;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.IconToggleButton;
 
 public class UtilsPlugin extends Plugin {
+	JMenuItem MergeNodeWay;
+	JMenuItem SimplifyWay;
+
+	public UtilsPlugin() {
+		MergeNodeWay = Main.main.menu.toolsMenu.add(new MergeNodeWayAction());
+		MergeNodeWay.setEnabled(false);
+		SimplifyWay = Main.main.menu.toolsMenu.add(new SimplifyWayAction());
+		SimplifyWay.setEnabled(false);
+	}
+	
 	@Override
 	public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
 		if (oldFrame == null && newFrame != null) {
-			Main.map.toolBarActions.addSeparator();
-			Main.map.toolBarActions.add(new MergeNodesAction());
-			Main.map.toolBarActions.add(new MergeNodeWayAction());
-			Main.map.toolBarActions.add(new SimplifyWayAction());
+			MergeNodeWay.setEnabled(true);
+			SimplifyWay.setEnabled(true);
 		}
 	}
 }
