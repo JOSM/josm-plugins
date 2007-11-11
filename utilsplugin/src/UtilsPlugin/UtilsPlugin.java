cffb466a0164b28c3d489d94b49d7bfa51eefd07
ulf <ulf@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-11-11 13:19:08 +0000

    move "join node to way" function from UtilsPlugin to core
    also remove Mergenodes code, it's already in the core for a while
    
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@5452 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- utilsplugin/src/UtilsPlugin/UtilsPlugin.java
+++ utilsplugin/src/UtilsPlugin/UtilsPlugin.java
@@ -20,17 +20,11 @@ import org.openstreetmap.josm.gui.IconToggleButton;
 import org.openstreetmap.josm.actions.JosmAction;
 
 public class UtilsPlugin extends Plugin {
-	JMenuItem JoinNodeWay;
 	JMenuItem SimplifyWay;
 
 	public UtilsPlugin() {
 		JosmAction action;
 		
-		action = new JoinNodeWayAction();
-		JoinNodeWay = Main.main.menu.toolsMenu.add(action);
-		JoinNodeWay.setAccelerator(action.shortCut);
-		JoinNodeWay.setEnabled(false);
-
 		action = new SimplifyWayAction();
 		SimplifyWay = Main.main.menu.toolsMenu.add(action);
 		SimplifyWay.setAccelerator(action.shortCut);
@@ -40,7 +34,6 @@ public class UtilsPlugin extends Plugin {
 	@Override
 	public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
 		if (oldFrame == null && newFrame != null) {
-			JoinNodeWay.setEnabled(true);
 			SimplifyWay.setEnabled(true);
 		}
 	}
