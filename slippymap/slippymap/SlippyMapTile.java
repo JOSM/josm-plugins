7d3fd33f6256f524060fa120e088a6385335ae0e
hakan <hakan@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2007-11-21 15:54:38 +0000

    Fetch map tiles from the official server
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@5667 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- slippymap/slippymap/SlippyMapTile.java
+++ slippymap/slippymap/SlippyMapTile.java
@@ -13,56 +13,53 @@ import javax.swing.JComponent;
 
 /**
  * Class that contains information about one single slippy map tile.
- * 
+ *
  * @author Frederik Ramm <frederik@remote.org>
  *
  */
-public class SlippyMapTile 
+public class SlippyMapTile
 {
 
 	private Image tileImage;
-	
+
 	int x;
 	int y;
 	int z;
 
     private String metadata;
-	
-	public SlippyMapTile(int x, int y, int z) 
+
+	public SlippyMapTile(int x, int y, int z)
 	{
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-	
+
 	public String getMetadata()
 	{
 		return metadata;
 	}
-	
+
 	public void loadImage()
 	{
 		try
 		{
-			// tileImage = new ImageIcon(new URL("http://dev.openstreetmap.org/~ojw/Tiles/tile.php/"+z+"/"+x+"/"+y+".png"));
-			//tileImage = Toolkit.getDefaultToolkit().createImage(new URL("http://openstreetmap.gryph.de/slippymap/tiles/"+z+"/"+x+"/"+y+".png"));
-			tileImage = Toolkit.getDefaultToolkit().createImage(new URL("http://dev.openstreetmap.org/~ojw/Tiles/tile.php/"+z+"/"+x+"/"+y+".png"));
-//				
+			tileImage = Toolkit.getDefaultToolkit().createImage(new URL("http://tah.openstreetmap.org/Tiles/tile/"+z+"/"+x+"/"+y+".png"));
 		}
 		catch (MalformedURLException mfu)
 		{
 			mfu.printStackTrace();
-		}		
+		}
 	}
 
 	public Image getImage()
 	{
 		return tileImage;
 	}
-	
+
 	public void loadMetadata()
 	{
-        try 
+        try
         {
             URL dev = new URL("http://tah.openstreetmap.org/Tiles/info_short.php?x=" +
                 x + "&y=" + y + "&z=12/layer=tile");
@@ -92,12 +89,12 @@ public class SlippyMapTile
             metadata = "error requesting update";
         }
     }
-	
+
 	public boolean equals(Object o)
 	{
 		if (!(o instanceof SlippyMapTile)) return false;
 		SlippyMapTile other = (SlippyMapTile) o;
 		return (this.x == other.x && this.y == other.y && this.z == other.z);
 	}
-	
+
 }
