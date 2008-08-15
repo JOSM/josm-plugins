f4749c6f913b5c3979546cb30424a282afe1397c
stoecker <stoecker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-08-15 12:03:19 +0000

    added two server messages
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@9857 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ lang/specialmessages.java
@@ -0,0 +1,7 @@
+/**
+  This file can be used to add some special messages to the translation,
+  which are not part of the programm texts, but should be translated nevertheless.
+*/
+
+tr("You requested too many nodes (limit is 50,000). Either request a smaller area, or use planet.osm"); // OSM server message
+tr("The maximum bbox size is 0.25, and your request was too large. Either request a smaller area, or use planet.osm"); // OSM server message
