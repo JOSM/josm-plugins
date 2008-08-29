3ea46609af3210c2ea94a788ad38848830e0d0c0
stoecker <stoecker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-08-29 16:41:34 +0000

    some more texts
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@10311 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- lang/specialmessages.java
+++ lang/specialmessages.java
@@ -5,3 +5,4 @@
 
 tr("You requested too many nodes (limit is 50,000). Either request a smaller area, or use planet.osm"); // OSM server message
 tr("The maximum bbox size is 0.25, and your request was too large. Either request a smaller area, or use planet.osm"); // OSM server message
+tr("could not get audio input stream from input URL"); // Java message loading audio data
