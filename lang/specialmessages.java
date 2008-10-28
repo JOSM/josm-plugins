aa3e7049862ed96f9ae155211f473f067d903bed
mfloryan <mfloryan@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-10-28 22:17:28 +0000

    Removed the JOSM language plugins as obsolete. Translations for JOSM can now be found in the i18n directory.
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@11537 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- lang/specialmessages.java
+++
@@ -1,8 +0,0 @@
-/**
-  This file can be used to add some special messages to the translation,
-  which are not part of the programm texts, but should be translated nevertheless.
-*/
-
-tr("You requested too many nodes (limit is 50,000). Either request a smaller area, or use planet.osm"); // OSM server message
-tr("The maximum bbox size is 0.25, and your request was too large. Either request a smaller area, or use planet.osm"); // OSM server message
-tr("could not get audio input stream from input URL"); // Java message loading audio data
