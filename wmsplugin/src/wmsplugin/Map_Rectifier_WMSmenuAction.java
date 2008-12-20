55a8be7e82d6112a6e51da94401113263374d9a3
stoecker <stoecker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-12-20 21:53:32 +0000

    remember id
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@12444 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- wmsplugin/src/wmsplugin/Map_Rectifier_WMSmenuAction.java
+++ wmsplugin/src/wmsplugin/Map_Rectifier_WMSmenuAction.java
@@ -21,12 +21,14 @@ public class Map_Rectifier_WMSmenuAction extends JosmAction {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		String newid = JOptionPane.showInputDialog(Main.parent, tr("Metacarta Map Rectifier image id"));
+		String newid = JOptionPane.showInputDialog(Main.parent, tr("Metacarta Map Rectifier image id"),
+		Main.pref.get("wmsplugin.rectifier_id"));
 
 		if (newid != null && !newid.equals("")) {
 			String newURL = "http://labs.metacarta.com/rectifier/wms.cgi?id="+newid+
 			"&srs=EPSG:4326&Service=WMS&Version=1.1.0&Request=GetMap&format=image/png";
 
+			Main.pref.put("wmsplugin.rectifier_id", newid);
 			WMSLayer wmsLayer = new WMSLayer(tr("rectifier id={0}",newid), newURL);
 			Main.main.addLayer(wmsLayer);
 		}
