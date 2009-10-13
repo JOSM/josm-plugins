2fe0746f5da023b17e5e65be25e71b85a0a7aa52
pieren <pieren@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-10-13 10:22:54 +0000

    add a file filter in selector
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@18109 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- cadastre-fr/src/cadastre_fr/MenuActionSaveRasterAs.java
+++ cadastre-fr/src/cadastre_fr/MenuActionSaveRasterAs.java
@@ -4,14 +4,20 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
+
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 public class MenuActionSaveRasterAs extends JosmAction {
 
@@ -20,6 +26,24 @@ public class MenuActionSaveRasterAs extends JosmAction {
     private static final long serialVersionUID = 1L;
     
     private WMSLayer wmsLayer;
+    
+    public class FiltrePng extends FileFilter {
+        @Override
+        public boolean accept(File file) {
+            if (file.isDirectory()) { 
+                return true;
+            } 
+            return file.getName().toLowerCase().endsWith(".png");
+        }
+
+        @Override
+        public String getDescription() {
+            return tr("PNG files (*.png)");
+        }
+        
+    }
+    
+    FiltrePng filtrePng = new FiltrePng();
 
     public MenuActionSaveRasterAs(WMSLayer wmsLayer) {
         super(tr(name), "save", tr("Export as PNG format (only raster images)"), null, false);
@@ -29,12 +53,22 @@ public class MenuActionSaveRasterAs extends JosmAction {
     public void actionPerformed(ActionEvent arg0) {
         File file;
         JFileChooser fc = new JFileChooser();
+        fc.setFileFilter(filtrePng);
         int returnVal = fc.showSaveDialog(Main.parent);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             file = fc.getSelectedFile();
+            if (!file.getName().endsWith(".png"))
+                file = new File(file.getParent(), file.getName()+".png");
             BufferedImage bi = wmsLayer.images.get(0).image; 
             try {
                 ImageIO.write(bi, "png", file);
+/*
+                FileOutputStream flux = new FileOutputStream(file);
+                BufferedOutputStream fluxBuf = new BufferedOutputStream(flux);
+                JPEGImageEncoder codec = JPEGCodec.createJPEGEncoder(fluxBuf, JPEGCodec.getDefaultJPEGEncodeParam(bi));
+                codec.encode(bi);
+                fluxBuf.close();
+*/
             } catch (IOException e) {
                 e.printStackTrace();
             }
