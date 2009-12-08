4498b80e0145bb907089c0059df705a969ab0dfa
casualwalker <casualwalker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-12-08 22:43:51 +0000

    LiveGps plugin: refresh interval will be set in seconds (more user-friendly than milliseconds).
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19000 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- livegps/src/livegps/LiveGpsSuppressor.java
+++ livegps/src/livegps/LiveGpsSuppressor.java
@@ -17,95 +17,100 @@ import org.openstreetmap.josm.Main;
  */
 public class LiveGpsSuppressor implements Runnable {
 
-    /**
-     * Default sleep time is 5 seconds.
-     */
-    private static final int DEFAULT_SLEEP_TIME = 5000;
+	/**
+	 * Default sleep time is 5 seconds.
+	 */
+	private static final int DEFAULT_SLEEP_TIME = 5000;
 
-    /**
-     * The currently used sleepTime.
-     */
-    private int sleepTime = DEFAULT_SLEEP_TIME;
+	/**
+	 * The currently used sleepTime.
+	 */
+	private int sleepTime = DEFAULT_SLEEP_TIME;
 
-    /**
-     * The flag allowUpdate is enabled once during the sleepTime.
-     */
-    private boolean allowUpdate = false;
+	/**
+	 * The flag allowUpdate is enabled once during the sleepTime.
+	 */
+	private boolean allowUpdate = false;
 
-    /**
-     * Controls if this thread is still in used.
-     */
-    boolean shutdownFlag = false;
+	/**
+	 * Controls if this thread is still in used.
+	 */
+	boolean shutdownFlag = false;
 
-    /**
-     * Run thread enables the allowUpdate flag once during its cycle.
-     * @see java.lang.Runnable#run()
-     */
-    public void run() {
-        initSleepTime();
+	/**
+	 * Run thread enables the allowUpdate flag once during its cycle.
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		initSleepTime();
 
-        shutdownFlag = false;
-        while (!shutdownFlag) {
-            setAllowUpdate(true);
+		shutdownFlag = false;
+		while (!shutdownFlag) {
+			setAllowUpdate(true);
 
-            try {
-                Thread.sleep(getSleepTime());
-            } catch (InterruptedException e) {
-                // TODO I never knew, how to handle this??? Probably just carry on
-            }
-        }
+			try {
+				Thread.sleep(getSleepTime());
+			} catch (InterruptedException e) {
+				// TODO I never knew, how to handle this??? Probably just carry
+				// on
+			}
+		}
 
-    }
+	}
 
-    /**
-     * Retrieve the sleepTime from the configuration.
-     * If no such configuration key exists, it will be initialized here.
-     */
-    private void initSleepTime() {
-        // fetch it from the user setting, or use the default value.
-        sleepTime = Main.pref.getInteger("livegps.refreshinterval",
-                DEFAULT_SLEEP_TIME);
-        // creates the setting, if none present.
-        Main.pref.putInteger("livegps.refreshinterval", sleepTime);
-    }
+	/**
+	 * Retrieve the sleepTime from the configuration.
+	 * If no such configuration key exists, it will be initialized here.
+	 */
+	private void initSleepTime() {
+		// fetch it from the user setting, or use the default value.
+		int sleepSeconds = 0;
+		sleepSeconds = Main.pref.getInteger("livegps.refreshinterval",
+				DEFAULT_SLEEP_TIME);
+		// creates the setting, if none present.
+		Main.pref.putInteger("livegps.refreshinterval", sleepSeconds);
 
-    /**
-     * Set the allowUpdate flag. May only privately accessible!
-     * @param allowUpdate the allowUpdate to set
-     */
-    private synchronized void setAllowUpdate(boolean allowUpdate) {
-        this.allowUpdate = allowUpdate;
-    }
+		// convert seconds into milliseconds internally.
+		this.sleepTime = sleepSeconds * 1000;
+	}
 
-    /**
-     * Query, if an update is currently allowed.
-     * When it is allowed, it will disable the allowUpdate flag as a side effect.
-     * (this means, one thread got to issue an update event)
-     *
-     * @return true, if an update is currently allowed; false, if the update shall be suppressed.
-     */
-    public synchronized boolean isAllowUpdate() {
+	/**
+	 * Set the allowUpdate flag. May only privately accessible!
+	 * @param allowUpdate the allowUpdate to set
+	 */
+	private synchronized void setAllowUpdate(boolean allowUpdate) {
+		this.allowUpdate = allowUpdate;
+	}
 
-        if (allowUpdate) {
-            allowUpdate = false;
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * Query, if an update is currently allowed.
+	 * When it is allowed, it will disable the allowUpdate flag as a side effect.
+	 * (this means, one thread got to issue an update event)
+	 *
+	 * @return true, if an update is currently allowed; false, if the update shall be suppressed.
+	 */
+	public synchronized boolean isAllowUpdate() {
 
-    /**
-     * Shut this thread down.
-     */
-    public void shutdown() {
-        shutdownFlag = true;
-    }
+		if (allowUpdate) {
+			allowUpdate = false;
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    /**
-     * @return the defaultSleepTime
-     */
-    private int getSleepTime() {
-        return this.sleepTime;
-    }
+	/**
+	 * Shut this thread down.
+	 */
+	public void shutdown() {
+		shutdownFlag = true;
+	}
+
+	/**
+	 * @return the defaultSleepTime
+	 */
+	private int getSleepTime() {
+		return this.sleepTime;
+	}
 
 }
