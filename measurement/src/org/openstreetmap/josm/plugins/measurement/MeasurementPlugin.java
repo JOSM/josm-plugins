f33f9e282087518f7c586e2e1793a9888f38faad
guggis <guggis@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-12-12 17:18:59 +0000

    'Updated to JOSM 2621; updated build.xml'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19058 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- measurement/src/org/openstreetmap/josm/plugins/measurement/MeasurementPlugin.java
+++ measurement/src/org/openstreetmap/josm/plugins/measurement/MeasurementPlugin.java
@@ -5,8 +5,9 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.IconToggleButton;
 import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.MapView;
+import org.openstreetmap.josm.gui.MapView.LayerChangeListener;
 import org.openstreetmap.josm.gui.layer.Layer;
-import org.openstreetmap.josm.gui.layer.Layer.LayerChangeListener;
 import org.openstreetmap.josm.plugins.Plugin;
 
 public class MeasurementPlugin extends Plugin {
@@ -35,7 +36,7 @@ public class MeasurementPlugin extends Plugin {
         if(currentLayer == null){
             currentLayer = new MeasurementLayer(tr("Measurements"));
             Main.main.addLayer(currentLayer);
-            Layer.listeners.add(new LayerChangeListener(){
+            MapView.addLayerChangeListener(new LayerChangeListener(){
                 public void activeLayerChange(final Layer oldLayer, final Layer newLayer) {
                     if(newLayer instanceof MeasurementLayer)
                         MeasurementPlugin.currentLayer = (MeasurementLayer)newLayer;
@@ -43,6 +44,8 @@ public class MeasurementPlugin extends Plugin {
                 public void layerAdded(final Layer newLayer) {
                 }
                 public void layerRemoved(final Layer oldLayer) {
+                	if (oldLayer != null && oldLayer == currentLayer)
+                		MapView.removeLayerChangeListener(this);
                 }
             });
         }
