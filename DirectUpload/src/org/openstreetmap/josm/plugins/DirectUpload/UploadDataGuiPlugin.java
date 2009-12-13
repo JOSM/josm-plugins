b826f2915bfd612af2ad448eeb068e5c48ca1062
guggis <guggis@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-12-13 09:00:16 +0000

    'Rebuilding because of JOSM trac ticket #4156; Enabled state management in UploadAction'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19082 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- DirectUpload/src/org/openstreetmap/josm/plugins/DirectUpload/UploadDataGuiPlugin.java
+++ DirectUpload/src/org/openstreetmap/josm/plugins/DirectUpload/UploadDataGuiPlugin.java
@@ -10,9 +10,11 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
-import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.MapView.LayerChangeListener;
+import org.openstreetmap.josm.gui.layer.GpxLayer;
 import org.openstreetmap.josm.gui.layer.Layer;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.tools.Shortcut;
@@ -28,8 +30,7 @@ public class UploadDataGuiPlugin extends Plugin{
         Main.main.menu.toolsMenu.add(openaction);
     }
 
-    class UploadAction extends JosmAction {
-
+    class UploadAction extends JosmAction{
         public UploadAction(){
             super(tr("Upload Traces"), "UploadAction", tr("Uploads traces to openstreetmap.org"),
             Shortcut.registerShortcut("tools:uploadtraces", tr("Tool: {0}", tr("Upload Traces")),
@@ -39,5 +40,18 @@ public class UploadDataGuiPlugin extends Plugin{
             UploadDataGui go = new UploadDataGui();
             go.setVisible(true);
         }
+
+        @Override
+		protected void updateEnabledState() {
+            if(Main.map == null
+                    || Main.map.mapView == null
+                    || Main.map.mapView.getActiveLayer() == null
+                    || !(Main.map.mapView.getActiveLayer() instanceof GpxLayer)) {                
+                setEnabled(false);
+            } else {
+            	setEnabled(true);
+            }
+
+		}		
     }
 }
