e877b5c614d37abfb7c0add18310bbb5dc6e66d5
guggis <guggis@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-12-28 12:26:27 +0000

    'Fixed JOSM ticket #4220 - validator: check for FIXME=*'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19229 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- validator/src/org/openstreetmap/josm/plugins/validator/tests/TagChecker.java
+++ validator/src/org/openstreetmap/josm/plugins/validator/tests/TagChecker.java
@@ -546,15 +546,16 @@ public class TagChecker extends Test
                     }
                 }
             }
-            if( checkFixmes && value != null && value.length() > 0 )
-            {
-                if( (value.contains("FIXME") || value.contains("check and delete") || key.contains("todo") || key.contains("fixme"))
-                && !withErrors.contains(p, "FIXME"))
-                {
-                    errors.add( new TestError(this, Severity.OTHER, tr("FIXMES"), FIXME, p) );
-                    withErrors.add(p, "FIXME");
-                }
-            }
+			if (checkFixmes && value != null && value.length() > 0) {
+				if ((value.toLowerCase().contains("FIXME")
+						|| value.contains("check and delete")
+						|| key.contains("todo") || key.toLowerCase().contains("fixme"))
+						&& !withErrors.contains(p, "FIXME")) {
+					errors.add(new TestError(this, Severity.OTHER,
+							tr("FIXMES"), FIXME, p));
+					withErrors.add(p, "FIXME");
+				}
+			}
         }
     }
 
---
+++ validator/test/org/openstreetmap/josm/plugins/validator/tests/TagCheckerTest.groovy
@@ -0,0 +1,57 @@
+package org.openstreetmap.josm.plugins.validator.tests;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.openstreetmap.josm.plugins.validator.tests.TagChecker
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.coor.LatLon
+import org.openstreetmap.josm.gui.progress.NullProgressMonitor;
+
+
+class TagCheckerTest {
+	
+	@BeforeClass
+	static public void init() {
+		TagChecker.initializeData()
+	}
+	
+	@Test
+	public void fixme_lowercase() {
+		TagChecker t  = new TagChecker()
+		
+		def ds = new DataSet()
+		def n1 = new Node(new LatLon(1.0,1.0))
+		n1.put("fixme", "suvey later")
+		ds.addPrimitive n1
+
+		def nodes = [n1]
+
+		t.startTest NullProgressMonitor.INSTANCE
+		t.visit nodes
+		t.endTest()
+		
+		assert t.@errors.size() == 1
+	}
+	
+	@Test
+	public void fixme_upercase() {
+		TagChecker t  = new TagChecker()
+		
+		def ds = new DataSet()
+		def n1 = new Node(new LatLon(1.0,1.0))
+		n1.put("FIXME", "suvey later")
+		ds.addPrimitive n1
+		
+		def nodes = [n1]
+		
+		t.startTest NullProgressMonitor.INSTANCE
+		t.visit nodes
+		t.endTest()
+		
+		assert t.@errors.size() == 1
+	}
+	
+}
