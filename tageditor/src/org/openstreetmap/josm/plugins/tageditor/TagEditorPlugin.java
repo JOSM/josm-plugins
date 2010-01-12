a10cc23b3b60589379a6761e5c59eb39d95b74f7
guggis <guggis@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-01-12 17:33:41 +0000

    'Uses new constructor for Plugin'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19413 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- tageditor/src/org/openstreetmap/josm/plugins/tageditor/TagEditorPlugin.java
+++ tageditor/src/org/openstreetmap/josm/plugins/tageditor/TagEditorPlugin.java
@@ -1,6 +1,7 @@
 package org.openstreetmap.josm.plugins.tageditor;
 
 import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class TagEditorPlugin extends Plugin {
 
@@ -9,7 +10,8 @@ public class TagEditorPlugin extends Plugin {
 	/**
 	 * constructor 
 	 */
-	public TagEditorPlugin() {
+	public TagEditorPlugin(PluginInformation info) {
+		super(info);
 		action = new LaunchAction();
 	}
 }
