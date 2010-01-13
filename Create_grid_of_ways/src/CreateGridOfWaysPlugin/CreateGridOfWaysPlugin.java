f2ef90373b0ace46ed6bba5d831afcc3f41142c2
guggis <guggis@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-01-13 13:20:44 +0000

    'Changed constructor signature, updated build.xml'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19428 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- Create_grid_of_ways/src/CreateGridOfWaysPlugin/CreateGridOfWaysPlugin.java
+++ Create_grid_of_ways/src/CreateGridOfWaysPlugin/CreateGridOfWaysPlugin.java
@@ -2,10 +2,12 @@ package CreateGridOfWaysPlugin;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
 import org.openstreetmap.josm.gui.MainMenu;
 
 public class CreateGridOfWaysPlugin extends Plugin {
-    public CreateGridOfWaysPlugin() {
+    public CreateGridOfWaysPlugin(PluginInformation info) {
+    	super(info);
         MainMenu.add(Main.main.menu.toolsMenu, new CreateGridOfWaysAction());
     }
 }
