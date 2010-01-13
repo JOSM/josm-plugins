5c6eb04faeafdfe92ded7246c93f4fb49b49e4dd
guggis <guggis@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-01-13 14:10:15 +0000

    'Changed the constructor signature of the plugin main class'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19469 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- plastic_laf/src/plastic_laf/Plugin.java
+++ plastic_laf/src/plastic_laf/Plugin.java
@@ -3,10 +3,12 @@ package plastic_laf;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import org.openstreetmap.josm.plugins.PluginInformation;
+
 import com.jgoodies.looks.plastic.PlasticLookAndFeel;
 
 public class Plugin {
-    public Plugin() throws UnsupportedLookAndFeelException {
+    public Plugin(PluginInformation info) throws UnsupportedLookAndFeelException {
         UIManager.getDefaults().put("ClassLoader", getClass().getClassLoader());
         UIManager.setLookAndFeel(new PlasticLookAndFeel());
     }
