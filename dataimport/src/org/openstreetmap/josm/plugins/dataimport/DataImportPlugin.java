f8afc70c6c87d3bcb99cbecc8e373df0d91b63c5
guggis <guggis@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-01-13 13:23:59 +0000

    'Changed constructor signature, updated build.xml'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19430 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- dataimport/src/org/openstreetmap/josm/plugins/dataimport/DataImportPlugin.java
+++ dataimport/src/org/openstreetmap/josm/plugins/dataimport/DataImportPlugin.java
@@ -9,14 +9,15 @@ import org.openstreetmap.josm.actions.ExtensionFileFilter;
 import org.openstreetmap.josm.plugins.dataimport.io.TangoGPS;
 import org.openstreetmap.josm.plugins.dataimport.io.Tcx;
 import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class DataImportPlugin extends Plugin {
 
     /**
      * Add new File import filter into open dialog
      */
-    public DataImportPlugin() throws IOException{
-        super();
+    public DataImportPlugin(PluginInformation info) throws IOException{
+        super(info);
 
         ExtensionFileFilter.importers.add(new TangoGPS());
         ExtensionFileFilter.importers.add(new Tcx());
