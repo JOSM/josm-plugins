d482a960645621d0c2a0cc46a088acd07d2eadad
guggis <guggis@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-01-13 13:48:46 +0000

    'Commit message'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19452 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- michigan_left/src/MichiganLeft/MichiganLeft.java
+++ michigan_left/src/MichiganLeft/MichiganLeft.java
@@ -5,11 +5,13 @@ import javax.swing.JMenuItem;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.MainMenu;
 import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class MichiganLeft extends Plugin {
     JMenuItem MichiganLeft;
 
-    public MichiganLeft() {
+    public MichiganLeft(PluginInformation info) {
+    	super(info);
         MichiganLeft = MainMenu.add(Main.main.menu.toolsMenu, new MichiganLeftAction());
 
     }
