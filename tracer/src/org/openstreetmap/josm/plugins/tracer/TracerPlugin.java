c289a9351014080daacd9ed1b236a615b1732710
jabi <jabi@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-02-04 18:11:21 +0000

    Change TracerPlugin constructor to work in JOSM 2561.
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@19868 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- tracer/src/org/openstreetmap/josm/plugins/tracer/TracerPlugin.java
+++ tracer/src/org/openstreetmap/josm/plugins/tracer/TracerPlugin.java
@@ -13,8 +13,18 @@ import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class TracerPlugin extends Plugin {
 
+    public TracerPlugin() {
+        MainMenu.add(Main.main.menu.toolsMenu, new TracerAction(Main.main.map));
+    }
+
+	/*
+	
+	Working only in new version.
+	
     public TracerPlugin(PluginInformation info) {
         super(info);
         MainMenu.add(Main.main.menu.toolsMenu, new TracerAction(Main.map));
     }
+    
+    */
 }
