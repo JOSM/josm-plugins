f03db059723b51a83654a1c73af5cfa7b2ac0e13
jttt <jttt@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-03-21 12:19:55 +0000

    Use referrers for UntaggedNode test
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@20579 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- validator/src/org/openstreetmap/josm/plugins/validator/tests/UntaggedNode.java
+++ validator/src/org/openstreetmap/josm/plugins/validator/tests/UntaggedNode.java
@@ -2,16 +2,15 @@ package org.openstreetmap.josm.plugins.validator.tests;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.DeleteCommand;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
-import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.gui.progress.ProgressMonitor;
 import org.openstreetmap.josm.plugins.validator.Severity;
 import org.openstreetmap.josm.plugins.validator.Test;
@@ -27,7 +26,7 @@ public class UntaggedNode extends Test
     protected static int UNTAGGED_NODE = 201;
 
     /** Bag of all nodes */
-    Set<Node> emptyNodes;
+    List<Node> emptyNodes;
 
     /**
      * Constructor
@@ -35,36 +34,22 @@ public class UntaggedNode extends Test
     public UntaggedNode()
     {
         super(tr("Untagged and unconnected nodes")+".",
-              tr("This test checks for untagged nodes that are not part of any way."));
+                tr("This test checks for untagged nodes that are not part of any way."));
     }
 
     @Override
     public void startTest(ProgressMonitor monitor)
     {
-    	super.startTest(monitor);
-        emptyNodes = new HashSet<Node>(100);
+        super.startTest(monitor);
+        emptyNodes = new ArrayList<Node>();
     }
 
     @Override
     public void visit(Collection<OsmPrimitive> selection)
     {
-        // If there is a partial selection, it may be false positives if a
-        // node is selected, but not the container way. So, in this
-        // case, we must visit all ways, selected or not.
-        if (partialSelection) {
-            for (OsmPrimitive p : selection) {
-                if (p.isUsable() && p instanceof Node) {
-                    p.visit(this);
-                }
-            }
-            for (Way w : Main.main.getCurrentDataSet().getWays()) {
-                visit(w);
-            }
-        } else {
-            for (OsmPrimitive p : selection) {
-                if (p.isUsable()) {
-                    p.visit(this);
-                }
+        for (OsmPrimitive p : selection) {
+            if (p.isUsable() && p instanceof Node) {
+                p.visit(this);
             }
         }
     }
@@ -72,16 +57,8 @@ public class UntaggedNode extends Test
     @Override
     public void visit(Node n)
     {
-        if(n.isUsable() && !n.isTagged())
+        if(n.isUsable() && !n.isTagged() && n.getReferrers().isEmpty())
             emptyNodes.add(n);
-    }
-
-    @Override
-    public void visit(Way w)
-    {
-        for (Node n : w.getNodes()) {
-            emptyNodes.remove(n);
-        }
     }
 
     @Override
