3d2e6ed9821bb3a8f4e5db109d3d3620d3ebdba5
stoecker <stoecker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-06-09 13:21:27 +0000

    fixes by Gleb Smirnoff
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@21622 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- livegps/src/livegps/LiveGpsAcquirer.java
+++ livegps/src/livegps/LiveGpsAcquirer.java
@@ -2,6 +2,8 @@ package livegps;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.lang.Float;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.BufferedReader;
@@ -15,13 +17,19 @@ import java.util.List;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.coor.LatLon;
 
+import org.json.JSONObject;
+import org.json.JSONException;
+
 public class LiveGpsAcquirer implements Runnable {
-	Socket gpsdSocket;
-	BufferedReader gpsdReader;
-	boolean connected = false;
-	String gpsdHost;
-	int gpsdPort;
-	boolean shutdownFlag = false;
+	private String gpsdHost;
+	private int gpsdPort;
+
+	private Socket gpsdSocket;
+	private BufferedReader gpsdReader;
+	private boolean connected = false;
+	private boolean shutdownFlag = false;
+	private boolean JSONProtocol = true;
+
 	private final List<PropertyChangeListener> propertyChangeListener = new ArrayList<PropertyChangeListener>();
 	private PropertyChangeEvent lastStatusEvent;
 	private PropertyChangeEvent lastDataEvent;
@@ -111,124 +119,37 @@ public class LiveGpsAcquirer implements Runnable {
 
 		shutdownFlag = false;
 		while (!shutdownFlag) {
-			double lat = 0;
-			double lon = 0;
-			float speed = 0;
-			float course = 0;
-			boolean haveFix = false;
 
 			try {
-				if (!connected) {
-					System.out.println("LiveGps tries to connect to gpsd");
-					fireGpsStatusChangeEvent(
-							LiveGpsStatus.GpsStatus.CONNECTING,
-							tr("Connecting"));
-					InetAddress[] addrs = InetAddress.getAllByName(gpsdHost);
-					for (int i = 0; i < addrs.length && gpsdSocket == null; i++) {
-						try {
-							gpsdSocket = new Socket(addrs[i], gpsdPort);
-							break;
-						} catch (Exception e) {
-							System.out
-									.println("LiveGps: Could not open connection to gpsd: "
-											+ e);
-							gpsdSocket = null;
-						}
-					}
-
-					if (gpsdSocket != null) {
-						gpsdReader = new BufferedReader(new InputStreamReader(
-								gpsdSocket.getInputStream()));
-						gpsdSocket.getOutputStream().write(
-								new byte[] { 'w', 13, 10 });
-						fireGpsStatusChangeEvent(
-								LiveGpsStatus.GpsStatus.CONNECTING,
-								tr("Connecting"));
-						connected = true;
-						System.out.println("LiveGps: Connected to gpsd");
-					}
-				}
+				if (!connected)
+					connect();
 
 				if (connected) {
+					String line;
+
 					// <FIXXME date="23.06.2007" author="cdaller">
 					// TODO this read is blocking if gps is connected but has no
 					// fix, so gpsd does not send positions
-					String line = gpsdReader.readLine();
+					line = gpsdReader.readLine();
 					// </FIXXME>
 					if (line == null)
 						break;
-					String words[] = line.split(",");
 
-					if ((words.length == 0) || (!words[0].equals("GPSD"))) {
-						// unexpected response.
+					if (JSONProtocol == true)
+						gpsData = ParseJSON(line);
+					else
+						gpsData = ParseOld(line);
+
+					if (gpsData == null)
 						continue;
-					}
 
-					for (int i = 1; i < words.length; i++) {
-
-						if ((words[i].length() < 2)
-								|| (words[i].charAt(1) != '=')) {
-							// unexpected response.
-							continue;
-						}
-
-						char what = words[i].charAt(0);
-						String value = words[i].substring(2);
-						oldGpsData = gpsData;
-						gpsData = new LiveGpsData();
-						switch (what) {
-						case 'O':
-							// full report, tab delimited.
-							String[] status = value.split("\\s+");
-							if (status.length >= 5) {
-								lat = Double.parseDouble(status[3]);
-								lon = Double.parseDouble(status[4]);
-								try {
-									speed = Float.parseFloat(status[9]);
-									course = Float.parseFloat(status[8]);
-									// view.setSpeed(speed);
-									// view.setCourse(course);
-								} catch (NumberFormatException nex) {
-								}
-								haveFix = true;
-							}
-							break;
-						case 'P':
-							// position report, tab delimited.
-							String[] pos = value.split("\\s+");
-							if (pos.length >= 2) {
-								lat = Double.parseDouble(pos[0]);
-								lon = Double.parseDouble(pos[1]);
-								speed = Float.NaN;
-								course = Float.NaN;
-								haveFix = true;
-							}
-							break;
-						default:
-							// not interested
-						}
-						fireGpsStatusChangeEvent(
-								LiveGpsStatus.GpsStatus.CONNECTED,
-								tr("Connected"));
-						gpsData.setFix(haveFix);
-						if (haveFix) {
-							// view.setCurrentPosition(lat, lon);
-							gpsData.setLatLon(new LatLon(lat, lon));
-							gpsData.setSpeed(speed);
-							gpsData.setCourse(course);
-							fireGpsDataChangeEvent(oldGpsData, gpsData);
-						}
-					}
+					fireGpsDataChangeEvent(oldGpsData, gpsData);
+					oldGpsData = gpsData;
 				} else {
-					// not connected:
-					fireGpsStatusChangeEvent(
-							LiveGpsStatus.GpsStatus.DISCONNECTED,
-							tr("Not connected"));
+					fireGpsStatusChangeEvent(LiveGpsStatus.GpsStatus.DISCONNECTED, tr("Not connected"));
 					try {
 						Thread.sleep(1000);
-					} catch (InterruptedException ignore) {
-					}
-					;
+					} catch (InterruptedException ignore) {}
 				}
 			} catch (IOException iox) {
 				connected = false;
@@ -241,11 +162,8 @@ public class LiveGpsAcquirer implements Runnable {
 						tr("Connection Failed"));
 				try {
 					Thread.sleep(1000);
-				} catch (InterruptedException ignore) {
-				}
-				;
+				} catch (InterruptedException ignore) {} ;
 				// send warning to layer
-
 			}
 		}
 
@@ -257,8 +175,7 @@ public class LiveGpsAcquirer implements Runnable {
 				gpsdSocket = null;
 				System.out.println("LiveGps: Disconnected from gpsd");
 			} catch (Exception e) {
-				System.out
-						.println("LiveGps: Unable to close socket; reconnection may not be possible");
+				System.out.println("LiveGps: Unable to close socket; reconnection may not be possible");
 			}
 		}
 	}
@@ -267,4 +184,150 @@ public class LiveGpsAcquirer implements Runnable {
 		shutdownFlag = true;
 	}
 
+	private void connect() throws IOException {
+		JSONObject greeting;
+		String line, type, release;
+
+		System.out.println("LiveGps: trying to connect to gpsd at " + gpsdHost + ":" + gpsdPort);
+		fireGpsStatusChangeEvent( LiveGpsStatus.GpsStatus.CONNECTING, tr("Connecting"));
+
+		InetAddress[] addrs = InetAddress.getAllByName(gpsdHost);
+		for (int i = 0; i < addrs.length && gpsdSocket == null; i++) {
+			try {
+				gpsdSocket = new Socket(addrs[i], gpsdPort);
+				break;
+			} catch (Exception e) {
+				System.out.println("LiveGps: Could not open connection to gpsd: " + e);
+				gpsdSocket = null;
+			}
+		}
+
+		if (gpsdSocket == null)
+			return;
+
+		fireGpsStatusChangeEvent(LiveGpsStatus.GpsStatus.CONNECTING, tr("Connecting"));
+
+		/*
+		 * First emit the "w" symbol. The older version will activate, the newer one will ignore it.
+		 */
+		gpsdSocket.getOutputStream().write(new byte[] { 'w', 13, 10 });
+
+		gpsdReader = new BufferedReader(new InputStreamReader(gpsdSocket.getInputStream()));
+		line = gpsdReader.readLine();
+		if (line == null)
+			return;
+
+		try {
+			greeting = new JSONObject(line);
+			type = greeting.getString("class");
+			if (type.equals("VERSION")) {
+				release = greeting.getString("release");
+				System.out.println("LiveGps: Connected to gpsd " + release);
+			} else
+				System.out.println("LiveGps: Unexpected JSON in gpsd greeting: " + line);
+		} catch (JSONException jex) {
+			if (line.startsWith("GPSD,")) {
+				connected = true;
+				JSONProtocol = false;
+				System.out.println("LiveGps: Connected to old gpsd protocol version.");
+				fireGpsStatusChangeEvent(LiveGpsStatus.GpsStatus.CONNECTED, tr("Connected"));
+			}
+		}
+
+		if (JSONProtocol == true) {
+			JSONObject Watch = new JSONObject();
+			try { 
+				Watch.put("enable", true);
+				Watch.put("json", true);
+			} catch (JSONException je) {};
+
+			String Request = "?WATCH=" + Watch.toString() + ";\n";
+			gpsdSocket.getOutputStream().write(Request.getBytes());
+
+			connected = true;
+			fireGpsStatusChangeEvent(LiveGpsStatus.GpsStatus.CONNECTED, tr("Connected"));
+		}
+	}
+
+	private LiveGpsData ParseJSON(String line) {
+		JSONObject report;
+		String type;
+		double lat = 0;
+		double lon = 0;
+		float speed = 0;
+		float course = 0;
+
+		try {
+			report = new JSONObject(line);
+			type = report.getString("class");
+		} catch (JSONException jex) {
+			System.out.println("LiveGps: line read from gpsd is not a JSON object:" + line);
+			return null;
+		}
+		if (!type.equals("TPV"))
+			return null;
+
+		try {
+			lat = report.getDouble("lat");
+			lon = report.getDouble("lon");
+			speed = (new Float(report.getDouble("speed"))).floatValue();
+			course = (new Float(report.getDouble("track"))).floatValue();
+
+			return new LiveGpsData(lat, lon, course, speed, true);
+		} catch (JSONException je) {}
+
+		return null;
+	}
+
+	private LiveGpsData ParseOld(String line) {
+		String words[];
+		double lat = 0;
+		double lon = 0;
+		float speed = 0;
+		float course = 0;
+
+		words = line.split(",");
+		if ((words.length == 0) || (!words[0].equals("GPSD")))
+			return null;
+
+		for (int i = 1; i < words.length; i++) {
+			if ((words[i].length() < 2) || (words[i].charAt(1) != '=')) {
+				// unexpected response.
+				continue;
+			}
+
+			char what = words[i].charAt(0);
+			String value = words[i].substring(2);
+			switch (what) {
+			case 'O':
+				// full report, tab delimited.
+				String[] status = value.split("\\s+");
+				if (status.length >= 5) {
+					lat = Double.parseDouble(status[3]);
+					lon = Double.parseDouble(status[4]);
+					try {
+						speed = Float.parseFloat(status[9]);
+						course = Float.parseFloat(status[8]);
+					} catch (NumberFormatException nex) {}
+					return new LiveGpsData(lat, lon, course, speed, true);
+				}
+				break;
+			case 'P':
+				// position report, tab delimited.
+				String[] pos = value.split("\\s+");
+				if (pos.length >= 2) {
+					lat = Double.parseDouble(pos[0]);
+					lon = Double.parseDouble(pos[1]);
+					speed = Float.NaN;
+					course = Float.NaN;
+					return new LiveGpsData(lat, lon, course, speed, true);
+				}
+				break;
+			default:
+				// not interested
+			}
+		}
+
+		return null;
+	}
 }
