09cc1ceb62d80f0a0e63b3f1dc4b68a6e570903b
mzdila <mzdila@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-06-21 18:18:25 +0000

    Initial import
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@21870 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ simplifyarea/src/sk/zdila/josm/plugin/simplify/SimplifyAreaPlugin.java
@@ -0,0 +1,15 @@
+package sk.zdila.josm.plugin.simplify;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class SimplifyAreaPlugin extends Plugin {
+
+	public SimplifyAreaPlugin(final PluginInformation info) {
+		super(info);
+		MainMenu.add(Main.main.menu.toolsMenu, new SimplifyAreaAction());
+	}
+
+}
