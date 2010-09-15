eb6b36062a02c24d625b5d96ab206d79feb52200
postfix <postfix@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-09-15 14:01:55 +0000

    first framework for Seamap Editor (smed)
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@23178 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ smed/src/smed/plug/ifc/SmedPluggable.java
@@ -0,0 +1,15 @@
+package smed.plug.ifc;
+
+import javax.swing.JComponent;
+
+
+public interface SmedPluggable {
+
+	boolean start();
+	boolean start(JComponent panel);
+	boolean stop();
+	String getName();
+	
+	void setPluginManager(SmedPluginManager manager);
+}
+
---
+++ smed/src/smed/plug/ifc/SmedPluginManager.java
@@ -0,0 +1,5 @@
+package smed.plug.ifc;
+
+public interface SmedPluginManager {
+	void showVisualMessage(String message);
+}
