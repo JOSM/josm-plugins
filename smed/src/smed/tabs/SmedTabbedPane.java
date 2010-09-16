7ca9c0b664faf0be31720502952632956c893c33
stoecker <stoecker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-09-16 09:38:18 +0000

    fix last remaining tabs
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@23209 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- smed/src/smed/tabs/SmedTabbedPane.java
+++ smed/src/smed/tabs/SmedTabbedPane.java
@@ -23,30 +23,30 @@ public class SmedTabbedPane extends JPanel {
      */
     private static final long serialVersionUID = 1L;
 
-	public SmedTabbedPane() {
-		super(new GridLayout(1, 1));
-		
-		List<SmedPluggable> plugins = null;
-		String pluginDirName = Main.pref.getPluginsDirectory().getAbsolutePath();
-		try {
-			plugins = SmedPluginLoader.loadPlugins(new File(pluginDirName + "/splug"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+    public SmedTabbedPane() {
+        super(new GridLayout(1, 1));
+
+        List<SmedPluggable> plugins = null;
+        String pluginDirName = Main.pref.getPluginsDirectory().getAbsolutePath();
+        try {
+            plugins = SmedPluginLoader.loadPlugins(new File(pluginDirName + "/splug"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         Icon icon = null;
         JTabbedPane tabbedPane = new JTabbedPane();
 
-		JComponent panel;
-		int i = 0;
-		for(SmedPluggable p : plugins) {
-			panel = p.getComponent();
-			tabbedPane.addTab(p.getName(),icon, panel, p.getInfo());
-			tabbedPane.setMnemonicAt(i, KeyEvent.VK_1 + i);
-			
-			i++;
-		}
+        JComponent panel;
+        int i = 0;
+        for(SmedPluggable p : plugins) {
+            panel = p.getComponent();
+            tabbedPane.addTab(p.getName(),icon, panel, p.getInfo());
+            tabbedPane.setMnemonicAt(i, KeyEvent.VK_1 + i);
+
+            i++;
+        }
 
         //Add the tabbed pane to this panel.
         add(tabbedPane);
