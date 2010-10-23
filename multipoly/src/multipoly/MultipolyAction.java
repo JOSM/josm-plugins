5a69b2d75f544c43d7b431456251218af7c5d9c5
extropy <extropy@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-10-23 19:20:27 +0000

    Improved create multipolygon plugin. Can now handle polygons consisting of several unclosed ways, several outer ways and other stuff. Detects crossings. Opens relation editor.
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@23771 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- multipoly/src/multipoly/MultipolyAction.java
+++ multipoly/src/multipoly/MultipolyAction.java
@@ -5,26 +5,24 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Collections;
 
 import javax.swing.JOptionPane;
+
+import multipoly.Multipolygon.JoinedPolygon;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.command.AddCommand;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.SequenceCommand;
-import org.openstreetmap.josm.data.coor.EastNorth;
-import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.RelationMember;
 import org.openstreetmap.josm.data.osm.Way;
-import org.openstreetmap.josm.tools.Pair;
+import org.openstreetmap.josm.gui.dialogs.relation.RelationEditor;
+import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.tools.Shortcut;
 
 /**
@@ -59,110 +57,37 @@ public class MultipolyAction extends JosmAction {
      *            Action Event
      */
     public void actionPerformed(ActionEvent e) {
-
-        // Get all ways in some type=multipolygon relation
-        HashSet<OsmPrimitive> relationsInMulti = new HashSet<OsmPrimitive>();
-        for (Relation r : Main.main.getCurrentDataSet().getRelations()) {
-            if (!r.isUsable())
-                continue;
-            if (r.get("type") != "multipolygon")
-                continue;
-            for (RelationMember rm : r.getMembers()) {
-                OsmPrimitive m = rm.getMember();
-                if (m instanceof Way) {
-                    relationsInMulti.add(m);
-                }
-            }
-        }
-
-        // List of selected ways
-        List<Way> selectedWays = new ArrayList<Way>();
-        // Area of largest way (in square degrees)
-        double maxarea = 0;
-        // Which way is the largest one (outer)
-        Way maxWay = null;
-
-        // For every selected way
-        for (OsmPrimitive osm : Main.main.getCurrentDataSet().getSelected()) {
-            if (osm instanceof Way) {
-                Way way = (Way) osm;
-                // Check if way is already in another multipolygon
-                if (relationsInMulti.contains(osm)) {
-                    JOptionPane
-                            .showMessageDialog(
-                                    Main.parent,
-                                    tr("One of the selected ways is already part of another multipolygon."));
-                    return;
-                }
-                EastNorth first = null, last = null;
-                // Boundingbox of way
-                double minx = 9999, miny = 9999, maxx = -9999, maxy = -9999;
-                for (Pair<Node, Node> seg : way.getNodePairs(false)) {
-                    if (first == null)
-                        first = seg.a.getEastNorth();
-                    last = seg.b.getEastNorth();
-                    double x = seg.a.getEastNorth().east();
-                    double y = seg.a.getEastNorth().north();
-                    if (x < minx)
-                        minx = x;
-                    if (y < miny)
-                        miny = y;
-                    if (x > maxx)
-                        maxx = x;
-                    if (y > maxy)
-                        maxy = y;
-                }
-                // Check if first and last node are the same
-                if (!first.equals(last)) {
-                    JOptionPane
-                            .showMessageDialog(
-                                    Main.parent,
-                                    tr("Multipolygon must consist only of closed ways."));
-                    return;
-                }
-                // Determine area
-                double area = (maxx - minx) * (maxy - miny);
-                selectedWays.add(way);
-                if (area > maxarea) {
-                    maxarea = area;
-                    maxWay = way;
-                }
-            }
-        }
-
-        if (Main.map == null) {
+        if (Main.main.getEditLayer() == null) {
             JOptionPane.showMessageDialog(Main.parent, tr("No data loaded."));
             return;
         }
-
+       
+        Collection<Way> selectedWays = Main.main.getCurrentDataSet().getSelectedWays();
+              
         if (selectedWays.size() < 2) {
             JOptionPane.showMessageDialog(Main.parent,
                     tr("You must select at least two ways."));
             return;
         }
-
-        Collection<Command> cmds = new LinkedList<Command>();
-        // Create new relation
-        Relation rel = new Relation();
-        rel.put("type", "multipolygon");
-        // Add ways to it
-        for (int i = 0; i < selectedWays.size(); i++) {
-            Way s = selectedWays.get(i);
-            String xrole = "inner";
-            if (s == maxWay)
-                xrole = "outer";
-            RelationMember rm = new RelationMember(xrole, s);
-            rel.addMember(rm);
+        
+        Multipolygon polygon = this.analyzeWays(selectedWays);
+        
+        if (polygon == null) {
+        	return; //could not make multipolygon.
         }
-        // Add relation
-        cmds.add(new AddCommand(rel));
-        // Commit
-        Main.main.undoRedo.add(new SequenceCommand(tr("Create multipolygon"),
-                cmds));
-        Main.map.repaint();
-    }
+        
+        Relation relation = this.createRelation(polygon); 
 
-    /** Enable this action only if something is selected */
+        //open relation edit window
+        RelationEditor editor = RelationEditor.getEditor(
+        		Main.main.getEditLayer(), 
+        		relation, 
+        		null);
+        
+        editor.setVisible(true);
+    }  
+
+	/** Enable this action only if something is selected */
     @Override
     protected void updateEnabledState() {
         if (getCurrentDataSet() == null) {
@@ -178,4 +103,50 @@ public class MultipolyAction extends JosmAction {
             Collection<? extends OsmPrimitive> selection) {
         setEnabled(selection != null && !selection.isEmpty());
     }
+    
+    
+    /**
+     * This method analyzes ways and creates multipolygon.
+     * @param selectedWays
+     * @return null, if there was a problem with the ways.
+     */
+    private Multipolygon analyzeWays(Collection<Way> selectedWays) {
+    	
+    	Multipolygon pol = new Multipolygon();    	
+    	String error = pol.makeFromWays(selectedWays);
+    	
+    	if (error != null) {
+    		JOptionPane.showMessageDialog(Main.parent, error);
+    		return null;    		
+    	}
+    	else {
+    		return pol;
+    	}
+	}    
+    
+    
+    /**
+     * Builds a relation from polygon ways. 
+     * @param pol 
+     * @return
+     */
+    private Relation createRelation(Multipolygon pol){
+           // Create new relation
+           Relation rel = new Relation();
+           rel.put("type", "multipolygon");           
+           // Add ways to it
+           for(JoinedPolygon jway: pol.outerWays){
+        	   for(Way way: jway.ways){
+        		   rel.addMember(new RelationMember("outer", way));
+        	   }
+           }
+           
+           for(JoinedPolygon jway: pol.innerWays){
+        	   for(Way way: jway.ways){
+        		   rel.addMember(new RelationMember("inner", way));
+        	   }
+           }
+           
+           return rel;
+    }    
 }
