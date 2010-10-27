92b457d77478bab39681683c8f78303882f14872
postfix <postfix@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-10-27 20:25:20 +0000

    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@23882 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ smed_fw/src/smed_fw/SmedFW.java
@@ -0,0 +1,14 @@
+package smed_fw;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class SmedFW extends Plugin {
+
+	public SmedFW(PluginInformation info) {
+		super(info);
+		
+		System.out.println("SmedFW (OSGi-Implementation) noch nicht weiter programmiert");
+	}
+
+}
