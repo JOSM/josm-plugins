bb4527d326912481066b8094120f95163e667b8e
postfix <postfix@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-11-01 10:52:23 +0000

    Aufsetzen OSGi-Framework
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@23986 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ smed_bfw/smed_bfw/test/smed_bfw/internal/ManagerImplTest.java
@@ -0,0 +1,9 @@
+package smed_bfw.internal;
+
+import junit.framework.TestCase;
+
+public class ManagerImplTest extends TestCase {
+	public void testTabedPane() {
+		 assertNotNull(null, new MangerImpl().getTabbedPane());
+	}
+}
