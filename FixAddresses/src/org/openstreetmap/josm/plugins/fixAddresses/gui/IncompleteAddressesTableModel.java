73f7ee56c45c2e7be50ecc03074d04823b3d129a
oliverw <oliverw@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-11-02 18:00:54 +0000

    Fixed imports.
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@24025 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- FixAddresses/src/org/openstreetmap/josm/plugins/fixAddresses/gui/IncompleteAddressesTableModel.java
+++ FixAddresses/src/org/openstreetmap/josm/plugins/fixAddresses/gui/IncompleteAddressesTableModel.java
@@ -15,14 +15,9 @@ package org.openstreetmap.josm.plugins.fixAddresses.gui;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.openstreetmap.josm.plugins.fixAddresses.AddressEditContainer;
 import org.openstreetmap.josm.plugins.fixAddresses.AddressNode;
 import org.openstreetmap.josm.plugins.fixAddresses.INodeEntity;
-import org.openstreetmap.josm.plugins.fixAddresses.IProgressMonitorFinishedListener;
-import org.openstreetmap.josm.plugins.fixAddresses.StringUtils;
 
 public class IncompleteAddressesTableModel extends AddressEditTableModel  {
 	/**
