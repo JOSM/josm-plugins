e2702d53c040e05560a42d3287f117962321a8bc
glebius <glebius@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2011-04-06 22:03:40 +0000

    New autocentering technique, based on position of cursor on the map. User configures the tunable:
    livegps.center_factor=80 (80% - default value)
    
    It represents a value (in percent) of a rectangular screen
    area where the cursor should be present. If cursor leaves the
    area, centering is performed.
    
    Time based centering can now be turned off by default with:
    
    livegps.center_interval_msec=0
    
    But is turned on by default for compatibility.
    
    
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@25809 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- livegps/src/livegps/LiveGpsLayer.java
+++ livegps/src/livegps/LiveGpsLayer.java
@@ -5,6 +5,8 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.awt.Rectangle;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.text.SimpleDateFormat;
@@ -27,11 +29,14 @@ public class LiveGpsLayer extends GpxLayer implements PropertyChangeListener {
 
     private static final int DEFAULT_REFRESH_INTERVAL = 250;
     private static final int DEFAULT_CENTER_INTERVAL = 5000;
+    private static final int DEFAULT_CENTER_FACTOR = 80;
     private static final String oldC_REFRESH_INTERVAL = "livegps.refreshinterval";     /* in seconds */
     private static final String C_REFRESH_INTERVAL = "livegps.refresh_interval_msec";  /* in msec */
     private static final String C_CENTER_INTERVAL = "livegps.center_interval_msec";  /* in msec */
+    private static final String C_CENTER_FACTOR = "livegps.center_factor" /* in percent */;
     private int refreshInterval;
     private int centerInterval;
+    private double centerFactor;
     private long lastRedraw = 0;
     private long lastCenter = 0;
 
@@ -68,13 +73,27 @@ public class LiveGpsLayer extends GpxLayer implements PropertyChangeListener {
         lastPoint = new WayPoint(thisPos);
         lastPoint.attr.put("time", dateFormat.format(new Date()));
         trackSegment.addWaypoint(lastPoint);
-        if (autocenter && allowCenter())
-            center();
+
+	conditionalCenter(thisPos);
     }
 
     public void center() {
         if (lastPoint != null)
             Main.map.mapView.zoomTo(lastPoint.getCoor());
+    }
+
+    public void conditionalCenter(LatLon Pos) {
+	Point2D P = Main.map.mapView.getPoint2D(Pos);
+	Rectangle rv = Main.map.mapView.getBounds(null);
+	Date date = new Date();
+	long current = date.getTime();
+
+	rv.grow(-(int)(rv.getHeight() * centerFactor), -(int)(rv.getWidth() * centerFactor));
+
+	if (!rv.contains(P) || (centerInterval > 0 && current - lastCenter >= centerInterval)) {
+		Main.map.mapView.zoomTo(Pos);
+		lastCenter = current;
+	}
     }
 
     // void setStatus(String status)
@@ -169,40 +188,32 @@ public class LiveGpsLayer extends GpxLayer implements PropertyChangeListener {
     }
 
     /**
-     * Check, if a autocentering is currently allowed.
-     *
-     * @return true, if a autocentering is permitted, false, if a autocentering
-     * should be suppressed.
-     */
-    private boolean allowCenter() {
-	Date date = new Date();
-	long current = date.getTime();
-
-	if (current - lastCenter >= centerInterval) {
-		lastCenter = current;
-		return true;
-	} else
-		return false;
-    }
-
-    /**
      * Retrieve the refreshInterval and centerInterval from the configuration. Be compatible
      * with old version that stored refreshInterval in seconds. If no such configuration key
      * exists, it will be initialized here.
      */
     private void initIntervals() {
-        if ((refreshInterval = Main.pref.getInteger(C_REFRESH_INTERVAL, 0)) == 0) {
-                if ((refreshInterval = Main.pref.getInteger(oldC_REFRESH_INTERVAL, 0)) != 0) {
-                        refreshInterval *= 1000;
-                        Main.pref.put(oldC_REFRESH_INTERVAL, null);
-                } else
-                        refreshInterval = DEFAULT_REFRESH_INTERVAL;
-        }
+	if ((refreshInterval = Main.pref.getInteger(oldC_REFRESH_INTERVAL, 0)) != 0) {
+		refreshInterval *= 1000;
+		Main.pref.put(oldC_REFRESH_INTERVAL, null);
+	} else
+		refreshInterval = Main.pref.getInteger(C_REFRESH_INTERVAL, DEFAULT_REFRESH_INTERVAL);
 
-	if ((centerInterval = Main.pref.getInteger(C_CENTER_INTERVAL, 0)) == 0)
-		centerInterval = DEFAULT_CENTER_INTERVAL;
+	centerInterval = Main.pref.getInteger(C_CENTER_INTERVAL, DEFAULT_CENTER_INTERVAL);
+	centerFactor = Main.pref.getInteger(C_CENTER_FACTOR, DEFAULT_CENTER_FACTOR);
+	if (centerFactor <= 1 || centerFactor >= 99)
+		centerFactor = DEFAULT_CENTER_FACTOR;
 
         Main.pref.putInteger(C_REFRESH_INTERVAL, refreshInterval);
         Main.pref.putInteger(C_CENTER_INTERVAL, centerInterval);
+	Main.pref.putInteger(C_CENTER_FACTOR, (int )centerFactor);
+
+	/*
+	 * Do one time conversion of factor: user value means "how big is inner rectangle
+	 * comparing to screen in percent", machine value means "what is the shrink ratio
+	 * for each dimension on _both_ sides".
+	 */
+
+	centerFactor = (100 - centerFactor) / 2 / 100;
     }
 }
