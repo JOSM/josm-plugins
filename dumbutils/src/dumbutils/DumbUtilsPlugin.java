43aac71804e1af14397e48d4e1f78ac3f0c5094b
zverik <zverik@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2011-04-20 17:46:40 +0000

    warning about utilsplugin2
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@25875 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- dumbutils/src/dumbutils/DumbUtilsPlugin.java
+++ dumbutils/src/dumbutils/DumbUtilsPlugin.java
@@ -1,6 +1,7 @@
 package dumbutils;
 
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.MainMenu;
@@ -23,6 +24,13 @@ public class DumbUtilsPlugin extends Plugin {
         sourceTag = MainMenu.add(Main.main.menu.toolsMenu, new TagSourceAction());
         pasteRelations = MainMenu.add(Main.main.menu.toolsMenu, new PasteRelationsAction());
         alignWayNodes = MainMenu.add(Main.main.menu.toolsMenu, new AlignWayNodesAction());
+
+        if( Main.pref.getBoolean("dumbutils.up2warning", true) ) {
+            JOptionPane.showMessageDialog(Main.parent,
+                    "DumbUtils plugin functionality was moved to UtilsPlugin2.\nThis plugin is no longer supported.",
+                    "DumbUtils Plugin", JOptionPane.WARNING_MESSAGE);
+            Main.pref.put("dumbutils.up2warning", false);
+        }
     }
 
     @Override
