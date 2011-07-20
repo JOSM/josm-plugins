544c3f84eec5a4e350e033ec84c8d19949f9865e
frederik <frederik@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2011-07-20 23:54:33 +0000

    (Hopefully) fix the "failed to locate image" bug on license change plugin
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@26377 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- licensechange/src/org/openstreetmap/josm/plugins/licensechange/Severity.java
+++ licensechange/src/org/openstreetmap/josm/plugins/licensechange/Severity.java
@@ -12,15 +12,15 @@ import org.openstreetmap.josm.Main;
 public enum Severity {
 
     /** Error messages */
-    DATA_LOSS(tr("Data loss"), "error.gif",                     
+    DATA_LOSS(tr("Data loss"), "error",                     
         Main.pref.getColor(marktr("license check error"), Color.RED)),
 
     /** Warning messages */
-    POSSIBLE_DATA_LOSS(tr("Possible data loss"), "warning.gif", 
+    POSSIBLE_DATA_LOSS(tr("Possible data loss"), "warning", 
         Main.pref.getColor(marktr("license check warning"), Color.ORANGE)),
 
     /** Other messages */
-    DATA_REDUCTION(tr("Data reduction"), "other.gif",           
+    DATA_REDUCTION(tr("Data reduction"), "other",           
         Main.pref.getColor(marktr("license check notice"), Color.YELLOW));
 
     /** Description of the severity code */
