ef8ba3c80957eea9e6a9bbbf537ca5713497c3a1
bastik <bastik@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2011-07-24 22:51:59 +0000

    minor update
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@26391 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- native-password-manager/src/org/openstreetmap/josm/plugins/npm/InitializationWizard.java
+++ native-password-manager/src/org/openstreetmap/josm/plugins/npm/InitializationWizard.java
@@ -320,7 +320,7 @@ public class InitializationWizard extends JDialog {
             HtmlPanel l = new HtmlPanel();
             l.setText("<html><b>"+tr("Found sensitive data that is still saved"
                     + " in JOSM''s preference file (plain text).")+"<b></html>");
-            rbClear = new JRadioButton("<html>"+tr("Erase and transfer to password manager")+"</html>");
+            rbClear = new JRadioButton("<html>"+tr("Transfer to password manager and remove from prefence file")+"</html>");
             rbKeep = new JRadioButton("<html>"+tr("No, just keep it")+"</html>");
             rbClear.setSelected(true);
             ButtonGroup group = new ButtonGroup();
