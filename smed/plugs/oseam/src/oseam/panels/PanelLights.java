4df49df474686b8d3eae02df55ffe506f3b74db1
malcolmh <malcolmh@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2011-08-24 18:25:10 +0000

    save
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@26569 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- smed/plugs/oseam/src/oseam/panels/PanelLights.java
+++ smed/plugs/oseam/src/oseam/panels/PanelLights.java
@@ -2,7 +2,9 @@ package oseam.panels;
 
 import java.awt.event.*;
 import java.awt.*;
+
 import javax.swing.*;
+
 import java.util.*;
 
 import oseam.Messages;
@@ -12,6 +14,21 @@ import oseam.seamarks.SeaMark.*;
 public class PanelLights extends JPanel {
 
 	private OSeaMAction dlg;
+	
+	public JLabel categoryLabel;
+
+	public JComboBox trafficCatBox;
+	private ActionListener alTrafficCatBox = new ActionListener() {
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+		}
+	};
+
+	public JComboBox warningCatBox;
+	private ActionListener alWarningCatBox = new ActionListener() {
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+		}
+	};
+
 	private ButtonGroup objButtons = new ButtonGroup();
 	public JRadioButton houseButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/LighthouseButton.png")));
 	public JRadioButton majorButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/LightMajorButton.png")));
@@ -44,6 +61,23 @@ public class PanelLights extends JPanel {
 		this.add(getObjButton(floatButton, 140, 0, 34, 32, "LightFloat", Obj.LITFLT), null);
 		this.add(getObjButton(trafficButton, 50, 35, 34, 32, "SSTraffic", Obj.SISTAT), null);
 		this.add(getObjButton(warningButton, 90, 35, 34, 32, "SSWarning", Obj.SISTAW), null);
+
+		categoryLabel = new JLabel(Messages.getString("Category"), SwingConstants.CENTER);
+		categoryLabel.setBounds(new Rectangle(20, 80, 140, 20));
+		this.add(categoryLabel, null);
+		
+		trafficCatBox = new JComboBox();
+		trafficCatBox.setBounds(new Rectangle(20, 110, 140, 20));
+		this.add(trafficCatBox, null);
+		trafficCatBox.addActionListener(alTrafficCatBox);
+		trafficCatBox.addItem(Messages.getString("NoneSpecified"));
+
+		warningCatBox = new JComboBox();
+		warningCatBox.setBounds(new Rectangle(20, 110, 140, 20));
+//		this.add(warningCatBox, null);
+		warningCatBox.addActionListener(alWarningCatBox);
+		warningCatBox.addItem(Messages.getString("NoneSpecified"));
+
 	}
 
 	public void clearSelections() {
