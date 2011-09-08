043bcc55bea30d1c61f453ce127027b261c78538
malcolmh <malcolmh@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2011-09-08 14:43:20 +0000

    save
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@26632 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ smed/plugs/oseam/src/oseam/panels/PanelPat.java
@@ -0,0 +1,73 @@
+package oseam.panels;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import java.util.*;
+
+import oseam.Messages;
+import oseam.dialogs.OSeaMAction;
+import oseam.seamarks.SeaMark.*	;
+
+public class PanelPat extends JPanel {
+
+	private OSeaMAction dlg;
+	public PanelCol panelCol;
+	private ActionListener alType;
+
+	private ButtonGroup patButtons = new ButtonGroup();
+	public JRadioButton noneButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/NoneButton.png")));
+	public JRadioButton horizButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/HorizontalButton.png")));
+	public JRadioButton vertButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/VerticalButton.png")));
+	public JRadioButton diagButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/DiagonalButton.png")));
+	public JRadioButton squareButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/SquaredButton.png")));
+	public JRadioButton borderButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/BorderButton.png")));
+	public EnumMap<Pat, JRadioButton> patterns = new EnumMap<Pat, JRadioButton>(Pat.class);
+	private ActionListener alPat = new ActionListener() {
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+			for (Pat pat : patterns.keySet()) {
+				JRadioButton button = patterns.get(pat);
+				if (button.isSelected()) {
+					dlg.mark.setPattern(Ent.BODY, pat);
+					button.setBorderPainted(true);
+				} else
+					button.setBorderPainted(false);
+			}
+			if (dlg.mark != null)
+				dlg.mark.paintSign();
+		}
+	};
+
+	public PanelPat(OSeaMAction dia) {
+		dlg = dia;
+		this.setLayout(null);
+		panelCol = new PanelCol(dlg, alType, Ent.BODY);
+		panelCol.setBounds(new Rectangle(0, 0, 68, 160));
+		this.add(panelCol, null);
+		this.add(getPatButton(noneButton, 72, 0, 27, 27, "NoPat", Pat.NONE), null);
+		this.add(getPatButton(horizButton, 72, 26, 27, 27, "HorizPat", Pat.HORIZ), null);
+		this.add(getPatButton(vertButton, 72, 52, 27, 27, "VertPat", Pat.VERT), null);
+		this.add(getPatButton(diagButton, 72, 78, 27, 27, "DiagPat", Pat.DIAG), null);
+		this.add(getPatButton(squareButton, 72, 104, 27, 27, "SquarePat", Pat.SQUARE), null);
+		this.add(getPatButton(borderButton, 72, 130, 27, 27, "BorderPat", Pat.BORDER), null);
+
+	}
+
+	public void clearSelections() {
+		patButtons.clearSelection();
+		alPat.actionPerformed(null);
+	}
+
+	private JRadioButton getPatButton(JRadioButton button, int x, int y, int w, int h, String tip, Pat pat) {
+		button.setBounds(new Rectangle(x, y, w, h));
+		button.setBorder(BorderFactory.createLineBorder(Color.magenta, 2));
+		button.setToolTipText(Messages.getString(tip));
+		button.addActionListener(alPat);
+		patButtons.add(button);
+		patterns.put(pat, button);
+		return button;
+	}
+
+}
