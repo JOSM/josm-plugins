013d780943e85af023b714376938caaa5a2b7db2
postfix <postfix@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2011-09-18 18:26:31 +0000

    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@26671 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- smed/plugs/harbour/src/harbour/panels/PanelEnv.java
+++ smed/plugs/harbour/src/harbour/panels/PanelEnv.java
@@ -29,7 +29,7 @@ public class PanelEnv extends JPanel {
 	private void initialize() {
 		envLabel = new JLabel();
 		envLabel.setBounds(new Rectangle(126, 17, 99, 46));
-		envLabel.setText("Environment");
+		envLabel.setText("nvironment");
 		
 		this.setSize(330, 270);
 		this.setLayout(null);
