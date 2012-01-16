a8651408c2e376d75136aeaaf6b9830afb3490b3
malcolmh <malcolmh@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-01-16 15:07:15 +0000

    save
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@27477 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ smed/plugs/wayarea/src/wayarea/dialogs/WayAreaAction.java
@@ -0,0 +1,78 @@
+package wayarea.dialogs;
+
+import wayarea.panels.*;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.*;
+import org.openstreetmap.josm.data.osm.*;
+
+import wayarea.Messages;
+import smed.plug.ifc.SmedPluginManager;
+
+public class WayAreaAction {
+
+	private WayAreaAction dlg = null;
+	public SmedPluginManager manager = null;;
+	public PanelMain panelMain = null;
+
+	public Node node = null;
+	private Collection<? extends OsmPrimitive> Selection = null;
+
+	public SelectionChangedListener SmpListener = new SelectionChangedListener() {
+		public void selectionChanged(Collection<? extends OsmPrimitive> newSelection) {
+			Node nextNode = null;
+			Selection = newSelection;
+
+			for (OsmPrimitive osm : Selection) {
+				if (osm instanceof Node) {
+					nextNode = (Node) osm;
+					if (Selection.size() == 1) {
+						if (nextNode.compareTo(node) != 0) {
+							node = nextNode;
+//							panelMain.mark.parseMark(node);
+						}
+					} else {
+						node = null;
+//						panelMain.mark.clrMark();
+						manager.showVisualMessage(Messages.getString("OneNode"));
+					}
+				}
+			}
+			if (nextNode == null) {
+				node = null;
+//				panelMain.mark.clrMark();
+				manager.showVisualMessage(Messages.getString("SelectNode"));
+			}
+		}
+	};
+
+	public WayAreaAction(SmedPluginManager mngr) {
+
+		// System.out.println("hello");
+		dlg = this;
+		manager = mngr;
+		DataSet.addSelectionListener(SmpListener);
+		String str = Main.pref.get("mappaint.style.sources");
+		if (!str.contains("dev.openseamap.org")) {
+			if (!str.isEmpty())
+				str += new String(new char[] { 0x1e });
+			Main.pref.put("mappaint.style.sources", str + "http://dev.openseamap.org/josm/seamark_styles.xml");
+		}
+	}
+
+	public JPanel getWayAreaPanel() {
+		if (panelMain == null) {
+			panelMain = new PanelMain(this);
+			panelMain.setLayout(null);
+			panelMain.setSize(new Dimension(400, 360));
+			node = null;
+			panelMain.syncPanel();
+		}
+		return panelMain;
+	}
+
+}
---
+++ smed/plugs/wayarea/src/wayarea/panels/PanelMain.java
@@ -0,0 +1,105 @@
+package wayarea.panels;
+
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import wayarea.Messages;
+import wayarea.dialogs.WayAreaAction;
+
+public class PanelMain extends JPanel {
+
+	private WayAreaAction dlg;
+	public JButton saveButton = null;
+	private ActionListener alSave = new ActionListener() {
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+//			mark.saveSign(dlg.node);
+		}
+	};
+	public ButtonGroup typeButtons = null;
+	public JRadioButton fButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/ChanButton.png")));
+	public JRadioButton jButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/HazButton.png")));
+	public JRadioButton kButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/SpecButton.png")));
+	public JRadioButton lButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/LightsButton.png")));
+	public JRadioButton mButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/LightsButton.png")));
+	public JRadioButton nButton = new JRadioButton(new ImageIcon(getClass().getResource("/images/LightsButton.png")));
+	private ActionListener alType = new ActionListener() {
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+			if (fButton.isSelected()) {
+				fButton.setBorderPainted(true);
+//				panelF.syncPanel();
+//				panelF.setVisible(true);
+			} else {
+				fButton.setBorderPainted(false);
+//				panelF.setVisible(false);
+			}
+			if (jButton.isSelected()) {
+				jButton.setBorderPainted(true);
+//				panelJ.syncPanel();
+//				panelJ.setVisible(true);
+			} else {
+				jButton.setBorderPainted(false);
+//				panelJ.setVisible(false);
+			}
+			if (kButton.isSelected()) {
+				kButton.setBorderPainted(true);
+//				panelK.syncPanel();
+//				panelK.setVisible(true);
+			} else {
+				kButton.setBorderPainted(false);
+//				panelK.setVisible(false);
+			}
+			if (lButton.isSelected()) {
+				lButton.setBorderPainted(true);
+//				panelL.syncPanel();
+//				panelL.setVisible(true);
+			} else {
+				lButton.setBorderPainted(false);
+//				panelL.setVisible(false);
+			}
+			if (mButton.isSelected()) {
+				mButton.setBorderPainted(true);
+//				panelM.syncPanel();
+//				panelM.setVisible(true);
+			} else {
+				mButton.setBorderPainted(false);
+//				panelM.setVisible(false);
+			}
+			if (nButton.isSelected()) {
+				nButton.setBorderPainted(true);
+//				panelN.syncPanel();
+//				panelN.setVisible(true);
+			} else {
+				nButton.setBorderPainted(false);
+//				panelN.setVisible(false);
+			}
+		}
+	};
+
+	public PanelMain(WayAreaAction dia) {
+
+		dlg = dia;
+		setLayout(null);
+
+	}
+
+	public void syncPanel() {
+		typeButtons.clearSelection();
+		fButton.setBorderPainted(false);
+		jButton.setEnabled(false);
+		kButton.setBorderPainted(false);
+		lButton.setEnabled(false);
+		mButton.setBorderPainted(false);
+		nButton.setEnabled(false);
+	}
+
+	private JRadioButton getButton(JRadioButton button, int x, int y, int w, int h, String tip) {
+		button.setBounds(new Rectangle(x, y, w, h));
+		button.setBorder(BorderFactory.createLoweredBevelBorder());
+		button.setToolTipText(Messages.getString(tip));
+		return button;
+	}
+
+}
