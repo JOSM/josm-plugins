b3c6bb87b67c08b569a03d70900e28e618923df0
roland <roland@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-02-02 19:11:10 +0000

    Removed toolbar action name to avoid conflict.
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@27694 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- mirrored_download/src/mirrored_download/DownloadAction2.java
+++ mirrored_download/src/mirrored_download/DownloadAction2.java
@@ -27,7 +27,7 @@ import org.openstreetmap.josm.tools.Shortcut;
  */
 public class DownloadAction2 extends JosmAction {
     public DownloadAction2() {
-        super(tr("Download from OSM mirror..."), "download", tr("Download map data from the OSM server."),
+        super(tr("Download from OSM mirror..."), null, tr("Download map data from the OSM server."),
                 Shortcut.registerShortcut("mirror:download", tr("File: {0}", tr("Download from OSM mirror...")), KeyEvent.VK_D, Shortcut.GROUPS_ALT1+Shortcut.GROUP_HOTKEY), true);
         putValue("help", ht("/Action/MirroredDownload"));
     }
