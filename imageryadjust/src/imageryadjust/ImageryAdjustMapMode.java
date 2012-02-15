be87e7f93924915314c4e970a237de24483e36bf
stoecker <stoecker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-02-15 20:56:08 +0000

    fix shortcut conflicts
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@27769 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- imageryadjust/src/imageryadjust/ImageryAdjustMapMode.java
+++ imageryadjust/src/imageryadjust/ImageryAdjustMapMode.java
@@ -35,7 +35,8 @@ public class ImageryAdjustMapMode extends MapMode implements MouseListener, Mous
     public ImageryAdjustMapMode(MapFrame mapFrame) {
         super(tr("Adjust imagery"), "adjustimg",
                 tr("Adjust the position of the selected imagery layer"), 
-                Shortcut.registerShortcut("imageryadjust:adjustmode", tr("Mode: {0}", tr("Adjust imagery")), KeyEvent.VK_Y, Shortcut.GROUP_EDIT),
+                Shortcut.registerShortcut("imageryadjust:adjustmode", tr("Mode: {0}", tr("Adjust imagery")),
+                KeyEvent.VK_Y, Shortcut.GROUP_DIRECT2+Shortcut.GROUPS_ALT2),
                 mapFrame,
                 ImageProvider.getCursor("normal", "move"));
         MapFrame.addMapModeChangeListener(this);
