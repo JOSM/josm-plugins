ffe613ee846ecffc6e55a12d7ff26710b59e6a35
stoecker <stoecker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-02-16 14:32:50 +0000

    fix shortcut conflict
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@27796 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- multipoly-convert/src/converttomultipoly/MultipolyAction.java
+++ multipoly-convert/src/converttomultipoly/MultipolyAction.java
@@ -36,9 +36,10 @@ public class MultipolyAction extends JosmAction {
 
     public MultipolyAction() {
         super(tr("Convert to multipolygon"), "multipoly_convert",
-                tr("Convert to multipolygon."), 
-			Shortcut.registerShortcut("tools:multipolyconv", tr("Tool: {0}",tr("Convert to multipolygon")), KeyEvent.VK_M,
-                        Shortcut.GROUP_EDIT, Shortcut.SHIFT_DEFAULT), true);
+            tr("Convert to multipolygon."),
+                Shortcut.registerShortcut("tools:multipolyconv", tr("Tool: {0}",
+                    tr("Convert to multipolygon")), KeyEvent.VK_M,
+                    Shortcut.GROUP_DIRECT3+Shortcut.GROUPS_ALT1), true);
     }
 
     /**
