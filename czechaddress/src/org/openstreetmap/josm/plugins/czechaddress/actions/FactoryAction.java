862ee416e5a211dbebe1392e0677a33537bea1d5
stoecker <stoecker@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-02-16 20:47:52 +0000

    fix one shortcut conflict
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@27814 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- czechaddress/src/org/openstreetmap/josm/plugins/czechaddress/actions/FactoryAction.java
+++ czechaddress/src/org/openstreetmap/josm/plugins/czechaddress/actions/FactoryAction.java
@@ -39,7 +39,7 @@ public class FactoryAction extends MapMode {
               "Vytváří adresní body jedním kliknutím",
               Shortcut.registerShortcut( "mapmode:clickaddress",
                   "Sputit továrnu na adresy",
-                  KeyEvent.VK_F, Shortcut.GROUP_EDIT),
+                  KeyEvent.VK_F, Shortcut.GROUP_EDIT+Shortcut.GROUPS_ALT2),
               frame,
               ImageProvider.getCursor("crosshair", "envelope-star-small"));
     }
