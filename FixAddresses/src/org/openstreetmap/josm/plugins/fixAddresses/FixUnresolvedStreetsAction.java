cf04e53f3511ead501aee24d65277bc596904445
akks <akks@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-02-17 14:27:18 +0000

    typo
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@27825 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- FixAddresses/src/org/openstreetmap/josm/plugins/fixAddresses/FixUnresolvedStreetsAction.java
+++ FixAddresses/src/org/openstreetmap/josm/plugins/fixAddresses/FixUnresolvedStreetsAction.java
@@ -30,7 +30,7 @@ public class FixUnresolvedStreetsAction extends JosmAction implements SelectionC
 		super(tr("Fix street addresses"), "fixstreets_24",
 				tr("Find and fix addresses without (valid) streets."),
 				Shortcut.registerShortcut("tools:AddressEdit", tr("Tool: {0}",
-				tr("Address Edit")), KeyEvent.VK_X, Shortcut.GROUP_MENU), false);
+				tr("Address Edit")), KeyEvent.VK_X, Shortcut.GROUPS_ALT1+Shortcut.GROUP_MENU), false);
 
 		setEnabled(false);
 		addressEditContainer = new AddressEditContainer();
