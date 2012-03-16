cf3d1a63941282b0245153f3749266ee302fa3f2
simon04 <simon04@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-03-16 21:50:32 +0000

    [mirrored_download] More intuitive position of menu entries
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@28106 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- mirrored_download/src/mirrored_download/MirroredDownloadPlugin.java
+++ mirrored_download/src/mirrored_download/MirroredDownloadPlugin.java
@@ -1,43 +1,28 @@
 package mirrored_download;
 
-import static org.openstreetmap.josm.tools.I18n.marktr;
-
-import java.awt.event.KeyEvent;
-
 import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 
 import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.actions.JosmAction;
-import org.openstreetmap.josm.data.ProjectionBounds;
-import org.openstreetmap.josm.gui.IconToggleButton;
 import org.openstreetmap.josm.gui.MainMenu;
-import org.openstreetmap.josm.gui.MapFrame;
-import org.openstreetmap.josm.gui.MapView;
-import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
-import org.openstreetmap.josm.io.CacheFiles;
-import org.openstreetmap.josm.io.MirroredInputStream;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class MirroredDownloadPlugin extends Plugin {
 
-  static JMenu jMenu;
+    static JMenu jMenu;
 
-  public MirroredDownloadPlugin(PluginInformation info)
-  {
-    super(info);
-    MainMenu.add(Main.main.menu.fileMenu, new DownloadAction2());
-    MainMenu.add(Main.main.menu.fileMenu, new UrlSelectionAction());
-  }
+    public MirroredDownloadPlugin(PluginInformation info) {
+        super(info);
+        MainMenu.addAfter(Main.main.menu.fileMenu, new DownloadAction2(), false, Main.main.menu.download);
+        MainMenu.add(Main.main.menu.editMenu, new UrlSelectionAction());
+    }
+    private static String downloadUrl = "http://overpass.osm.rambler.ru/cgi/xapi?";//"http://overpass-api.de/api/xapi?";
 
-  private static String downloadUrl = "http://overpass.osm.rambler.ru/cgi/xapi?";//"http://overpass-api.de/api/xapi?";
+    public static String getDownloadUrl() {
+        return downloadUrl;
+    }
 
-  public static String getDownloadUrl() {
-    return downloadUrl;
-  }
-
-  public static void setDownloadUrl(String downloadUrl_) {
-    downloadUrl = downloadUrl_;
-  }
+    public static void setDownloadUrl(String downloadUrl_) {
+        downloadUrl = downloadUrl_;
+    }
 }
--- mirrored_download/src/mirrored_download/UrlSelectionAction.java
+++ mirrored_download/src/mirrored_download/UrlSelectionAction.java
@@ -5,17 +5,9 @@ import static org.openstreetmap.josm.gui.help.HelpUtil.ht;
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.util.concurrent.Future;
 
 import org.openstreetmap.josm.actions.JosmAction;
 
-import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.actions.downloadtasks.DownloadGpsTask;
-import org.openstreetmap.josm.actions.downloadtasks.PostDownloadHandler;
-import org.openstreetmap.josm.data.Bounds;
-import org.openstreetmap.josm.gui.download.DownloadDialog;
-import org.openstreetmap.josm.tools.Shortcut;
 
 /**
  * TODO: Write comment.
@@ -23,9 +15,8 @@ import org.openstreetmap.josm.tools.Shortcut;
 public class UrlSelectionAction extends JosmAction {
 
     public UrlSelectionAction() {
-        super(tr("Select URL..."), null, tr("Select URL to download from."),
-                Shortcut.registerShortcut("file:selecturl", tr("File: {0}", tr("Select URL...")), KeyEvent.VK_D, Shortcut.CTRL_SHIFT),
-                true, "mirroreddownload/urlselection", true);
+        super(tr("Select OSM mirror URL"), null, tr("Select OSM mirror URL to download from."),
+                null, true, "mirroreddownload/urlselection", true);
         putValue("help", ht("/Action/SelectUrl"));
     }
 
