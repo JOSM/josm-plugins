66675a0ac9c3f7ce7230a8fb2e581b0fa7b06cdb
joshdoe <joshdoe@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-05-30 02:39:19 +0000

    Support EastNorth and LatLon for coordinates, and rename converter class
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@28415 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- jts/src/org/openstreetmap/josm/plugins/jts/JTSConverter.java
+++ jts/src/org/openstreetmap/josm/plugins/jts/JTSConverter.java
@@ -3,7 +3,6 @@ package org.openstreetmap.josm.plugins.jts;
 import com.vividsolutions.jts.geom.*;
 import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
 import java.util.List;
-import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Relation;
@@ -12,17 +11,37 @@ import org.openstreetmap.josm.data.osm.Way;
 /**
  * Methods to convert JOSM geometry to JTS geometry
  */
-public final class JTSUtils {
+public class JTSConverter {
+    private PrecisionModel precisionModel;
+    private GeometryFactory geometryFactory;
+    private boolean useEastNorth;
+    /**
+     * Conversions will use latitude/longitude for coordinates
+     */
+    public JTSConverter() {
+        this(false);
+    }
 
-    public final OsmPrecisionModel osmPrecisionModel = new OsmPrecisionModel();
-    public final GeometryFactory osmGeometryFactory = new GeometryFactory(getPrecisionModel());
+    /**
+     * Conversions will use LatLon if useEastNorth is false, otherwise EastNorth
+     * (the currently selected projection) will be used for coordinates.
+     * @param useEastNorth
+     */
+    public JTSConverter(boolean useEastNorth) {
+        this.useEastNorth = useEastNorth;
+        if (useEastNorth)
+            precisionModel = new PrecisionModel();
+        else
+            precisionModel = new OsmPrecisionModel();
+        geometryFactory = new GeometryFactory(precisionModel);
+    }
 
-    public OsmPrecisionModel getPrecisionModel() {
-        return osmPrecisionModel;
+    public PrecisionModel getPrecisionModel() {
+        return precisionModel;
     }
 
     public GeometryFactory getGeometryFactory() {
-        return osmGeometryFactory;
+        return geometryFactory;
     }
 
     /**
@@ -37,8 +56,10 @@ public final class JTSUtils {
     }
 
     public Coordinate convertNodeToCoordinate(Node node) {
-        LatLon ll = node.getCoor();
-        return new Coordinate(ll.lon(), ll.lat());
+        if (useEastNorth)
+            return new Coordinate(node.getEastNorth().getX(), node.getEastNorth().getY());
+        else
+            return new Coordinate(node.getCoor().getX(), node.getCoor().getY());
     }
 
     public CoordinateSequence convertNodesToCoordinateSequence(List<Node> nodes) {
