3c1b12b7cb373ca78e522b0e7f242c22a682c9eb
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-08-21 22:12:47 +0000

    [josm_geotools] New JOSM plugin to provide GeoTools 8.0 to other JOSM plugins
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@28592 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ geotools/src/org/openstreetmap/josm/plugins/geotools/GeoToolsPlugin.java
@@ -0,0 +1,11 @@
+// License: GPL. Copyright 2012 Don-vip
+package org.openstreetmap.josm.plugins.geotools;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class GeoToolsPlugin extends Plugin {
+    public GeoToolsPlugin(PluginInformation info) {
+        super(info);
+    }
+}
