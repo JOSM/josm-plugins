7fc72d7333d22da0efb3d860c5f382f3162eebcc
malcolmh <malcolmh@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-08-26 10:09:48 +0000

    save
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@28633 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ smed2/src/smed/Smed.java
@@ -0,0 +1,10 @@
+package smed;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class Smed extends Plugin {
+    public Smed(PluginInformation info) {
+        super(info);
+    }
+}
