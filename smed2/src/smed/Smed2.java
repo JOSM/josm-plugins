a1850cbf91f7d9edb3fc91bc4d56ccae2b0da0db
malcolmh <malcolmh@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-08-26 11:30:33 +0000

    save
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@28636 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ smed2/src/smed/Smed2.java
@@ -0,0 +1,14 @@
+package smed;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class Smed2 extends Plugin {
+    public Smed2(PluginInformation info) {
+        super(info);
+        MainMenu.add(Main.main.menu.toolsMenu, new SmedAction());
+    }
+}
---
+++ smed2/src/smed/SmedAction.java
@@ -0,0 +1,7 @@
+public class SmedAction extends JosmAction {
+
+  public SimplifyAreaAction() {
+  }
+  
+}
+                                                    
