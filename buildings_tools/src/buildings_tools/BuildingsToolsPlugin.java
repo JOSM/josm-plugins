10f15f0b3fc86be7c6587991cfb2a21a6a18859b
bastik <bastik@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-11-02 22:44:57 +0000

    update to latest josm (projection)
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@28888 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- buildings_tools/src/buildings_tools/BuildingsToolsPlugin.java
+++ buildings_tools/src/buildings_tools/BuildingsToolsPlugin.java
@@ -3,7 +3,8 @@ package buildings_tools;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.data.coor.EastNorth;
 import org.openstreetmap.josm.data.coor.LatLon;
-import org.openstreetmap.josm.data.projection.Mercator;
+import org.openstreetmap.josm.data.projection.Projection;
+import org.openstreetmap.josm.data.projection.ProjectionInfo;
 import org.openstreetmap.josm.gui.IconToggleButton;
 import org.openstreetmap.josm.gui.MainMenu;
 import org.openstreetmap.josm.gui.MapFrame;
@@ -11,7 +12,7 @@ import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class BuildingsToolsPlugin extends Plugin {
-    public static Mercator proj = new Mercator();
+    public static Projection proj = ProjectionInfo.getProjectionByCode("EPSG:3857"); // Mercator
 
     public static EastNorth latlon2eastNorth(LatLon p) {
         return proj.latlon2eastNorth(p);
