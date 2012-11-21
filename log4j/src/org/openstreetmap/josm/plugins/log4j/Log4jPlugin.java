b0f14a988ff5c3d07a2d5a2295f97f53dbe5b114
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-11-21 23:11:36 +0000

    [josm_log4j] initialize plugin
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@28963 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ log4j/src/org/openstreetmap/josm/plugins/log4j/Log4jPlugin.java
@@ -0,0 +1,11 @@
+//License: GPL. Copyright 2012 Don-vip
+package org.openstreetmap.josm.plugins.log4j;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class Log4jPlugin extends Plugin {
+    public Log4jPlugin(PluginInformation info) {
+        super(info);
+    }
+}
