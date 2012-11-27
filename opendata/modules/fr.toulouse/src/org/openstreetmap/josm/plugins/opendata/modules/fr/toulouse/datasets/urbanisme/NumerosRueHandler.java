4ad53e1aba3838c2c6287b97cd61607803720f19
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-11-27 18:20:09 +0000

    [josm_opendata] Handle ref:FR:FANTOIR in Toulouse housenumbers
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@29001 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- opendata/modules/fr.toulouse/src/org/openstreetmap/josm/plugins/opendata/modules/fr/toulouse/datasets/urbanisme/NumerosRueHandler.java
+++ opendata/modules/fr.toulouse/src/org/openstreetmap/josm/plugins/opendata/modules/fr/toulouse/datasets/urbanisme/NumerosRueHandler.java
@@ -49,8 +49,8 @@ public class NumerosRueHandler extends ToulouseDataSetHandler {
             n.remove("mot_directeur");
             n.remove("name");
             n.remove("rivoli");
+            String fantoir = n.get("nrivoli").substring(6);
             n.remove("nrivoli");
-            //n.remove("sti");
             n.remove("color");
             String streetName = NamesFrUtils.checkStreetName(n, "addr:street");
             Relation street = associatedStreets.get(n.get("sti"));
@@ -58,9 +58,11 @@ public class NumerosRueHandler extends ToulouseDataSetHandler {
                 associatedStreets.put(n.get("sti"), street = new Relation());
                 street.put("type", "associatedStreet");
                 street.put("name", streetName);
+                street.put("ref:FR:FANTOIR", fantoir);
                 ds.addPrimitive(street);
             }
             street.addMember(new RelationMember("house", n));
+            n.remove("sti");
         }
     }
 }
