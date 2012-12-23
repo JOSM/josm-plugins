b222df4f3f7a7f094ace11976c4bb303c3673d73
malcolmh <malcolmh@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2012-12-23 19:13:18 +0000

    save
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@29112 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- smed2/src/symbols/Landmarks.java
+++ smed2/src/symbols/Landmarks.java
@@ -9,14 +9,47 @@
 
 package symbols;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 import symbols.Symbols.Instr;
+import symbols.Symbols.Prim;
 
 public class Landmarks {
 	public static final ArrayList<Instr> Chimney = new ArrayList<Instr>();
+	static {
+		Chimney.add(new Instr(Prim.STRK, new BasicStroke(4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER)));
+		Chimney.add(new Instr(Prim.FILL, Color.black));
+		Chimney.add(new Instr(Prim.ELPS, new Ellipse2D.Double(-10,-10,20,20)));
+		Chimney.add(new Instr(Prim.LINE, new Line2D.Double(-35,0,-10,0)));
+		Chimney.add(new Instr(Prim.LINE, new Line2D.Double(10,0,35,0)));
+		Path2D.Double p = new Path2D.Double(); p.moveTo(-25.0,0.0); p.lineTo(-10.0,-120.0); p.lineTo(10.0,-120.0); p.lineTo(25.0,0.0);
+		p.moveTo(-10.0,-128.0); p.curveTo(-13.0,-147.0,15.0,-159.0,20.0,-148.0);
+		p.moveTo(16.0,-152.3); p.curveTo(58.0,-194.0,98.0,-87.0,16.0,-132.0);
+		p.moveTo(11.0,-128.0); p.curveTo(13.4,-132.0,20.0,-132.0,20.0,-136.0);
+		Chimney.add(new Instr(Prim.PLIN, p));
+	}
 	public static final ArrayList<Instr> Church = new ArrayList<Instr>();
+	static {
+		Church.add(new Instr(Prim.FILL, Color.black));
+		Path2D.Double p = new Path2D.Double(); p.moveTo(10.0,-10.0); p.lineTo(37.0,-10.0); p.quadTo(48.0,-10.0,48.0,-21.0); p.lineTo(50.0,-21.0); p.lineTo(50.0,21.0);
+		p.lineTo(48.0,21.0); p.quadTo(48.0,10.0,37.0,10.0); p.lineTo(10.0,10.0); p.lineTo(10.0,37.0); p.quadTo(10.0,48.0,21.0,48.0); p.lineTo(21.0,50.0);
+		p.lineTo(-21.0,50.0); p.lineTo(-21.0,48.0); p.quadTo(-10.0,48.0,-10.0,37.0); p.lineTo(-10.0,10.0); p.lineTo(-37.0,10.0); p.quadTo(-48.0,10.0,-48.0,21.0);
+		p.lineTo(-50.0,21.0); p.lineTo(-50.0,-21.0); p.lineTo(-48.0,-21.0); p.quadTo(-48.0,-10.0,-37.0,-10.0); p.lineTo(-10.0,-10.0); p.lineTo(-10.0,-37.0);
+		p.quadTo(-10.0,-48.0,-21.0,-48.0); p.lineTo(-21.0,-50.0); p.lineTo(21.0,-50.0); p.lineTo(21.0,-48.0); p.quadTo(10.0,-48.0,10.0,-37.0); p.closePath();
+		Church.add(new Instr(Prim.PGON, p));
+	}
 	public static final ArrayList<Instr> ChurchTower = new ArrayList<Instr>();
+	static {
+		ChurchTower.add(new Instr(Prim.STRK, new BasicStroke(4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER)));
+		ChurchTower.add(new Instr(Prim.FILL, Color.black));
+		ChurchTower.add(new Instr(Prim.RECT, new Rectangle2D.Double(-36,-36,72,72)));
+	}
 	public static final ArrayList<Instr> Cross = new ArrayList<Instr>();
 	public static final ArrayList<Instr> DishAerial = new ArrayList<Instr>();
 	public static final ArrayList<Instr> Dome = new ArrayList<Instr>();
