a33b9f440b86a887bb57cd2674ad70dbbd1e34a8
simon04 <simon04@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2013-03-03 14:57:12 +0000

    JOSM/wikipedia: store copy templates in preference key `wikipedia.copytemplates` and thus make them configurable
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@29335 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- wikipedia/src/org/wikipedia/WikipediaCopyTemplate.java
+++ wikipedia/src/org/wikipedia/WikipediaCopyTemplate.java
@@ -9,6 +9,8 @@ import java.util.List;
 import javax.swing.JMenuItem;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
+import org.openstreetmap.josm.data.Preferences;
+import org.openstreetmap.josm.data.Preferences.pref;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.MainMenu;
@@ -16,26 +18,55 @@ import org.openstreetmap.josm.tools.Utils;
 
 public class WikipediaCopyTemplate {
 
-    private static final List<CoordCopyTemplate> TEMPLATES = Arrays.asList(
-            new CoordCopyTemplate(tr("Copy {0} template", "{{Coordinate}}"), "wikipedia-coordinate", "{{Coordinate|NS={lat}|EW={lon}|type=landmark|region=}}"),
-            new CoordCopyTemplate(tr("Copy {0} template", "{{Coord}}"), "wikipedia-coord", "{{Coord|{lat}|{lon}}}"),
-            new CoordCopyTemplate(tr("Copy {0} template", "{{Location dec}}"), "wikipedia-location-dec", "{{Location dec|{lat}|{lon}}}"),
-            new CoordCopyTemplate(tr("Copy {0} template", "{{Object location dec}}"), "wikipedia-object-location-dec", "{{Object location dec|{lat}|{lon}}}")
-            );
+    private static final List<CoordCopyTemplateEntry> DEFAULT_TEMPLATES = Arrays.asList(
+            new CoordCopyTemplateEntry("{{Coordinate}}", "wikipedia-coordinate", "{{Coordinate|NS={lat}|EW={lon}|type=landmark|region=}}"),
+            new CoordCopyTemplateEntry("{{Coord}}", "wikipedia-coord", "{{Coord|{lat}|{lon}}}"),
+            new CoordCopyTemplateEntry("{{Location dec}}", "wikipedia-location-dec", "{{Location dec|{lat}|{lon}}}"),
+            new CoordCopyTemplateEntry("{{Object location dec}}", "wikipedia-object-location-dec", "{{Object location dec|{lat}|{lon}}}")
+    );
+
+    private static final List<CoordCopyTemplateEntry> TEMPLATE_ENTRIES =
+            Main.pref.getListOfStructs("wikipedia.copytemplates", DEFAULT_TEMPLATES, CoordCopyTemplateEntry.class);
 
     public WikipediaCopyTemplate() {
         JosmAction previous = Main.main.menu.copyCoordinates;
-        for (final CoordCopyTemplate t : TEMPLATES) {
+        for (final CoordCopyTemplateEntry templateEntry : TEMPLATE_ENTRIES) {
+            CoordCopyTemplate t = new CoordCopyTemplate(templateEntry);
             final JMenuItem menu = MainMenu.addAfter(Main.main.menu.editMenu, t, false, previous);
-            menu.setToolTipText(tr("Copies the template to the system clipboard instantiated with the coordinates of the first selected node"));
+            menu.setToolTipText(tr("Copies the {0} template to the system clipboard instantiated with the coordinates of the first selected node", templateEntry.name));
             previous = t;
-            //MainMenu.addAfter(Main.main.menu.editMenu, coord, false, Main.main.menu.copyCoordinates);
+        }
+    }
+
+    /**
+     * Class to hold copy templates for serialization using {@link Preferences}.
+     * Public visibility is needed for reflection used in {@link Preferences#getListOfStructs}.
+     */
+    public static class CoordCopyTemplateEntry {
+        @pref
+        public String name;
+        @pref
+        public String id;
+        @pref
+        public String pattern;
+
+        public CoordCopyTemplateEntry() {
+        }
+
+        public CoordCopyTemplateEntry(String name, String id, String pattern) {
+            this.name = name;
+            this.id = id;
+            this.pattern = pattern;
         }
     }
 
     private static class CoordCopyTemplate extends JosmAction {
 
         protected final String pattern;
+
+        public CoordCopyTemplate(final CoordCopyTemplateEntry entry) {
+            this(tr("Copy {0} template", entry.name), entry.id, entry.pattern);
+        }
 
         public CoordCopyTemplate(String name, String toolbarId, String pattern) {
             super(name, "dialogs/wikipedia", null, null, false, toolbarId, true);
