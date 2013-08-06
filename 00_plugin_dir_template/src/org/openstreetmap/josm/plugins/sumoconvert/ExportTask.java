3b712f52a859643c92f73c9c2d945ac824c24600
julior <julior@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2013-08-06 15:55:39 +0000

    sumoconvert - starting plugin development
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@29831 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ 00_plugin_dir_template/src/org/openstreetmap/josm/plugins/sumoconvert/ExportTask.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package org.openstreetmap.josm.plugins.sumoconvert;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openstreetmap.josm.gui.PleaseWaitRunnable;
+import org.openstreetmap.josm.io.OsmTransferException;
+import org.xml.sax.SAXException;
+
+/**
+ * @author Ignacio Palermo - Julio Rivera
+ *
+ */
+
+public class ExportTask extends PleaseWaitRunnable {
+
+	static Properties sumoConvertProperties = new Properties();
+	
+	public ExportTask() {
+		super("sumoexport");
+		try {
+			sumoConvertProperties.load(ExportTask.class.getResourceAsStream("/resources/properties/sumoConvert.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openstreetmap.josm.gui.PleaseWaitRunnable#cancel()
+	 */
+	@Override
+	protected void cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openstreetmap.josm.gui.PleaseWaitRunnable#finish()
+	 */
+	@Override
+	protected void finish() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openstreetmap.josm.gui.PleaseWaitRunnable#realRun()
+	 */
+	@Override
+	protected void realRun() throws SAXException, IOException,
+			OsmTransferException {
+		
+		try {
+			Runtime.getRuntime().exec(sumoConvertProperties.getProperty("resources") +
+									  sumoConvertProperties.getProperty("netconvert") +
+									  sumoConvertProperties.getProperty("netconvert.osmfiles") +
+									  sumoConvertProperties.getProperty("plainoutput"),
+									  null, 
+									  new File("E:\\Users\\ignacio_palermo\\Documents\\Facultad\\Tesis\\sumo-0.15.0\\bin")
+			); 			
+		} catch (IOException e) {
+		     e.printStackTrace();
+		    }
+	}
+
+}
---
+++ 00_plugin_dir_template/src/org/openstreetmap/josm/plugins/sumoconvert/SumoConvertPlugin.java
@@ -0,0 +1,48 @@
+package org.openstreetmap.josm.plugins.sumoconvert;
+
+import java.util.ArrayList;
+
+import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.plugins.turnrestrictions.list.TurnRestrictionsListDialog;
+import org.openstreetmap.josm.plugins.turnrestrictions.preferences.PreferenceEditor;
+import org.openstreetmap.josm.plugins.turnrestrictions.*;
+
+/**
+ * This is the main class for the sumoconvert plugin.
+ * 
+ */
+public class SumoConvertPlugin extends Plugin{
+    
+	private final ArrayList<Relation> turnrestrictions = new ArrayList<Relation>();
+	private SumoExportAction exportAction;
+	
+    public SumoConvertPlugin(PluginInformation info) {
+        super(info);
+        exportAction = new SumoExportAction();
+        Main.main.menu.toolsMenu.add(exportAction);
+    }
+    
+    /**
+     * Called when the JOSM map frame is created or destroyed. 
+     */
+    @Override
+    public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {             
+        if (oldFrame == null && newFrame != null) { // map frame added
+        	
+            //TurnRestrictionsListDialog dialog  = new TurnRestrictionsListDialog();
+            //add the dialog
+            //newFrame.addToggleDialog(dialog);
+            //CreateOrEditTurnRestrictionAction.getInstance();
+        }
+    }
+
+    @Override
+    public PreferenceSetting getPreferenceSetting() {
+        return new PreferenceEditor();
+    }
+}
