addef17c80dfa030877a25266eeb992d755342b9
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2013-08-23 01:13:10 +0000

    [josm_tagging_preset_tester] fix #josm8989 - move tagging preset entry to Windows menu
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@29864 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- tagging-preset-tester/src/org/openstreetmap/josm/plugins/taggingpresettester/TaggingPresetTesterAction.java
+++ tagging-preset-tester/src/org/openstreetmap/josm/plugins/taggingpresettester/TaggingPresetTesterAction.java
@@ -27,7 +27,7 @@ public class TaggingPresetTesterAction extends JosmAction {
         Shortcut.registerShortcut("tools:taggingpresettester",
         tr("Tool: {0}", tr("Tagging Preset Tester")),
         KeyEvent.VK_T, Shortcut.ALT_CTRL_SHIFT), true);
-        MainMenu.addAfter(Main.main.menu.presetsMenu, this, false, Main.main.menu.presetSearchAction);
+        MainMenu.addAfter(Main.main.menu.windowMenu, this, false, Main.main.menu.changesetManager);
     }
 
     public TaggingPresetTesterAction(PluginInformation info) {
