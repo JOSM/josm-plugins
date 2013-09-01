2d7bf4295020e2b872cf2a2032c524a41b8ba5a3
malcolmh <malcolmh@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2013-09-01 14:18:42 +0000

    'New release'
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@29905 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- smed/src/smed/Smed.java
+++ smed/src/smed/Smed.java
@@ -2,13 +2,22 @@ package smed;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
 public class Smed extends Plugin {
 
+	SmedAction dialog = new SmedAction();
+	
 	public Smed(PluginInformation info) {
-        super(info);
-        MainMenu.add(Main.main.menu.toolsMenu, new SmedAction());
-    }
+		super(info);
+		MainMenu.add(Main.main.menu.toolsMenu, dialog);
+	}
+	
+	public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
+		if (newFrame == null) {
+			dialog.closeDialog();
+		}
+	}
 }
