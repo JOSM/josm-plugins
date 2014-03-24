eaf55f466970741a93028fd415bb84184dbb60f7
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2014-03-24 16:03:02 +0000

    [josm_pbf] code cleanup
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@30341 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- pbf/src/org/openstreetmap/josm/plugins/pbf/io/PbfReader.java
+++ pbf/src/org/openstreetmap/josm/plugins/pbf/io/PbfReader.java
@@ -1,18 +1,4 @@
-//    JOSM PBF plugin.
-//    Copyright (C) 2011 Don-vip
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.pbf.io;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
@@ -62,9 +48,6 @@ public class PbfReader extends AbstractReader {
         protected void parse(HeaderBlock header) {
         }
 
-        /* (non-Javadoc)
-         * @see crosby.binary.BinaryParser#skipBlock(crosby.binary.file.FileBlockPosition)
-         */
         @Override
         public boolean skipBlock(FileBlockPosition block) {
             return exception != null;
