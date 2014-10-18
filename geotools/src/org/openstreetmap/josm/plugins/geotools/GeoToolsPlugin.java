08c6bc588b1eb4236146564abf405ecaed38c131
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2014-10-18 14:48:52 +0000

    [josm_geotools] fix sonar issues
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@30734 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- geotools/src/org/openstreetmap/josm/plugins/geotools/GeoToolsPlugin.java
+++ geotools/src/org/openstreetmap/josm/plugins/geotools/GeoToolsPlugin.java
@@ -16,7 +16,15 @@ import org.openstreetmap.josm.plugins.PluginInformation;
 
 import com.sun.media.jai.imageioimpl.ImageReadWriteSpi;
 
+/**
+ * GeoTools plugin, bringing this library to other JOSM plugins.
+ */
 public class GeoToolsPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code GeoToolsPlugin}.
+     * @param info plugin information
+     */
     public GeoToolsPlugin(PluginInformation info) {
         super(info);
         initJAI();
@@ -28,42 +36,31 @@ public class GeoToolsPlugin extends Plugin {
         // Disable mediaLib searching that produces unwanted errors
         // See https://www.java.net/node/666373
         System.setProperty("com.sun.media.jai.disableMediaLib", "true");
-                
+
         // As the JAI jars are bundled in the geotools plugin, JAI initialization does not work,
         // so we need to perform the tasks described here ("Initialization and automatic loading of registry objects"):
         // http://docs.oracle.com/cd/E17802_01/products/products/java-media/jai/forDevelopers/jai-apidocs/javax/media/jai/OperationRegistry.html
         OperationRegistry registry = JAI.getDefaultInstance().getOperationRegistry();
         if (registry == null) {
-            System.err.println("geotools: error in JAI initialization. Cannot access default operation registry");
+            Main.error("geotools: error in JAI initialization. Cannot access default operation registry");
         } else {
             // Update registry with com.sun.media.jai.imageioimpl.ImageReadWriteSpi (only class listed javax.media.jai.OperationRegistrySpi)
-            // it would be safer to parse this file instead, but a JAI update is very unlikely as it has not been modified since 2005 
+            // it would be safer to parse this file instead, but a JAI update is very unlikely as it has not been modified since 2005
             new ImageReadWriteSpi().updateRegistry(registry);
 
-            // Update registry with GeoTools registry file 
-            InputStream in = GeoToolsPlugin.class.getResourceAsStream("/META-INF/registryFile.jai");
-            if (in == null) {
-                System.err.println("geotools: error in JAI initialization. Cannot access META-INF/registryFile.jai");
-            } else {
-                try {
+            // Update registry with GeoTools registry file
+            try (InputStream in = GeoToolsPlugin.class.getResourceAsStream("/META-INF/registryFile.jai")) {
+                if (in == null) {
+                    Main.error("geotools: error in JAI initialization. Cannot access META-INF/registryFile.jai");
+                } else {
                     registry.updateFromStream(in);
-                } catch (IOException e) {
-                    System.err.println("geotools: error in JAI initialization. Cannot update default operation registry");
                 }
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    System.err.println("geotools: error in JAI initialization. Cannot close input stream");
-                }
+            } catch (IOException e) {
+                Main.error("geotools: error in JAI initialization: "+e.getMessage());
             }
-            
-            // Print JAI registry contents
-            //for (String mode : RegistryMode.getModeNames()) {
-            //    System.out.println("geotools: JAI mode "+mode+": "+Arrays.toString(registry.getDescriptorNames(mode)));
-            //}
         }
     }
-    
+
     private void initGeoTools() {
         // Force Axis order. Fix #8248
         // See http://docs.geotools.org/stable/userguide/library/referencing/order.html
