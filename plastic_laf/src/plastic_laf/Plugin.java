e0ab5f110e8a40708b6a4fd94ec096275854ea8f
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2014-10-19 16:50:58 +0000

    [josm_plastic_laf] fix plugin, update to latest version of look and feel
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@30749 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- plastic_laf/src/plastic_laf/Plugin.java
+++ plastic_laf/src/plastic_laf/Plugin.java
@@ -1,3 +1,4 @@
+//License: GPL
 package plastic_laf;
 
 import javax.swing.UIManager;
@@ -5,11 +6,24 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 import org.openstreetmap.josm.plugins.PluginInformation;
 
+import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
 import com.jgoodies.looks.plastic.PlasticLookAndFeel;
+import com.jgoodies.looks.plastic.PlasticXPLookAndFeel;
 
+/**
+ * Plugin that brings JGoodies Plastic Look and Feel to JOSM.
+ */
 public class Plugin {
+
+	/**
+	 * Constructs a new {@code Plugin}.
+	 * @param info plugin info
+	 * @throws UnsupportedLookAndFeelException if look and feel cannot be set
+	 */
     public Plugin(PluginInformation info) throws UnsupportedLookAndFeelException {
         UIManager.getDefaults().put("ClassLoader", getClass().getClassLoader());
-        UIManager.setLookAndFeel(new PlasticLookAndFeel());
+    	UIManager.installLookAndFeel("Plastic", PlasticLookAndFeel.class.getName());
+    	UIManager.installLookAndFeel("Plastic3D", Plastic3DLookAndFeel.class.getName());
+    	UIManager.installLookAndFeel("PlasticXP", PlasticXPLookAndFeel.class.getName());
     }
 }
