e38d28a106af11fd663aef5ce05bf9fb89897ee1
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2014-11-29 14:23:33 +0000

    [josm_jna] initialize new JNA plugin
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@30818 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ jna/src/org/openstreetmap/josm/plugins/jna/JNAPlugin.java
@@ -0,0 +1,18 @@
+//License: GPL. Copyright 2014 Don-vip
+package org.openstreetmap.josm.plugins.jna;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * This plugin provides the Java Native Access (JNA) library for JOSM plugins.
+ */
+public class JNAPlugin extends Plugin {
+    /**
+     * Constructs a new {@code JNAPlugin}
+     * @param info plugin info
+     */
+    public JNAPlugin(PluginInformation info) {
+        super(info);
+    }
+}
