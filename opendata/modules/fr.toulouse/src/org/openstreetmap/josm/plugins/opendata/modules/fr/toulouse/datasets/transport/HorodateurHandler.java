3b3e7036b316719718e3371cd889407d48e3cc74
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-01-22 23:55:14 +0000

    [josm_opendata] update to JOSM 7977
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@30920 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- opendata/modules/fr.toulouse/src/org/openstreetmap/josm/plugins/opendata/modules/fr/toulouse/datasets/transport/HorodateurHandler.java
+++ opendata/modules/fr.toulouse/src/org/openstreetmap/josm/plugins/opendata/modules/fr/toulouse/datasets/transport/HorodateurHandler.java
@@ -15,7 +15,7 @@ public class HorodateurHandler extends ToulouseDataSetHandler {
         super(12540, "vending=parking_tickets");
         setWikiPage("Horodateurs");
         setCategory(CAT_TRANSPORT);
-        setMenuIcon("presets/vending_machine.png");
+        setMenuIcon("styles/standard/transport/ticket-machine.png");
     }
 
     @Override
