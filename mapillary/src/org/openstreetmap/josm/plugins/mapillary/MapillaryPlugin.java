906405cf271a7b029df7b15ff937da577ec1cc9b
polyglot <polyglot@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-02-09 00:48:11 +0000

    Added some import statements
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@30951 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- mapillary/src/org/openstreetmap/josm/plugins/mapillary/MapillaryPlugin.java
+++ mapillary/src/org/openstreetmap/josm/plugins/mapillary/MapillaryPlugin.java
@@ -3,11 +3,15 @@ package org.openstreetmap.josm.plugins.mapillary;
 import java.util.ArrayList;
 
 import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.gui.preferences.PreferenceSetting;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
+
+import org.openstreetmap.josm.gui.layer.geoimage.GeoImageLayer;
+import org.openstreetmap.josm.gui.layer.geoimage.ImageDisplay;
+import org.openstreetmap.josm.gui.layer.geoimage.ImageEntry;
+import org.openstreetmap.josm.gui.layer.geoimage.ImageViewerDialog;
 
 /**
  * This is the main class for the mapillary plugin.
