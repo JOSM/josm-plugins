319a72405d493eb98dc87d356b7eefff89ede2a8
nokutu <nokutu@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-06-02 14:20:45 +0000

    Updated repo
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@31181 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ commons-imaging/src/org/openstreemap/josm/plugins/commons/imaging/CommonsImagingPlugin.java
@@ -0,0 +1,15 @@
+package org.openstreetmap.josm.plugins.commons.imaging;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class CommonsImagingPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code CommonsImagingPlugin}.
+     * @param info plugin information
+     */
+    public CommonsImagingPlugin(PluginInformation info) {
+        super(info);
+    } 
+}
