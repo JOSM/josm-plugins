264c81bd28cf5fd7372f6f527712b14920630cb1
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-06-07 14:54:32 +0000

    [josm_log4j] migrate to Log4j 2.3
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@31229 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- log4j/src/org/openstreetmap/josm/plugins/log4j/Log4jPlugin.java
+++ log4j/src/org/openstreetmap/josm/plugins/log4j/Log4jPlugin.java
@@ -1,10 +1,19 @@
-//License: GPL. Copyright 2012 Don-vip
+// License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.log4j;
 
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
+/**
+ * Log4j plugin brings the Log4j API to other plugins.
+ * @version 2.3
+ */
 public class Log4jPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code Log4jPlugin}.
+     * @param info plugin info
+     */
     public Log4jPlugin(PluginInformation info) {
         super(info);
     }
---
+++ log4j/src/org/openstreetmap/josm/plugins/log4j/package-info.java
@@ -0,0 +1,6 @@
+// License: GPL. For details, see LICENSE file.
+
+/**
+ * Provides Main plugin class.
+ */
+package org.openstreetmap.josm.plugins.log4j;
