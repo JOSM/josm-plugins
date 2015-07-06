bfef32481fa337e1bb2972e6e1ec18f13e97ffa8
nokutu <nokutu@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-07-06 13:57:42 +0000

    Added externals again, JOSM doesn't support oauth 2.0
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@31342 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- mapillary/src/org/openstreetmap/josm/plugins/mapillary/gui/MapillaryOAuthUI.java
+++ mapillary/src/org/openstreetmap/josm/plugins/mapillary/gui/MapillaryOAuthUI.java
@@ -1,12 +1,42 @@
 package org.openstreetmap.josm.plugins.mapillary.gui;
 
+import java.util.Scanner;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.openstreetmap.josm.plugins.mapillary.oauth.MapillaryOAuthApi;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.Token;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+
+/**
+ * JPanel used to get the OAuth tokens from Mapillary.
+ * 
+ * @author nokutu
+ *
+ */
 public class MapillaryOAuthUI extends JPanel {
 
+    private static final Token EMPTY_TOKEN = null;
+
     public MapillaryOAuthUI() {
-        this.add(new JLabel("Aquí esta el login"));
+        Scanner in = new Scanner(System.in);
+        OAuthService service = new ServiceBuilder()
+                .provider(MapillaryOAuthApi.class)
+                .apiKey("NzNRM2otQkR2SHJzaXJmNmdQWVQ0dzo1YTA2NmNlODhlNWMwOTBm")
+                .apiSecret("Secret").build();
+        String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
+        this.add(new JLabel("Login"));
+        System.out.println("Fetching the Authorization URL...");
+        System.out.println("Got the Authorization URL!");
+        System.out.println("Now go and authorize Scribe here:");
+        System.out.println(authorizationUrl);
+        System.out.println("And paste the authorization code here");
+        System.out.print(">>");
+        Verifier verifier = new Verifier(in.nextLine());
+        System.out.println();
     }
 
 }
---
+++ mapillary/src/org/openstreetmap/josm/plugins/mapillary/oauth/MapillaryOAuthApi.java
@@ -0,0 +1,29 @@
+package org.openstreetmap.josm.plugins.mapillary.oauth;
+
+import org.scribe.builder.api.DefaultApi20;
+import org.scribe.model.OAuthConfig;
+import org.scribe.model.Verb;
+
+/**
+ * API class of Mapillary for the scrable library.
+ * 
+ * @author nokutu
+ *
+ */
+public class MapillaryOAuthApi extends DefaultApi20 {
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return "https://a.mapillary.com/v2/oauth/token";
+    }
+
+    @Override
+    public String getAuthorizationUrl(OAuthConfig config) {
+        return "http://www.mapillary.io/connect?client_id=MkJKbDA0bnZuZlcxeTJHTmFqN3g1dzplZTlkZjQyYjYyZTczOTdi&redirect_uri=https:%2F%2Fjosm.openstreetmap.de%2F&response_type=token&scope=upload";
+    }
+
+    @Override
+    public Verb getAccessTokenVerb() {
+        return Verb.POST;
+    }
+}
