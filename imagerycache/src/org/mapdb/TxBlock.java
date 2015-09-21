6358ff6bcf175e4d74aa8a498ec2fb1793c5017d
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-09-21 00:24:33 +0000

    [josm_imagerycache] deprecate plugin (now in core in JOSM 8772)
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@31575 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- imagerycache/src/org/mapdb/TxBlock.java
+++
@@ -1,9 +0,0 @@
-package org.mapdb;
-
-/**
- * Wraps single transaction in a block
- */
-public interface TxBlock {
-
-    void tx(DB db) throws TxRollbackException;
-}
--- imagerycache/src/org/mapdb/TxRollbackException.java
+++
@@ -1,9 +0,0 @@
-package org.mapdb;
-
-/**
- * Exception thrown when transaction is rolled back.
- * @author Jan Kotek
- */
-public class TxRollbackException extends RuntimeException {
-
-}
