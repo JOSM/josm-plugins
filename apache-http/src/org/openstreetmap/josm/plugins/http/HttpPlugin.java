06e42dd72b96e4303f3d9c6250bd14c5acb2e7d9
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-10-25 02:39:47 +0000

    [josm_apache-http] Add new plugin for Apache HTTP libraries, used by several plugins
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@31682 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ apache-http/src/org/openstreetmap/josm/plugins/http/HttpPlugin.java
@@ -0,0 +1,16 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.http;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class HttpPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code HttpPlugin}.
+     * @param info plugin information
+     */
+    public HttpPlugin(PluginInformation info) {
+        super(info);
+    } 
+}
