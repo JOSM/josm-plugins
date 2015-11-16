28885773ca335598531f6010edfc55350b8333ba
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-11-16 21:42:40 +0000

    [josm_gson] see #josm12107 - add gson as new plugin
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@31760 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ gson/src/org/openstreetmap/plugins/gson/GsonPlugin.java
@@ -0,0 +1,19 @@
+// License: GPL. Copyright 2015 Don-vip
+package org.openstreetmap.plugins.gson;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * GSON plugin, bringing this library to other JOSM plugins.
+ */
+public class GsonPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code GsonPlugin}.
+     * @param info plugin information
+     */
+    public GsonPlugin(PluginInformation info) {
+        super(info);
+    }
+}
