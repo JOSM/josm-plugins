bf00d7dc68c00af6951afa442c147197dda0e4b0
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-11-16 01:08:06 +0000

    [josm_plugins] add EJML plugin
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@31753 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ ejml/src/org/openstreetmap/josm/plugins/ejml/EjmlPlugin.java
@@ -0,0 +1,19 @@
+// License: GPL. Copyright 2015 Don-vip
+package org.openstreetmap.josm.plugins.ejml;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * EJML plugin, bringing this library to other JOSM plugins.
+ */
+public class EjmlPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code EjmlPlugin}.
+     * @param info plugin information
+     */
+    public EjmlPlugin(PluginInformation info) {
+        super(info);
+    }
+}
