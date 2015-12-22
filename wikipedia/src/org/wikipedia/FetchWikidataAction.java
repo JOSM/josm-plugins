92832fca57f93a7b414e3b86ebf96a2183612144
simon04 <simon04@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2015-12-22 20:46:48 +0000

    JOSM/wikipedia: show notification w/ not found articles - see #josm10148
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@31853 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- wikipedia/src/org/wikipedia/FetchWikidataAction.java
+++ wikipedia/src/org/wikipedia/FetchWikidataAction.java
@@ -12,15 +12,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import javax.swing.JOptionPane;
+
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.command.ChangePropertyCommand;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.SequenceCommand;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.gui.Notification;
 import org.openstreetmap.josm.gui.PleaseWaitRunnable;
 import org.openstreetmap.josm.gui.progress.ProgressMonitor;
 import org.openstreetmap.josm.tools.MultiMap;
+import org.openstreetmap.josm.tools.Utils;
 
 public class FetchWikidataAction extends JosmAction {
 
@@ -40,7 +44,8 @@ public class FetchWikidataAction extends JosmAction {
     static class Fetcher extends PleaseWaitRunnable {
         private final Collection<? extends OsmPrimitive> selection;
         private boolean canceled = false;
-        private List<Command> commands;
+        private final List<Command> commands = new ArrayList<>();
+        private final Collection<WikipediaApp.WikipediaLangArticle> notFound = new ArrayList<>();
 
         public Fetcher(Collection<? extends OsmPrimitive> selection) {
             super(tr("Fetching Wikidata IDs"));
@@ -55,16 +60,18 @@ public class FetchWikidataAction extends JosmAction {
         @Override
         protected void realRun() {
             final Map<String, PrimitivesWithWikipedia> wikipediaByLanguage = getLanguageToArticlesMap(selection);
-            commands = new ArrayList<>(wikipediaByLanguage.keySet().size());
             getProgressMonitor().setTicksCount(wikipediaByLanguage.keySet().size());
             for (final Map.Entry<String, PrimitivesWithWikipedia> i : wikipediaByLanguage.entrySet()) {
                 if (canceled) {
                     break;
                 }
-                final Command command = i.getValue().updateWikidataIds(getProgressMonitor().createSubTaskMonitor(1, false));
+                final PrimitivesWithWikipedia fetcher = i.getValue();
+                fetcher.updateWikidataIds(getProgressMonitor().createSubTaskMonitor(1, false));
+                final Command command = fetcher.getCommand();
                 if (command != null) {
                     commands.add(command);
                 }
+                notFound.addAll(fetcher.getNotFound());
             }
         }
 
@@ -87,12 +94,20 @@ public class FetchWikidataAction extends JosmAction {
             if (!canceled && !commands.isEmpty()) {
                 Main.main.undoRedo.add(commands.size() == 1 ? commands.get(0) : new SequenceCommand(tr("Add Wikidata"), commands));
             }
+            if (!canceled && !notFound.isEmpty()) {
+                new Notification(tr("No Wikidata ID found for: {0}", Utils.joinAsHtmlUnorderedList(notFound)))
+                        .setIcon(JOptionPane.WARNING_MESSAGE)
+                        .setDuration(Notification.TIME_LONG)
+                        .show();
+            }
         }
     }
 
     static class PrimitivesWithWikipedia {
         final String lang;
         final MultiMap<String, OsmPrimitive> byArticle = new MultiMap<>();
+        final List<Command> commands = new ArrayList<>();
+        final List<WikipediaApp.WikipediaLangArticle> notFound = new ArrayList<>();
 
         public PrimitivesWithWikipedia(String lang) {
             this.lang = lang;
@@ -102,12 +117,11 @@ public class FetchWikidataAction extends JosmAction {
             byArticle.put(wikipedia, key);
         }
 
-        protected Command updateWikidataIds(ProgressMonitor monitor) {
+        protected void updateWikidataIds(ProgressMonitor monitor) {
             final int size = byArticle.keySet().size();
             monitor.beginTask(trn(
                     "Fetching {0} Wikidata ID for language ''{1}''",
                     "Fetching {0} Wikidata IDs for language ''{1}''", size, size, lang));
-            final List<Command> commands = new ArrayList<>(size);
             final Map<String, String> wikidataByWikipedia = WikipediaApp.getWikidataForArticles(lang, byArticle.keySet());
             for (Map.Entry<String, Set<OsmPrimitive>> i : byArticle.entrySet()) {
                 final String wikipedia = i.getKey();
@@ -115,13 +129,22 @@ public class FetchWikidataAction extends JosmAction {
                 if (wikidata != null) {
                     commands.add(new ChangePropertyCommand(i.getValue(), "wikidata", wikidata));
                 } else {
-                    Main.warn(tr("No Wikidata ID found for {0}", lang + ":" + wikipedia));
+                    final WikipediaApp.WikipediaLangArticle article = new WikipediaApp.WikipediaLangArticle(lang, wikipedia);
+                    Main.warn(tr("No Wikidata ID found for: {0}", article));
+                    notFound.add(article);
                 }
             }
             monitor.finishTask();
+        }
+
+        public Command getCommand() {
             return commands.isEmpty()
                     ? null
                     : new SequenceCommand(tr("Add Wikidata for language ''{0}''", lang), commands);
+        }
+
+        public List<WikipediaApp.WikipediaLangArticle> getNotFound() {
+            return notFound;
         }
     }
 
