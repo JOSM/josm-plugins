2c6ff540008942f8fdf955626e37620f79399575
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-06-16 03:29:37 +0000

    fix crash with JOSM 10358+
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32278 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- OSMRecPlugin/src/org/openstreetmap/josm/plugins/osmrec/MenuExportAction.java
+++ OSMRecPlugin/src/org/openstreetmap/josm/plugins/osmrec/MenuExportAction.java
@@ -10,18 +10,19 @@ import org.openstreetmap.josm.actions.JosmAction;
  * @author imis-nkarag
  *
  */
-
 public class MenuExportAction extends JosmAction {
-       
-        public MenuExportAction(){
-        super(tr("OSM Recommendation"), "images/dialogs/logo-osmrec.png", 
-                tr("Recommend categories to your newly created instances."), null, false);
+
+    /**
+     * Constructs a new {@code MenuExportAction}.
+     */
+    public MenuExportAction(){
+        super(tr("OSM Recommendation"), "dialogs/logo-osmrec.png", 
+              tr("Recommend categories to your newly created instances."), null, false);
     }
         
     @Override
     public void actionPerformed(ActionEvent arg0) {
-
-        if( OSMRecPlugin.getCurrentMapFrame() !=null ){
+        if (OSMRecPlugin.getCurrentMapFrame() != null) {
             OSMRecToggleDialog pro = new OSMRecToggleDialog();
             OSMRecPlugin.getCurrentMapFrame().addToggleDialog(pro);
         }       
