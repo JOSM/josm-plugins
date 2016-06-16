775a4c69453d9fe525e7074a0b6f97f04f6670a2
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-06-16 21:24:44 +0000

    update plugin to JOSM 10407
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32290 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- pbf/src/org/openstreetmap/josm/plugins/pbf/PbfPlugin.java
+++ pbf/src/org/openstreetmap/josm/plugins/pbf/PbfPlugin.java
@@ -10,18 +10,21 @@ import org.openstreetmap.josm.plugins.pbf.io.PbfExporter;
 import org.openstreetmap.josm.plugins.pbf.io.PbfImporter;
 
 /**
- * 
+ * PBF plugin.
  * @author Don-vip
- *
  */
 public class PbfPlugin extends Plugin {
 
+    /**
+     * Constructs a new {@code PbfPlugin}.
+     * @param info plugin information
+     */
     public PbfPlugin(PluginInformation info) {
         super(info);
         // Allow JOSM to import *.osm.pbf files
-        ExtensionFileFilter.importers.add(new PbfImporter());
+        ExtensionFileFilter.addImporter(new PbfImporter());
         // Allow JOSM to export *.osm.pbf files
-        ExtensionFileFilter.exporters.add(new PbfExporter());
+        ExtensionFileFilter.addExporter(new PbfExporter());
         // Allow JOSM to download remote *.osm.pbf files
         Main.main.menu.openLocation.addDownloadTaskClass(DownloadPbfTask.class);
     }
