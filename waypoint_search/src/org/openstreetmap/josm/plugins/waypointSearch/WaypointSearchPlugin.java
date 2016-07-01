9556be6c40186ffb04e74c08891f5141cf1b2f28
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-07-01 22:14:23 +0000

    remove calls to deprecated methods
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32514 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- waypoint_search/src/org/openstreetmap/josm/plugins/waypointSearch/WaypointSearchPlugin.java
+++ waypoint_search/src/org/openstreetmap/josm/plugins/waypointSearch/WaypointSearchPlugin.java
@@ -2,10 +2,12 @@ package org.openstreetmap.josm.plugins.waypointSearch;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.MapFrame;
-import org.openstreetmap.josm.gui.MapView;
-import org.openstreetmap.josm.gui.MapView.LayerChangeListener;
-import org.openstreetmap.josm.gui.layer.Layer;
+import org.openstreetmap.josm.gui.layer.LayerManager.LayerAddEvent;
+import org.openstreetmap.josm.gui.layer.LayerManager.LayerChangeListener;
+import org.openstreetmap.josm.gui.layer.LayerManager.LayerOrderChangeEvent;
+import org.openstreetmap.josm.gui.layer.LayerManager.LayerRemoveEvent;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
@@ -25,7 +27,7 @@ public class WaypointSearchPlugin extends Plugin implements LayerChangeListener 
      */
     public WaypointSearchPlugin(PluginInformation info) {
         super(info);
-        MapView.addLayerChangeListener(this);
+        Main.getLayerManager().addLayerChangeListener(this);
     }
      
     @Override
@@ -41,12 +43,12 @@ public class WaypointSearchPlugin extends Plugin implements LayerChangeListener 
     }
 
     @Override
-    public void activeLayerChange(Layer oldLayer, Layer newLayer) {
+    public void layerOrderChanged(LayerOrderChangeEvent e) {
         // Do nothing
     }
     
     @Override
-    public void layerAdded(Layer newLayer) {
+    public void layerAdded(LayerAddEvent e) {
         // update search
         if (waypointDialog != null && Engine.gpxLayersExist()) {
             waypointDialog.updateSearchResults();
@@ -54,7 +56,7 @@ public class WaypointSearchPlugin extends Plugin implements LayerChangeListener 
     }
 
     @Override
-    public void layerRemoved(Layer oldLayer) {
+    public void layerRemoving(LayerRemoveEvent e) {
         if (waypointDialog != null && !Engine.gpxLayersExist()) {
             waypointDialog.updateSearchResults();
         }   
