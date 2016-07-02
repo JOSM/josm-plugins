d909665b59209d7d2a380dc9737436cb9d3e568f
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-07-02 00:48:15 +0000

    add non-regression unit tests
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32524 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- pdfimport/src/pdfimport/pdfbox/PdfBoxParser.java
+++ pdfimport/src/pdfimport/pdfbox/PdfBoxParser.java
@@ -4,6 +4,7 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.awt.geom.Rectangle2D;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -21,35 +22,37 @@ public class PdfBoxParser extends PDFStreamEngine{
 		this.target = target;
 	}
 
-	public void parse(File file, int maxPaths, ProgressMonitor monitor) throws Exception
-	{
-		monitor.beginTask(tr("Parsing PDF", 1));
+    public void parse(File file, int maxPaths, ProgressMonitor monitor) throws IOException {
+        monitor.beginTask(tr("Parsing PDF", 1));
 
-		PDDocument document = PDDocument.load( file);
+        try (PDDocument document = PDDocument.load(file)) {
 
-		if( document.isEncrypted() ){
-			throw new Exception(tr("Encrypted documents not supported."));
-		}
+            if (document.isEncrypted()) {
+                throw new IllegalArgumentException(tr("Encrypted documents not supported."));
+            }
 
-		List<?> allPages = document.getDocumentCatalog().getAllPages();
+            List<?> allPages = document.getDocumentCatalog().getAllPages();
 
-		if (allPages.size() != 1) {
-			throw new Exception(tr("The PDF file must have exactly one page."));
-		}
+            if (allPages.size() != 1) {
+                throw new IllegalArgumentException(tr("The PDF file must have exactly one page."));
+            }
+            
+            PDPage page = (PDPage) allPages.get(0);
+            PDRectangle pageSize = page.findMediaBox();
+            Integer rotationVal = page.getRotation();
+            int rotation = 0;
+            if (rotationVal != null) {
+                rotation = rotationVal.intValue();
+            }
+    
+            new PageDrawer().drawPage(new GraphicsProcessor(target, rotation, maxPaths, monitor), page);
+            this.target.bounds = new Rectangle2D.Double(
+                    pageSize.getLowerLeftX(),
+                    pageSize.getLowerLeftY(), 
+                    pageSize.getWidth(), 
+                    pageSize.getHeight());
+        }
 
-		PDPage page = (PDPage)allPages.get(0);
-		PDRectangle pageSize = page.findMediaBox();
-		Integer rotationVal = page.getRotation();
-		int rotation = 0;
-		if (rotationVal != null){
-			rotation = rotationVal.intValue();
-		}
-
-		GraphicsProcessor p = new GraphicsProcessor(target, rotation, maxPaths, monitor);
-		PageDrawer drawer = new PageDrawer();
-		drawer.drawPage(p, page);
-		this.target.bounds = new Rectangle2D.Double(pageSize.getLowerLeftX(), pageSize.getLowerLeftY(), pageSize.getWidth(), pageSize.getHeight());
-
-		monitor.finishTask();
-	}
+        monitor.finishTask();
+    }
 }
