c26f1a75a572ce9d3ac36628829fdf945869f3fd
simon04 <simon04@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-07-17 12:33:31 +0000

    JOSM/wikipedia: use `updateEnabledStateOnCurrentSelection`
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32667 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- wikipedia/src/org/wikipedia/FetchWikidataAction.java
+++ wikipedia/src/org/wikipedia/FetchWikidataAction.java
@@ -157,12 +157,7 @@ public class FetchWikidataAction extends JosmAction {
 
     @Override
     protected void updateEnabledState() {
-        DataSet ds = getLayerManager().getEditDataSet();
-        if (ds == null) {
-            setEnabled(false);
-        } else {
-            updateEnabledState(ds.getSelected());
-        }
+        updateEnabledStateOnCurrentSelection();
     }
 
     @Override
