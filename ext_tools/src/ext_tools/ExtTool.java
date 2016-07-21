e59a3d031cd6eb5a6f003724f039f514174cac3b
simon04 <simon04@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-07-21 11:45:56 +0000

    JOSM/ext_tools: fix build
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32688 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- ext_tools/src/ext_tools/ExtTool.java
+++ ext_tools/src/ext_tools/ExtTool.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -203,7 +204,7 @@ public class ExtTool {
             synchronized (debugstr) {
                 showErrorMessage(
                         tr("Error executing the script:"),
-                        debugstr.toString() + e.getMessage() + "\n" + e.getStackTrace());
+                        debugstr.toString() + e.getMessage() + "\n" + Arrays.toString(e.getStackTrace()));
             }
             return;
         }
