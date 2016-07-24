2f85cd69b4d7cce9012ef4586417e936da4e0e9f
darya <darya@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-07-24 02:47:21 +0000

    stop-by-stop test added
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32708 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ pt_assistant/test/unit/org/openstreetmap/josm/plugins/pt_assistant/validation/SegmentCheckerTest.java
@@ -0,0 +1,42 @@
+package org.openstreetmap.josm.plugins.pt_assistant.validation;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.plugins.pt_assistant.AbstractTest;
+import org.openstreetmap.josm.plugins.pt_assistant.ImportUtils;
+import org.openstreetmap.josm.plugins.pt_assistant.utils.RouteUtils;
+
+public class SegmentCheckerTest extends AbstractTest {
+	
+	@Test
+	public void test() {
+		
+		
+		File file = new File(AbstractTest.PATH_TO_SEGMENT_TEST);
+		DataSet ds = ImportUtils.importOsmFile(file, "testLayer");
+		PTAssistantValidatorTest test = new PTAssistantValidatorTest();
+		
+		Relation route = null;
+		
+		for (Relation r: ds.getRelations()) {
+			if (RouteUtils.isTwoDirectionRoute(r)) {
+				route = r;
+				break;
+			}
+		}
+		
+		SegmentChecker segmentChecker = new SegmentChecker(route, test);
+		segmentChecker.performStopByStopTest();
+		assertEquals(SegmentChecker.getCorrectSegmentCount(), 29);
+		assertEquals(segmentChecker.getErrors().size(), 0);
+		
+		
+		
+		
+	}
+}
