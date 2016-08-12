b21158fa6762b9879f757c91909046d8c74ff077
darya <darya@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-08-12 11:14:04 +0000

    pt_assistant layer is repainted at fix start
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32804 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- pt_assistant/src/org/openstreetmap/josm/plugins/pt_assistant/validation/PTAssistantValidatorTest.java
+++ pt_assistant/src/org/openstreetmap/josm/plugins/pt_assistant/validation/PTAssistantValidatorTest.java
@@ -13,6 +13,7 @@ import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.SequenceCommand;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
 import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.data.validation.Severity;
@@ -416,6 +417,12 @@ public class PTAssistantValidatorTest extends Test {
 
 	@Override
 	public Command fixError(TestError testError) {
+		
+		// repaint the relation in the pt_assistant layer:
+		if (testError.getPrimitives().iterator().next().getType().equals(OsmPrimitiveType.RELATION)) {
+			Relation relationToBeFixed = (Relation) testError.getPrimitives().iterator().next();
+			this.layer.repaint(relationToBeFixed);
+		}
 
 		List<Command> commands = new ArrayList<>();
 
