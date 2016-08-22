980b69ee8e0c4ce535f3aa3c3082bd837652423f
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-08-22 23:59:35 +0000

    update unit test to JOSM 10876
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32868 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- ElevationProfile/test/unit/org/openstreetmap/josm/plugins/elevation/tests/EleVertexTest.java
+++ ElevationProfile/test/unit/org/openstreetmap/josm/plugins/elevation/tests/EleVertexTest.java
@@ -1,28 +1,29 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.elevation.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.awt.Color;
 import java.util.List;
 
-import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.data.Preferences;
+import org.junit.Rule;
+import org.junit.Test;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.plugins.elevation.ColorMap;
 import org.openstreetmap.josm.plugins.elevation.grid.EleCoordinate;
 import org.openstreetmap.josm.plugins.elevation.grid.EleVertex;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
 
-import junit.framework.TestCase;
+public class EleVertexTest {
 
-public class EleVertexTest extends TestCase {
+    private static final double EPS = 1e-10;
 
-    /**
-     * Setup test.
-     */
-    @Override
-    public void setUp() {
-        Main.pref = new Preferences();
-    }
+    @Rule
+    public JOSMTestRules rules = new JOSMTestRules().preferences();
 
+    @Test
     public void testDivide() {
         EleCoordinate p1 = new EleCoordinate(30.0, 30.0, 100.0);
         EleCoordinate p2 = new EleCoordinate(35.0, 30.0, 120.0);
@@ -35,7 +36,7 @@ public class EleVertexTest extends TestCase {
 
         // 1st vertex (p1, p2, pN  105m)
         EleVertex v1 = list.get(0);
-        assertEquals(325 / 3D, v1.getEle());
+        assertEquals(325 / 3D, v1.getEle(), EPS);
         assertCoorEq(v1, 30D, 30D, 0);
         assertCoorEq(v1, 30D, 35D, 1);
         assertCoorEq(v1, 35D, 32.5D, 2);
@@ -43,12 +44,13 @@ public class EleVertexTest extends TestCase {
         // 2nd vertex (p3, p2, pN = 105m)
         EleVertex v2 = list.get(1);
 
-        assertEquals(335/3D, v2.getEle());
+        assertEquals(335/3D, v2.getEle(), EPS);
         assertCoorEq(v2, 40D, 35D, 0);
         assertCoorEq(v2, 30D, 35D, 1);
         assertCoorEq(v2, 35D, 32.5D, 2);
     }
 
+    @Test
     public void testSimpleRecurse() {
         EleCoordinate c1 = new EleCoordinate(new LatLon(50.8328, 8.1337), 300);
         EleCoordinate c2 = new EleCoordinate(new LatLon(50.8328, 7.9217), 200);
@@ -88,6 +90,7 @@ public class EleVertexTest extends TestCase {
     er.run();
     }*/
 
+    @Test
     public void testColorMap() {
         ColorMap testMap = ColorMap.create("Test", new Color[]{Color.white, Color.black}, new int[]{0, 1000});
 
@@ -110,7 +113,7 @@ public class EleVertexTest extends TestCase {
     }
 
     private void assertCoorEq(EleVertex v1, double x, double y, int n) {
-        assertEquals(x, v1.get(n).getX());
-        assertEquals(y, v1.get(n).getY());
+        assertEquals(x, v1.get(n).getX(), EPS);
+        assertEquals(y, v1.get(n).getY(), EPS);
     }
 }
