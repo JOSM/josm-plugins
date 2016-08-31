41e51fed7e3ca9ec295f98fd1a9ae10018e4682d
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-08-31 23:07:54 +0000

    junit4 style
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@32893 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- ElevationProfile/test/unit/org/openstreetmap/josm/plugins/elevation/tests/HgtReaderTest.java
+++ ElevationProfile/test/unit/org/openstreetmap/josm/plugins/elevation/tests/HgtReaderTest.java
@@ -1,6 +1,9 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.elevation.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import java.io.IOException;
 import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
@@ -8,15 +11,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.openstreetmap.josm.TestUtils;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.plugins.elevation.HgtReader;
 import org.openstreetmap.josm.testutils.JOSMTestRules;
 
-import junit.framework.TestCase;
-
-public class HgtReaderTest extends TestCase {
+public class HgtReaderTest {
 
     @Rule
     public JOSMTestRules rules = new JOSMTestRules().preferences();
@@ -25,7 +28,7 @@ public class HgtReaderTest extends TestCase {
      * Setup test.
      * @throws IOException if SRTM files cannot be installed
      */
-    @Override
+    @Before
     public void setUp() throws IOException {
         // Install SRTM files to plugin directory
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(TestUtils.getTestDataRoot()), "*.hgt")) {
@@ -45,6 +48,7 @@ public class HgtReaderTest extends TestCase {
         }
     }
 
+    @Test
     public void testGetElevationFromHgt() {
         // Staufenberg, Hessen
         testHgtData(50.6607106, 8.7337029, "N50E008.hgt", 199);
