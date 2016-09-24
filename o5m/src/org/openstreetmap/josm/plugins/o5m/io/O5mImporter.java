46ae6f244656f7ed6fe1944b000b937c4cdc6307
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2016-09-24 12:37:01 +0000

    checkstyle
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@33012 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- o5m/src/org/openstreetmap/josm/plugins/o5m/io/O5mImporter.java
+++ o5m/src/org/openstreetmap/josm/plugins/o5m/io/O5mImporter.java
@@ -1,18 +1,4 @@
-//    JOSM o5m plugin.
-//    Copyright (C) 2013 Gerd Petermann
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.o5m.io;
 
 import java.io.IOException;
@@ -36,17 +22,14 @@ public class O5mImporter extends OsmImporter {
         super(O5mConstants.FILE_FILTER);
     }
 
-	/* (non-Javadoc)
-	 * @see org.openstreetmap.josm.io.OsmImporter#parseDataSet(java.io.InputStream, org.openstreetmap.josm.gui.progress.ProgressMonitor)
-	 */
-	@Override
-	protected DataSet parseDataSet(InputStream in, ProgressMonitor progressMonitor) throws IllegalDataException {
-		return O5mReader.parseDataSet(in, progressMonitor);
-	}
+    @Override
+    protected DataSet parseDataSet(InputStream in, ProgressMonitor progressMonitor) throws IllegalDataException {
+        return O5mReader.parseDataSet(in, progressMonitor);
+    }
 
-	protected DataSet parseDataSet(final String source) throws IOException, IllegalDataException {
-		try(CachedFile cf = new CachedFile(source)){
-			return parseDataSet(cf.getInputStream(), NullProgressMonitor.INSTANCE);
-		}
-	}
+    protected DataSet parseDataSet(final String source) throws IOException, IllegalDataException {
+        try (CachedFile cf = new CachedFile(source)) {
+            return parseDataSet(cf.getInputStream(), NullProgressMonitor.INSTANCE);
+        }
+    }
 }
