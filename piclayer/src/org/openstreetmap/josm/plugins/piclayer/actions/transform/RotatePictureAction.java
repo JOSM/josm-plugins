953fc0f999cf93eedd3f91469f897f2d4e5f0d7e
bastik <bastik@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2017-02-15 10:42:23 +0000

    [piclayer] make rotation mouse handling more natural (stick to mouse pointer)
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@33146 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- piclayer/src/org/openstreetmap/josm/plugins/piclayer/actions/transform/RotatePictureAction.java
+++ piclayer/src/org/openstreetmap/josm/plugins/piclayer/actions/transform/RotatePictureAction.java
@@ -22,8 +22,9 @@ package org.openstreetmap.josm.plugins.piclayer.actions.transform;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
-import java.awt.event.InputEvent;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.gui.MapFrame;
@@ -46,12 +47,9 @@ public class RotatePictureAction extends GenericPicTransformAction {
 
     @Override
     protected void doAction(MouseEvent e) {
-        double factor;
-        if ((e.getModifiersEx() & InputEvent.SHIFT_DOWN_MASK) != 0) {
-            factor = Main.pref.getDouble("piclayer.rotatefactors.high_precision", 100.0);
-        } else {
-            factor = Main.pref.getDouble("piclayer.rotatefactors.low_precision", 10.0);
-        }
-        currentLayer.rotatePictureBy((e.getY() - prevMousePoint.getY()) / factor);
+        Point2D center = new Point(Main.map.mapView.getWidth()/2, Main.map.mapView.getHeight()/2);
+        double alpha1 = Math.atan2(e.getY() - center.getY(), e.getX() - center.getX());
+        double alpha0 = Math.atan2(prevMousePoint.getY() - center.getY(), prevMousePoint.getX() - center.getX());
+        currentLayer.rotatePictureBy(alpha1 - alpha0);
     }
 }
