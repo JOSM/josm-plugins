779851b7a131d9091e8c8834232e0eaf4b8b1396
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2017-06-22 21:08:53 +0000

    see #josm14933 - fix unit tests in Eclipse
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@33412 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- pt_assistant/test/unit/org/openstreetmap/josm/plugins/pt_assistant/actions/SplitRoundaboutTest.java
+++ pt_assistant/test/unit/org/openstreetmap/josm/plugins/pt_assistant/actions/SplitRoundaboutTest.java
@@ -7,15 +7,16 @@ import static org.junit.Assert.fail;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.openstreetmap.josm.JOSMFixture;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.SplitWayAction;
+import org.openstreetmap.josm.actions.SplitWayAction.SplitWayResult;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
@@ -26,46 +27,48 @@ import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.io.IllegalDataException;
 import org.openstreetmap.josm.io.OsmReader;
 import org.openstreetmap.josm.plugins.pt_assistant.AbstractTest;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
 
 public class SplitRoundaboutTest extends AbstractTest {
 
-    DataSet ds;
-    OsmDataLayer layer;
-    SplitRoundaboutAction action;
-    SplitWayAction swa;
-    Way r1, r2, r3;
+    /**
+     * Setup test.
+     */
+    @Rule
+    public JOSMTestRules rules = new JOSMTestRules().preferences().platform();
+
+    private DataSet ds;
+    private OsmDataLayer layer;
+    private SplitRoundaboutAction action;
+    private Way r1, r2, r3;
 
     @Before
     public void init() throws FileNotFoundException, IllegalDataException {
-
-        new JOSMFixture("../../JOSM/test/config/unit-josm.home").init(true);
-
         ds = OsmReader.parseDataSet(new FileInputStream(AbstractTest.PATH_TO_ROUNDABOUT), null);
         layer = new OsmDataLayer(ds, OsmDataLayer.createNewName(), null);
         Main.getLayerManager().addLayer(layer);
 
         Main.pref.put("pt_assistant.roundabout-splitter.alignalways", true);
         action = new SplitRoundaboutAction();
-        swa = Main.main.menu.splitWay;
-        swa.setEnabled(true);
         r1 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(293302077L, OsmPrimitiveType.WAY));
         r2 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(205833435L, OsmPrimitiveType.WAY));
         r3 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(25739002L, OsmPrimitiveType.WAY));
     }
 
+    private Collection<Way> splitWay(Way w) {
+        Map<Relation, Integer> savedPositions = action.getSavedPositions(w);
+        List<Node> splitNodes = action.getSplitNodes(w);
+        assertEquals(4, splitNodes.size());
+        SplitWayResult result = SplitWayAction.split(layer, w, splitNodes, Collections.emptyList());
+        result.getCommand().executeCommand();
+        Collection<Way> splitWays = result.getNewWays();
+        action.updateRelations(savedPositions, splitNodes, splitWays);
+        return splitWays;
+    }
+
     @Test
     public void test1() {
-
-        Map<Relation, Integer> savedPositions = action.getSavedPositions(r1);
-        List<Node> splitNodes = action.getSplitNodes(r1);
-        assertEquals(4, splitNodes.size());
-        ds.setSelected(splitNodes);
-        swa.actionPerformed(null);
-
-        Collection<Way> splitWays = ds.getSelectedWays();
-        action.updateRelations(savedPositions, splitNodes, splitWays);
-
-        splitWays.forEach(w -> {
+        splitWay(r1).forEach(w -> {
                 if (w.firstNode().getUniqueId() == 267843779L && w.lastNode().getUniqueId() == 2968718407L)
                 assertEquals(w.getReferrers().size(), 5);
             else if (w.firstNode().getUniqueId() == 2968718407L && w.lastNode().getUniqueId() == 2383688231L)
@@ -79,18 +82,9 @@ public class SplitRoundaboutTest extends AbstractTest {
         });
     }
 
-     @Test
-     public void test2() {
-        Map<Relation, Integer> savedPositions = action.getSavedPositions(r2);
-        List<Node> splitNodes = action.getSplitNodes(r2);
-        assertEquals(4, splitNodes.size());
-        ds.setSelected(splitNodes);
-        swa.actionPerformed(null);
-
-        Collection<Way> splitWays = ds.getSelectedWays();
-        action.updateRelations(savedPositions, splitNodes, splitWays);
-
-        splitWays.forEach(w -> {
+    @Test
+    public void test2() {
+        splitWay(r2).forEach(w -> {
             if(w.firstNode().getUniqueId() == 2158181809L && w.lastNode().getUniqueId() == 2158181798L)
                 assertEquals(w.getReferrers().size(), 8);
             else if (w.firstNode().getUniqueId() == 2158181798L && w.lastNode().getUniqueId() == 2158181789L)
@@ -102,20 +96,11 @@ public class SplitRoundaboutTest extends AbstractTest {
             else
                 fail();
         });
-     }
+    }
 
-     @Test
-     public void test3() {
-         Map<Relation, Integer> savedPositions = action.getSavedPositions(r3);
-        List<Node> splitNodes = action.getSplitNodes(r3);
-        assertEquals(4, splitNodes.size());
-        ds.setSelected(splitNodes);
-        swa.actionPerformed(null);
-
-        Collection<Way> splitWays = ds.getSelectedWays();
-        action.updateRelations(savedPositions, splitNodes, splitWays);
-
-        splitWays.forEach(w -> {
+    @Test
+    public void test3() {
+        splitWay(r3).forEach(w -> {
             if(w.firstNode().getUniqueId() == 280697532L && w.lastNode().getUniqueId() == 280697452L)
                 assertEquals(w.getReferrers().size(), 0);
             else if (w.firstNode().getUniqueId() == 280697452L && w.lastNode().getUniqueId() == 280697591L)
@@ -127,10 +112,5 @@ public class SplitRoundaboutTest extends AbstractTest {
             else
                 fail();
         });
-     }
-
-    @After
-    public void end() {
-        Main.getLayerManager().removeLayer(layer);
     }
 }
