e1fe36dd8e98e861e79fb90c197af4959b722f15
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2017-06-22 20:13:09 +0000

    checkstyle
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@33410 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- pt_assistant/test/unit/org/openstreetmap/josm/plugins/pt_assistant/actions/SplitRoundaboutTest.java
+++ pt_assistant/test/unit/org/openstreetmap/josm/plugins/pt_assistant/actions/SplitRoundaboutTest.java
@@ -1,3 +1,4 @@
+// License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.pt_assistant.actions;
 
 import static org.junit.Assert.assertEquals;
@@ -28,108 +29,108 @@ import org.openstreetmap.josm.plugins.pt_assistant.AbstractTest;
 
 public class SplitRoundaboutTest extends AbstractTest {
 
-	DataSet ds;
-	OsmDataLayer layer;
-	SplitRoundaboutAction action;
-	SplitWayAction swa;
-	Way r1, r2, r3;
+    DataSet ds;
+    OsmDataLayer layer;
+    SplitRoundaboutAction action;
+    SplitWayAction swa;
+    Way r1, r2, r3;
 
-	@Before
-	public void init() throws FileNotFoundException, IllegalDataException {
+    @Before
+    public void init() throws FileNotFoundException, IllegalDataException {
 
         new JOSMFixture("../../JOSM/test/config/unit-josm.home").init(true);
 
-		ds = OsmReader.parseDataSet(new FileInputStream(AbstractTest.PATH_TO_ROUNDABOUT), null);
+        ds = OsmReader.parseDataSet(new FileInputStream(AbstractTest.PATH_TO_ROUNDABOUT), null);
         layer = new OsmDataLayer(ds, OsmDataLayer.createNewName(), null);
         Main.getLayerManager().addLayer(layer);
 
-		Main.pref.put("pt_assistant.roundabout-splitter.alignalways", true);
-		action = new SplitRoundaboutAction();
+        Main.pref.put("pt_assistant.roundabout-splitter.alignalways", true);
+        action = new SplitRoundaboutAction();
         swa = Main.main.menu.splitWay;
         swa.setEnabled(true);
-		r1 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(293302077L, OsmPrimitiveType.WAY));
-		r2 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(205833435L, OsmPrimitiveType.WAY));
-		r3 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(25739002L, OsmPrimitiveType.WAY));
-	}
+        r1 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(293302077L, OsmPrimitiveType.WAY));
+        r2 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(205833435L, OsmPrimitiveType.WAY));
+        r3 = (Way) ds.getPrimitiveById(new SimplePrimitiveId(25739002L, OsmPrimitiveType.WAY));
+    }
 
-	@Test
-	public void test1() {
+    @Test
+    public void test1() {
 
-		Map<Relation, Integer> savedPositions = action.getSavedPositions(r1);
-		List<Node> splitNodes = action.getSplitNodes(r1);
-		assertEquals(4, splitNodes.size());
-		ds.setSelected(splitNodes);
-		swa.actionPerformed(null);
-
-		Collection<Way> splitWays = ds.getSelectedWays();
-		action.updateRelations(savedPositions, splitNodes, splitWays);
-
-		splitWays.forEach(w -> {
-				if (w.firstNode().getUniqueId() == 267843779L && w.lastNode().getUniqueId() == 2968718407L)
-				assertEquals(w.getReferrers().size(), 5);
-			else if (w.firstNode().getUniqueId() == 2968718407L && w.lastNode().getUniqueId() == 2383688231L)
-				assertEquals(w.getReferrers().size(), 0);
-			else if (w.firstNode().getUniqueId() == 2383688231L && w.lastNode().getUniqueId() == 267843741L)
-				assertEquals(w.getReferrers().size(), 5);
-			else if (w.firstNode().getUniqueId() == 267843741L && w.lastNode().getUniqueId() == 267843779L)
-				assertEquals(w.getReferrers().size(), 0);
-			else
-				fail();
-		});
-	}
-
-	 @Test
-	 public void test2() {
-		Map<Relation, Integer> savedPositions = action.getSavedPositions(r2);
-		List<Node> splitNodes = action.getSplitNodes(r2);
-		assertEquals(4, splitNodes.size());
-		ds.setSelected(splitNodes);
+        Map<Relation, Integer> savedPositions = action.getSavedPositions(r1);
+        List<Node> splitNodes = action.getSplitNodes(r1);
+        assertEquals(4, splitNodes.size());
+        ds.setSelected(splitNodes);
         swa.actionPerformed(null);
 
-		Collection<Way> splitWays = ds.getSelectedWays();
-		action.updateRelations(savedPositions, splitNodes, splitWays);
+        Collection<Way> splitWays = ds.getSelectedWays();
+        action.updateRelations(savedPositions, splitNodes, splitWays);
 
-		splitWays.forEach(w -> {
-			if(w.firstNode().getUniqueId() == 2158181809L && w.lastNode().getUniqueId() == 2158181798L)
-				assertEquals(w.getReferrers().size(), 8);
-			else if (w.firstNode().getUniqueId() == 2158181798L && w.lastNode().getUniqueId() == 2158181789L)
-				assertEquals(w.getReferrers().size(), 0);
-			else if (w.firstNode().getUniqueId() == 2158181789L && w.lastNode().getUniqueId() == 2158181803L)
-				assertEquals(w.getReferrers().size(), 8);
-			else if (w.firstNode().getUniqueId() == 2158181803L && w.lastNode().getUniqueId() == 2158181809L)
-				assertEquals(w.getReferrers().size(), 0);
-			else
-				fail();
-		});
-	 }
+        splitWays.forEach(w -> {
+                if (w.firstNode().getUniqueId() == 267843779L && w.lastNode().getUniqueId() == 2968718407L)
+                assertEquals(w.getReferrers().size(), 5);
+            else if (w.firstNode().getUniqueId() == 2968718407L && w.lastNode().getUniqueId() == 2383688231L)
+                assertEquals(w.getReferrers().size(), 0);
+            else if (w.firstNode().getUniqueId() == 2383688231L && w.lastNode().getUniqueId() == 267843741L)
+                assertEquals(w.getReferrers().size(), 5);
+            else if (w.firstNode().getUniqueId() == 267843741L && w.lastNode().getUniqueId() == 267843779L)
+                assertEquals(w.getReferrers().size(), 0);
+            else
+                fail();
+        });
+    }
 
-	 @Test
-	 public void test3() {
-		 Map<Relation, Integer> savedPositions = action.getSavedPositions(r3);
-		List<Node> splitNodes = action.getSplitNodes(r3);
-		assertEquals(4, splitNodes.size());
-		ds.setSelected(splitNodes);
+     @Test
+     public void test2() {
+        Map<Relation, Integer> savedPositions = action.getSavedPositions(r2);
+        List<Node> splitNodes = action.getSplitNodes(r2);
+        assertEquals(4, splitNodes.size());
+        ds.setSelected(splitNodes);
         swa.actionPerformed(null);
 
-		Collection<Way> splitWays = ds.getSelectedWays();
-		action.updateRelations(savedPositions, splitNodes, splitWays);
+        Collection<Way> splitWays = ds.getSelectedWays();
+        action.updateRelations(savedPositions, splitNodes, splitWays);
 
-		splitWays.forEach(w -> {
-			if(w.firstNode().getUniqueId() == 280697532L && w.lastNode().getUniqueId() == 280697452L)
-				assertEquals(w.getReferrers().size(), 0);
-			else if (w.firstNode().getUniqueId() == 280697452L && w.lastNode().getUniqueId() == 280697591L)
-				assertEquals(w.getReferrers().size(), 2);
-			else if (w.firstNode().getUniqueId() == 280697591L && w.lastNode().getUniqueId() == 280697534L)
-				assertEquals(w.getReferrers().size(), 0);
-			else if (w.firstNode().getUniqueId() == 280697534L && w.lastNode().getUniqueId() == 280697532L)
-				assertEquals(w.getReferrers().size(), 1);
-			else
-				fail();
-		});
-	 }
+        splitWays.forEach(w -> {
+            if(w.firstNode().getUniqueId() == 2158181809L && w.lastNode().getUniqueId() == 2158181798L)
+                assertEquals(w.getReferrers().size(), 8);
+            else if (w.firstNode().getUniqueId() == 2158181798L && w.lastNode().getUniqueId() == 2158181789L)
+                assertEquals(w.getReferrers().size(), 0);
+            else if (w.firstNode().getUniqueId() == 2158181789L && w.lastNode().getUniqueId() == 2158181803L)
+                assertEquals(w.getReferrers().size(), 8);
+            else if (w.firstNode().getUniqueId() == 2158181803L && w.lastNode().getUniqueId() == 2158181809L)
+                assertEquals(w.getReferrers().size(), 0);
+            else
+                fail();
+        });
+     }
 
-	@After
-	public void end() {
+     @Test
+     public void test3() {
+         Map<Relation, Integer> savedPositions = action.getSavedPositions(r3);
+        List<Node> splitNodes = action.getSplitNodes(r3);
+        assertEquals(4, splitNodes.size());
+        ds.setSelected(splitNodes);
+        swa.actionPerformed(null);
+
+        Collection<Way> splitWays = ds.getSelectedWays();
+        action.updateRelations(savedPositions, splitNodes, splitWays);
+
+        splitWays.forEach(w -> {
+            if(w.firstNode().getUniqueId() == 280697532L && w.lastNode().getUniqueId() == 280697452L)
+                assertEquals(w.getReferrers().size(), 0);
+            else if (w.firstNode().getUniqueId() == 280697452L && w.lastNode().getUniqueId() == 280697591L)
+                assertEquals(w.getReferrers().size(), 2);
+            else if (w.firstNode().getUniqueId() == 280697591L && w.lastNode().getUniqueId() == 280697534L)
+                assertEquals(w.getReferrers().size(), 0);
+            else if (w.firstNode().getUniqueId() == 280697534L && w.lastNode().getUniqueId() == 280697532L)
+                assertEquals(w.getReferrers().size(), 1);
+            else
+                fail();
+        });
+     }
+
+    @After
+    public void end() {
         Main.getLayerManager().removeLayer(layer);
-	}
+    }
 }
