3ae3a0fb45287df083823526b9261c6b5582d10c
giackserva <giackserva@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2017-07-14 14:27:00 +0000

    [pt_assistant] #josm15020
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@33443 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- pt_assistant/src/org/openstreetmap/josm/plugins/pt_assistant/actions/AddStopPositionAction.java
+++ pt_assistant/src/org/openstreetmap/josm/plugins/pt_assistant/actions/AddStopPositionAction.java
@@ -6,20 +6,33 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.Cursor;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JoinNodeWayAction;
 import org.openstreetmap.josm.actions.SplitWayAction;
+import org.openstreetmap.josm.actions.SplitWayAction.SplitWayResult;
 import org.openstreetmap.josm.actions.mapmode.MapMode;
 import org.openstreetmap.josm.command.AddCommand;
 import org.openstreetmap.josm.command.ChangeCommand;
-import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.command.Command;
+import org.openstreetmap.josm.command.SequenceCommand;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.OsmPrimitiveType;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.data.osm.Way;
 import org.openstreetmap.josm.data.osm.WaySegment;
+import org.openstreetmap.josm.plugins.pt_assistant.data.PTStop;
+import org.openstreetmap.josm.plugins.pt_assistant.utils.RouteUtils;
 import org.openstreetmap.josm.tools.ImageProvider;
 import org.openstreetmap.josm.tools.Shortcut;
 
@@ -32,7 +45,7 @@ import org.openstreetmap.josm.tools.Shortcut;
 @SuppressWarnings("serial")
 public class AddStopPositionAction extends MapMode {
 
-    private static final String mapModeName = "Add stop position";
+    private static final String MAP_MODE_NAME = "Add stop position";
 
     private transient Set<OsmPrimitive> newHighlights = new HashSet<>();
     private transient Set<OsmPrimitive> oldHighlights = new HashSet<>();
@@ -44,9 +57,9 @@ public class AddStopPositionAction extends MapMode {
      * Creates a new AddStopPositionAction
      */
     public AddStopPositionAction() {
-        super(tr(mapModeName), "bus", tr(mapModeName),
+        super(tr(MAP_MODE_NAME), "bus", tr(MAP_MODE_NAME),
                 Shortcut.registerShortcut("mapmode:stop_position",
-                        tr("Mode: {0}", tr(mapModeName)),
+                        tr("Mode: {0}", tr(MAP_MODE_NAME)),
                         KeyEvent.VK_K, Shortcut.CTRL_SHIFT),
                 getCursor());
 
@@ -127,10 +140,10 @@ public class AddStopPositionAction extends MapMode {
             Main.main.undoRedo.add(new AddCommand(newStopPos));
         } else {
             Main.main.undoRedo.add(new ChangeCommand(n, newStopPos));
+            newStopPos = n;
         }
 
-        DataSet ds = Main.getLayerManager().getEditLayer().data;
-        ds.setSelected(newStopPos);
+        Main.getLayerManager().getEditLayer().data.setSelected(newStopPos);
 
         //join the node to the way only if the node is new
         if (newNode) {
@@ -138,9 +151,94 @@ public class AddStopPositionAction extends MapMode {
             joinNodeWayAction.actionPerformed(null);
         }
 
-        // split the way in any case
-        SplitWayAction splitWayAction = new SplitWayAction();
-        splitWayAction.actionPerformed(null);
+        if (newStopPos.getParentWays().isEmpty())
+            return;
+
+        Way affected = newStopPos.getParentWays().get(0);
+        Map<Relation, Boolean> needPostProcess = getAffectedRelation(affected);
+
+        if (needPostProcess.isEmpty())
+            return;
+
+        SplitWayResult result = SplitWayAction.split(getLayerManager().getEditLayer(),
+                affected, Collections.singletonList(newStopPos), Collections.emptyList());
+        Main.main.undoRedo.add(result.getCommand());
+
+        List<Command> cmds = new ArrayList<>();
+        for (Entry<Relation, Boolean> route : needPostProcess.entrySet()) {
+            Relation r = new Relation(route.getKey());
+            if (route.getValue())
+                deleteFirstWay(r);
+            else
+                deleteLastWay(r);
+            cmds.add(new ChangeCommand(route.getKey(), r));
+        }
+        Main.main.undoRedo.add(new SequenceCommand("Update PT Relations", cmds));
+    }
+
+    private void deleteLastWay(Relation r) {
+        int delete = 0;
+        for (int i = r.getMembersCount() - 1; i >= 0; i--) {
+            RelationMember rm = r.getMember(i);
+            if (rm.getType() == OsmPrimitiveType.WAY &&
+                    !PTStop.isPTPlatform(rm)) {
+                delete = i;
+                break;
+            }
+        }
+        r.removeMember(delete);
+    }
+
+    private void deleteFirstWay(Relation r) {
+        int delete = 0;
+        for (int i = 0; i < r.getMembersCount(); i++) {
+            RelationMember rm = r.getMember(i);
+            if (rm.getType() == OsmPrimitiveType.WAY &&
+                    !PTStop.isPTPlatform(rm)) {
+                delete = i;
+                break;
+            }
+        }
+        r.removeMember(delete);
+    }
+
+    private Map<Relation, Boolean> getAffectedRelation(Way affected) {
+        Map<Relation, Boolean> ret = new HashMap<>();
+        for (Relation route : getPTRouteParents(affected)) {
+            if (isFirstMember(affected, route)) {
+                ret.put(route, true);
+            } else if (isLastMember(affected, route)) {
+                ret.put(route, false);
+            }
+        }
+        return ret;
+    }
+
+    private boolean isFirstMember(Way affected, Relation route) {
+        for (int i = 0; i < route.getMembersCount(); i++) {
+            RelationMember rm = route.getMember(i);
+            if (rm.getMember().equals(affected)) {
+                return true;
+            } else if (rm.getType() == OsmPrimitiveType.WAY &&
+                    !PTStop.isPTPlatform(rm)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isLastMember(Way affected, Relation route) {
+        for (int i = route.getMembersCount() - 1; i >= 0; i--) {
+            RelationMember rm = route.getMember(i);
+            if (rm.getMember().equals(affected)) {
+                return true;
+            } else if (rm.getType() == OsmPrimitiveType.WAY &&
+                    !PTStop.isPTPlatform(rm)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private void clearNodeTags(Node newStopPos) {
@@ -148,6 +246,13 @@ public class AddStopPositionAction extends MapMode {
             newStopPos.put(key, null);
         }
 
+    }
+
+    private List<Relation> getPTRouteParents(Way way) {
+        List<Relation> referrers = OsmPrimitive.getFilteredList(
+                way.getReferrers(), Relation.class);
+        referrers.removeIf(r -> !RouteUtils.isPTRoute(r));
+        return referrers;
     }
 
     //turn off what has been highlighted on last mouse move and highlight what has to be highlighted now
