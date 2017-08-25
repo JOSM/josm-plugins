aac809cacfc03e13a0c4f235493d0023c21e6707
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2017-08-25 23:59:33 +0000

    fix deprecation warnings
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@33525 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- OSMRecPlugin/src/org/openstreetmap/josm/plugins/osmrec/OSMRecPlugin.java
+++ OSMRecPlugin/src/org/openstreetmap/josm/plugins/osmrec/OSMRecPlugin.java
@@ -1,7 +1,7 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.osmrec;
 
-import org.openstreetmap.josm.Main;
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
@@ -12,42 +12,42 @@ import org.openstreetmap.josm.plugins.PluginInformation;
  */
 public class OSMRecPlugin extends Plugin {
 
-    private final MenuExportAction menuExportAction;
-    private static MapFrame mapFrame;
-    public OSMRecPlugin plugin;
+	private final MenuExportAction menuExportAction;
+	private static MapFrame mapFrame;
+	public OSMRecPlugin plugin;
 
-    public OSMRecPlugin(PluginInformation info) { // NO_UCD (unused code)
-        super(info);
-        menuExportAction = new MenuExportAction();
-        Main.main.menu.toolsMenu.add(menuExportAction);
-    }
+	public OSMRecPlugin(PluginInformation info) { // NO_UCD (unused code)
+		super(info);
+		menuExportAction = new MenuExportAction();
+		MainApplication.getMenu().toolsMenu.add(menuExportAction);
+	}
 
-    @Override
-    public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
-        if (oldFrame == null && newFrame != null) { // map frame added
-            setCurrentMapFrame(newFrame);
-            setState(this);
-        }
-    }
+	@Override
+	public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
+		if (oldFrame == null && newFrame != null) { // map frame added
+			setCurrentMapFrame(newFrame);
+			setState(this);
+		}
+	}
 
-    private void setCurrentMapFrame(MapFrame newFrame) {
-        OSMRecPlugin.mapFrame = newFrame;
-    }
+	private void setCurrentMapFrame(MapFrame newFrame) {
+		OSMRecPlugin.mapFrame = newFrame;
+	}
 
-    public static MapFrame getCurrentMapFrame() {
-        return mapFrame;
-    }
+	public static MapFrame getCurrentMapFrame() {
+		return mapFrame;
+	}
 
-    private void setState(OSMRecPlugin plugin) {
-        this.plugin = plugin;
-    }
+	private void setState(OSMRecPlugin plugin) {
+		this.plugin = plugin;
+	}
 
-    public OSMRecPlugin getState() {
-        return plugin;
-    }
+	public OSMRecPlugin getState() {
+		return plugin;
+	}
 
-    //    @Override
-    //    public PreferenceSetting getPreferenceSetting() {
-    //        return new PreferenceEditor();
-    //    }
+	//    @Override
+	//    public PreferenceSetting getPreferenceSetting() {
+	//        return new PreferenceEditor();
+	//    }
 }
