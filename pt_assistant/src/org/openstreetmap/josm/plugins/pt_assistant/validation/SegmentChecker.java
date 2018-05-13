922158646535710096dce57af9771f3e0ff6d321
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2018-05-13 23:28:20 +0000

    update to JOSM 13729
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34193 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- pt_assistant/src/org/openstreetmap/josm/plugins/pt_assistant/validation/SegmentChecker.java
+++ pt_assistant/src/org/openstreetmap/josm/plugins/pt_assistant/validation/SegmentChecker.java
@@ -941,7 +941,7 @@ public class SegmentChecker extends Checker {
         modifiedRelation.setMembers(getModifiedRelationMembers(testError, fix));
         ChangeCommand changeCommand = new ChangeCommand(originalRelation, modifiedRelation);
         MainApplication.undoRedo.addNoRedraw(changeCommand);
-        MainApplication.undoRedo.afterAdd();
+        MainApplication.undoRedo.afterAdd(changeCommand);
         PTRouteSegment wrongSegment = wrongSegments.get(testError);
         wrongSegments.remove(testError);
         wrongSegment.setPTWays(fix);
@@ -1076,7 +1076,7 @@ public class SegmentChecker extends Checker {
                 modifiedRelation.setMembers(getModifiedRelationMembers(testError, segment.getPTWays()));
                 ChangeCommand changeCommand = new ChangeCommand(originalRelation, modifiedRelation);
                 MainApplication.undoRedo.addNoRedraw(changeCommand);
-                MainApplication.undoRedo.afterAdd();
+                MainApplication.undoRedo.afterAdd(changeCommand);
                 wrongSegmentsToRemove.add(testError);
             }
         }
