9bd9e91c7682edced7f721ae6e0dbe542f160e30
renerr18 <renerr18@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2018-07-12 03:03:38 +0000

    Changed image map from HashMap to ConcurrentHashmap in CubemapBuilder.
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34418 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- MicrosoftStreetside/src/org/openstreetmap/josm/plugins/streetside/cubemap/CubemapBuilder.java
+++ MicrosoftStreetside/src/org/openstreetmap/josm/plugins/streetside/cubemap/CubemapBuilder.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -33,7 +34,7 @@ public class CubemapBuilder implements ITileDownloadingTaskListener, StreetsideD
 	private StreetsideCubemap cubemap;
 	protected boolean cancelled;
 	private long startTime;
-  private Map<String, BufferedImage> tileImages = new HashMap<>();
+  private Map<String, BufferedImage> tileImages = new ConcurrentHashMap();//new HashMap<>();
 
   /**
    * @return the tileImages
