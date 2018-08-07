e70dccc49cd76d9f2447dbd84c71cc140aaeab31
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2018-08-07 18:15:15 +0000

    see #josm2452, see #josm16578 - drop `videomapping` plugin: - never finished - nobody interested in maintaining it - buggy - requires native libraries / VLC - if video mapping is really wanted, the plugin should be started again using JavaFX
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34442 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- videomapping/src/org/openstreetmap/josm/plugins/videomapping/video/VideoPlayerObserver.java
+++
@@ -1,8 +0,0 @@
-// License: GPL. For details, see LICENSE file.
-package org.openstreetmap.josm.plugins.videomapping.video;
-
-/** Update mechanism for GPS position, title bar,...
- */
-public interface VideoPlayerObserver {
-    void update_plays();
-}
--- videomapping/src/org/openstreetmap/josm/plugins/videomapping/video/VideosObserver.java
+++
@@ -1,8 +0,0 @@
-// License: GPL. For details, see LICENSE file.
-package org.openstreetmap.josm.plugins.videomapping.video;
-
-/** Getting basic control informations per video
- */
-public interface VideosObserver {
-    void update(VideoObserversEvents event);
-}
