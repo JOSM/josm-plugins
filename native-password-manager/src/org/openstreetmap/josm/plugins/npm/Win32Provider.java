8965718510e545af56fe71c0ce4216dbd86bd0c8
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2018-08-10 16:52:36 +0000

    fix #josm16588 - drop JAXB
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34447 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- native-password-manager/src/org/openstreetmap/josm/plugins/npm/Win32Provider.java
+++ native-password-manager/src/org/openstreetmap/josm/plugins/npm/Win32Provider.java
@@ -1,8 +1,6 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.npm;
 
-import javax.xml.bind.DatatypeConverter;
-
 import org.netbeans.modules.keyring.fallback.FallbackProvider;
 import org.netbeans.modules.keyring.win32.Win32Protect;
 import org.openstreetmap.josm.Main;
@@ -13,11 +11,11 @@ public class Win32Provider extends FallbackProvider {
     
         @Override public byte[] getByteArray(String key, byte[] def) {
             String p = Main.pref.get(key, null);
-            return p == null ? def : DatatypeConverter.parseBase64Binary(p);
+            return p == null ? def : DatatypeConverter._parseBase64Binary(p);
         }
 
         @Override public void putByteArray(String key, byte[] val) {
-            Main.pref.put(key, val == null ? null : DatatypeConverter.printBase64Binary(val));
+            Main.pref.put(key, val == null ? null : DatatypeConverter._printBase64Binary(val));
         }
 
         @Override public void remove(String key) {
