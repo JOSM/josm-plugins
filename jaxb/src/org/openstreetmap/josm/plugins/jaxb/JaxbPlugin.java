e9904afa5c599fd5e4e8f499941e1e4d658fe0d9
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2018-08-13 23:53:42 +0000

    add JAXB as a plugin
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34460 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ jaxb/src/org/openstreetmap/josm/plugins/jaxb/JaxbPlugin.java
@@ -0,0 +1,19 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.jaxb;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * JAXB plugin brings the JAXB API to other plugins.
+ */
+public class JaxbPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code JaxbPlugin}.
+     * @param info plugin info
+     */
+    public JaxbPlugin(PluginInformation info) {
+        super(info);
+    }
+}
