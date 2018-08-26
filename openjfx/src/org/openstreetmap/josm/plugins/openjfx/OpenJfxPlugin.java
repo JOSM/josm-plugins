a765bfef0a808fa12433a8573a86723cfc8ca2bf
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2018-08-26 21:16:27 +0000

    see #16047, see #16682 - add new plugin for openjfx/javafx
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34602 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ openjfx/src/org/openstreetmap/josm/plugins/openjfx/OpenJfxPlugin.java
@@ -0,0 +1,19 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.openjfx;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * JAXB plugin brings OpenJFX (JavaFX) to other plugins.
+ */
+public class OpenJfxPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code OpenJfxPlugin}.
+     * @param info plugin info
+     */
+    public OpenJfxPlugin(PluginInformation info) {
+        super(info);
+    }
+}
