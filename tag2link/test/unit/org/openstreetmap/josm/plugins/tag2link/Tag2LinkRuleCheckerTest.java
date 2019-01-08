56b0a3392c20ef1e9f6b27c4a5e3ffffbdaaa7e8
simon04 <simon04@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2019-01-08 21:37:06 +0000

    fix #josm16922 - tag2link: add support for *:wikidata
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34811 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- tag2link/test/unit/org/openstreetmap/josm/plugins/tag2link/Tag2LinkRuleCheckerTest.java
+++ tag2link/test/unit/org/openstreetmap/josm/plugins/tag2link/Tag2LinkRuleCheckerTest.java
@@ -12,14 +12,21 @@ import org.openstreetmap.josm.plugins.tag2link.data.Link;
 public class Tag2LinkRuleCheckerTest {
 
     @BeforeClass
-    public static void load() throws Exception {
+    public static void load() {
         Tag2LinkRuleChecker.init();
     }
 
     @Test
-    public void testImageCommons() throws Exception {
+    public void testImageCommons() {
         final Collection<Link> links = Tag2LinkRuleChecker.getLinks(new Tag("image", "File:Witten Brücke Gasstraße.jpg"));
         assertEquals(1, links.size());
         assertEquals("https://commons.wikimedia.org/wiki/File%3AWitten_Br%C3%BCcke_Gasstra%C3%9Fe.jpg", links.iterator().next().url);
+    }
+
+    @Test
+    public void testBrandWikidata() {
+        final Collection<Link> links = Tag2LinkRuleChecker.getLinks(new Tag("brand:wikidata", "Q259340"));
+        assertEquals(1, links.size());
+        assertEquals("https://www.wikidata.org/wiki/Q259340", links.iterator().next().url);
     }
 }
