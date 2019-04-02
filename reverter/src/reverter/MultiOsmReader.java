c955507b79f232730c5505761371df581e1cb1b8
gerdp <gerdp@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2019-04-02 06:43:52 +0000

    fix some sonarLint issues
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34960 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- reverter/src/reverter/MultiOsmReader.java
+++ reverter/src/reverter/MultiOsmReader.java
@@ -4,13 +4,10 @@ package reverter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-
 import org.openstreetmap.josm.io.IllegalDataException;
 import org.openstreetmap.josm.io.OsmReader;
 import org.openstreetmap.josm.io.UTFInputStreamReader;
+import org.openstreetmap.josm.tools.XmlUtils;
 
 /**
  * Subclass of {@link org.openstreetmap.josm.io.OsmReader} that can handle multiple XML streams.
@@ -18,14 +15,15 @@ import org.openstreetmap.josm.io.UTFInputStreamReader;
  */
 public class MultiOsmReader extends OsmReader {
 
+    /**
+     * Parse data in source and add to existing dataset.
+     * @param source the input stream with OSM data
+     * @throws IllegalDataException in case of any exception
+     */
     public void addData(InputStream source) throws IllegalDataException {
-        try {
-            InputStreamReader ir = UTFInputStreamReader.create(source, "UTF-8");
-            XMLStreamReader parser = XMLInputFactory.newInstance().createXMLStreamReader(ir);
-            setParser(parser);
+        try (InputStreamReader ir = UTFInputStreamReader.create(source)) {
+            setParser(XmlUtils.newSafeXMLInputFactory().createXMLStreamReader(ir));
             parse();
-        } catch (XMLStreamException e) {
-           throw new IllegalDataException(e);
         } catch (Exception e) {
             throw new IllegalDataException(e);
         }
