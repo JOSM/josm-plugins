fd497f5aaa39029ec0fc2875939170ff9f80b5fd
gerdp <gerdp@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2019-04-07 05:14:21 +0000

    fix #17568: newline missing in exported *.poly file when only a single ring is written Further changes - use PrinterWriter - add Unit test for export of simple polygon
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@34966 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- poly/src/poly/PolyExporter.java
+++ poly/src/poly/PolyExporter.java
@@ -3,9 +3,9 @@ package poly;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.HashSet;
@@ -42,7 +42,7 @@ public class PolyExporter extends OsmExporter {
             if (((OsmDataLayer) layer).getDataSet().getWays().stream().anyMatch(w -> !w.isClosed())) {
                 throw new IOException(tr("Data contains unclosed ways."));
             }
-            try (BufferedWriter writer = Files.newBufferedWriter(file.toPath(), StandardCharsets.UTF_8)) {
+            try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(file.toPath(), StandardCharsets.UTF_8))) {
                 DataSet ds = ((OsmDataLayer) layer).getDataSet();
                 HashSet<Way> written = new HashSet<>();
                 boolean firstFile = true;
@@ -55,7 +55,7 @@ public class PolyExporter extends OsmExporter {
                 for (Relation rel : ds.getRelations()) {
                     if (rel.isMultipolygon()) {
                         if (!firstFile) {
-                            writer.newLine();
+                            writer.println();
                         }
                         writeRelation(writer, fileName, rel, written);
                         firstFile = false;
@@ -63,7 +63,7 @@ public class PolyExporter extends OsmExporter {
                 }
                 
                 if (firstFile) {
-                    writer.write(fileName);
+                    writer.println(fileName);
                 }
                 int counter = 1;
                 for (Way w : ds.getWays()) {
@@ -71,18 +71,16 @@ public class PolyExporter extends OsmExporter {
                         writeWay(writer, w, counter);
                     }
                 }
-                writer.write("END");
-                writer.newLine();
+                writer.println("END");
             }
         }
     }
 
-    private static void writeRelation(BufferedWriter writer, String fileName, Relation rel, Set<Way> written) throws IOException {
+    private static void writeRelation(PrintWriter writer, String fileName, Relation rel, Set<Way> written) {
         String polygonName = fileName;
         if (rel.getName() != null)
             polygonName = rel.getName();
-        writer.write(polygonName);
-        writer.newLine();
+        writer.println(polygonName);
         int counter = 1;
         for (RelationMember rm: rel.getMembers()) {
             if (rm.isWay()) {
@@ -95,20 +93,17 @@ public class PolyExporter extends OsmExporter {
         }
     }
 
-    private static int writeWay(BufferedWriter writer, Way w, int counter) throws IOException {
+    private static int writeWay(PrintWriter writer, Way w, int counter) {
         String name = w.getName();
         if (name == null) {
             name = String.valueOf(counter++);
         } 
-        writer.write(name);
-        writer.newLine();
+        writer.println(name);
 
         for (Node n : w.getNodes()) {
-            writer.write(String.format(Locale.ENGLISH, "   %f   %f", n.getCoor().lon(), n.getCoor().lat()));
-            writer.newLine();
+            writer.println(String.format(Locale.ENGLISH, "   %f   %f", n.getCoor().lon(), n.getCoor().lat()));
         }
-        writer.write("END");
-        writer.newLine();
+        writer.println("END");
         return counter;
     }
 }
--- poly/test/unit/poly/PolyExporterTest.java
+++ poly/test/unit/poly/PolyExporterTest.java
@@ -31,6 +31,29 @@ public class PolyExporterTest {
      * @throws Exception if an error occurs
      */
     @Test
+    public void testSimpleExport() throws Exception {
+        DataSet dsIn1 = new PolyImporter().parseDataSet(TestUtils.getTestDataRoot() + "/simple.poly");
+        assertNotNull(dsIn1);
+        assertEquals(4, dsIn1.getNodes().size());
+        assertEquals(1, dsIn1.getWays().size());
+        assertEquals(0, dsIn1.getRelations().size());
+
+        Path out = Files.createTempFile("simple-out", "poly");
+        new PolyExporter().exportData(out.toFile(), new OsmDataLayer(dsIn1, null, null));
+        DataSet dsIn2 = new PolyImporter().parseDataSet(out.toString());
+        assertNotNull(dsIn2);
+        assertEquals(4, dsIn2.getNodes().size());
+        assertEquals(1, dsIn2.getWays().size());
+        assertEquals(0, dsIn2.getRelations().size());
+        
+        Files.delete(out);
+    }
+
+    /**
+     * Import file, export it, import the exported file and compare content
+     * @throws Exception if an error occurs
+     */
+    @Test
     public void testExport() throws Exception {
         DataSet dsIn1 = new PolyImporter().parseDataSet(TestUtils.getTestDataRoot() + "/holes.poly");
         assertNotNull(dsIn1);
