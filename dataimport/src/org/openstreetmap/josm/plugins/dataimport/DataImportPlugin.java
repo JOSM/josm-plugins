c14ba7bf028a6a6af5c3a226a54ae804f0ff2e82
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2019-05-27 23:20:32 +0000

    fix #josm17152 - fix JAXB initialization
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@35012 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- dataimport/src/org/openstreetmap/josm/plugins/dataimport/DataImportPlugin.java
+++ dataimport/src/org/openstreetmap/josm/plugins/dataimport/DataImportPlugin.java
@@ -3,6 +3,8 @@ package org.openstreetmap.josm.plugins.dataimport;
 
 import java.io.IOException;
 
+import javax.xml.bind.JAXBException;
+
 import org.openstreetmap.josm.actions.ExtensionFileFilter;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
@@ -18,8 +20,9 @@ public class DataImportPlugin extends Plugin {
      * Add new File import filter into open dialog
      * @param info plugin information
      * @throws IOException in case of I/O error
+     * @throws JAXBException if JAXB cannot be initialized
      */
-    public DataImportPlugin(PluginInformation info) throws IOException {
+    public DataImportPlugin(PluginInformation info) throws IOException, JAXBException {
         super(info);
 
         ExtensionFileFilter.addImporter(new TangoGPS());
