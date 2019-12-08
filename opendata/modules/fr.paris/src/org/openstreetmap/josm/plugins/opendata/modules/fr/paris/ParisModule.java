07a2d396360ce9accf49f22cc0ae53998fec58d5
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2019-12-08 15:04:09 +0000

    see #josm18385 - cleanup Paris module
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@35244 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- opendata/modules/fr.paris/src/org/openstreetmap/josm/plugins/opendata/modules/fr/paris/ParisModule.java
+++ opendata/modules/fr.paris/src/org/openstreetmap/josm/plugins/opendata/modules/fr/paris/ParisModule.java
@@ -6,17 +6,12 @@ import org.openstreetmap.josm.plugins.opendata.core.modules.ModuleInformation;
 import org.openstreetmap.josm.plugins.opendata.modules.fr.paris.datasets.environnement.ArbresRemarquablesHandler;
 import org.openstreetmap.josm.plugins.opendata.modules.fr.paris.datasets.environnement.MobilierVoiePubliqueHandler;
 import org.openstreetmap.josm.plugins.opendata.modules.fr.paris.datasets.urbanisme.EclairagePublicHandler;
-import org.openstreetmap.josm.plugins.opendata.modules.fr.paris.datasets.urbanisme.SanisettesHandler;
 
 public class ParisModule extends AbstractModule {
 
     public ParisModule(ModuleInformation info) {
         super(info);
-        handlers.add(SanisettesHandler.class);
         handlers.add(ArbresRemarquablesHandler.class);
-        //handlers.add(VolumesBatisHandler.class); // Disabled as the projection cannot be transformed
-        //handlers.add(VolumesNonBatisHandler.class); // Disabled as the projection cannot be transformed
-        //handlers.add(ElectriciteHandler.class); // Disabled (useless for OSM)
         handlers.add(EclairagePublicHandler.class);
         handlers.add(MobilierVoiePubliqueHandler.class);
     }
