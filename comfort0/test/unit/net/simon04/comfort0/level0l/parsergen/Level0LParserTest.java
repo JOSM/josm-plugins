ea7acfea65f174979c58cbb5c1d119879e551598
simon04 <simon04@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2020-01-13 20:45:14 +0000

    JOSM/comfort0: parse tags
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@35284 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- comfort0/test/unit/net/simon04/comfort0/level0l/parsergen/Level0LParserTest.java
+++ comfort0/test/unit/net/simon04/comfort0/level0l/parsergen/Level0LParserTest.java
@@ -1,7 +1,7 @@
 package net.simon04.comfort0.level0l.parsergen;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import java.io.StringReader;
 
@@ -11,10 +11,16 @@ import org.openstreetmap.josm.data.osm.NodeData;
 public class Level0LParserTest {
     @Test
     public void testNode() throws Exception {
-        final NodeData node = new Level0LParser(new StringReader("node 298884272: 54.0901447, 12.2516513\n")).node();
+        final String level0l = "" +
+                "node 298884272: 54.0901447, 12.2516513\n" +
+                "  name = Neu Broderstorf\n" +
+                "  traffic_sign = city_limit\n";
+        final NodeData node = new Level0LParser(new StringReader(level0l)).node();
         assertThat(node.getId(), is(298884272L));
         assertThat(node.getCoor().lat(), is(54.0901447));
         assertThat(node.getCoor().lon(), is(12.2516513));
-
+        assertThat(node.getKeys().size(), is(2));
+        assertThat(node.getKeys().get("name"), is("Neu Broderstorf"));
+        assertThat(node.getKeys().get("traffic_sign"), is("city_limit"));
     }
 }
