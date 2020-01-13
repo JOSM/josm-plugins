fa16992ab2739898c18c12403141640648af2992
simon04 <simon04@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2020-01-13 20:45:18 +0000

    JOSM/comfort0: parse ways
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@35285 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- comfort0/test/unit/net/simon04/comfort0/level0l/parsergen/Level0LParserTest.java
+++ comfort0/test/unit/net/simon04/comfort0/level0l/parsergen/Level0LParserTest.java
@@ -7,6 +7,7 @@ import java.io.StringReader;
 
 import org.junit.Test;
 import org.openstreetmap.josm.data.osm.NodeData;
+import org.openstreetmap.josm.data.osm.WayData;
 
 public class Level0LParserTest {
     @Test
@@ -22,5 +23,25 @@ public class Level0LParserTest {
         assertThat(node.getKeys().size(), is(2));
         assertThat(node.getKeys().get("name"), is("Neu Broderstorf"));
         assertThat(node.getKeys().get("traffic_sign"), is("city_limit"));
+    }
+
+    @Test
+    public void testWay() throws Exception {
+        final String level0l = "" +
+                "way 26659127\n" +
+                "  nd 292403538\n" +
+                "  nd 298884289\n" +
+                "  nd 261728686\n" +
+                "  highway = unclassified\n" +
+                "  name = Pastower Straße\n";
+        final WayData way = new Level0LParser(new StringReader(level0l)).way();
+        assertThat(way.getId(), is(26659127L));
+        assertThat(way.getNodesCount(), is(3));
+        assertThat(way.getNodeId(0), is(292403538L));
+        assertThat(way.getNodeId(1), is(298884289L));
+        assertThat(way.getNodeId(2), is(261728686L));
+        assertThat(way.getKeys().size(), is(2));
+        assertThat(way.getKeys().get("highway"), is("unclassified"));
+        assertThat(way.getKeys().get("name"), is("Pastower Straße"));
     }
 }
