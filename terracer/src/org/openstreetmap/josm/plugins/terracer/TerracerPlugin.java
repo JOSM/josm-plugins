cd2bb1174d8e0f69b7e9fec48c7d2070283e3663
donvip <donvip@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2020-02-15 17:05:24 +0000

    fix #18713 - make terracer plugin restartless (patch by taylor.smock)
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@35324 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- terracer/src/org/openstreetmap/josm/plugins/terracer/TerracerPlugin.java
+++ terracer/src/org/openstreetmap/josm/plugins/terracer/TerracerPlugin.java
@@ -1,21 +1,51 @@
 // License: GPL. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.terracer;
 
+import java.awt.Component;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import javax.swing.Action;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import org.openstreetmap.josm.actions.JosmAction;
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.MainMenu;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.tools.Destroyable;
 
 /**
  * Plugin interface implementation for Terracer.
  *
  * @author zere - Copyright 2009 CloudMade Ltd
  */
-public class TerracerPlugin extends Plugin {
+public class TerracerPlugin extends Plugin implements Destroyable {
+	private List<JosmAction> actions = Arrays.asList(new TerracerAction(), new ReverseTerraceAction());
+
     public TerracerPlugin(PluginInformation info) {
         super(info);
-
-        MainMenu.add(MainApplication.getMenu().moreToolsMenu, new TerracerAction());
-        MainMenu.add(MainApplication.getMenu().moreToolsMenu, new ReverseTerraceAction());
+        for (JosmAction action : actions) {
+        	MainMenu.add(MainApplication.getMenu().moreToolsMenu, action);
+        }
     }
+
+	@Override
+	public void destroy() {
+        final JMenu moreToolsMenu = MainApplication.getMenu().moreToolsMenu;
+        final Map<Action, Component> actionsMap = Arrays.asList(moreToolsMenu.getMenuComponents()).stream()
+                .filter(JMenuItem.class::isInstance).map(JMenuItem.class::cast)
+                .collect(Collectors.toMap(JMenuItem::getAction, component -> component));
+
+        for (final Entry<Action, Component> action : actionsMap.entrySet()) {
+            if (actionsMap.containsKey(action.getKey())) {
+                moreToolsMenu.remove(action.getValue());
+            }
+        }
+		actions.forEach(JosmAction::destroy);
+	}
 }
