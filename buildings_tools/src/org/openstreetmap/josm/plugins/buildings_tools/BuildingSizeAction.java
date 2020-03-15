ae947013b42984c6f27bc8e76140a01d8f6cbb19
gerdp <gerdp@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2020-03-15 15:27:57 +0000

    fix #josm18932: use different toolbarId for BuildingSizeAction
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@35364 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- buildings_tools/src/org/openstreetmap/josm/plugins/buildings_tools/BuildingSizeAction.java
+++ buildings_tools/src/org/openstreetmap/josm/plugins/buildings_tools/BuildingSizeAction.java
@@ -15,7 +15,7 @@ public class BuildingSizeAction extends JosmAction {
         super(tr("Set buildings size"), "mapmode/building", tr("Set buildings size"),
                 Shortcut.registerShortcut("edit:buildingsdialog", tr("Edit: {0}", tr("Set buildings size")),
                 KeyEvent.VK_B, Shortcut.ALT_CTRL),
-                true);
+                true, "edit:buildingsdialog", true);
     }
 
     @Override
