b849b864df85aa2d7e43655c9f52d5a5e9dfc445
GerdP <GerdP@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2021-01-16 10:27:46 +0000

    fix #20395: ConcurrentModificationException: Revert can't revert 833386 - We must execute commands with `GuiHelper.runInEDT()`. Regression introduced for #15906
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@35688 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- reverter/src/reverter/RevertChangesetTask.java
+++ reverter/src/reverter/RevertChangesetTask.java
@@ -171,12 +171,14 @@ public class RevertChangesetTask extends PleaseWaitRunnable {
             Logging.warn(MessageFormat.format("No revert commands found for changeset {0}", Long.toString(changesetId)));
             return null;
         }
-        for (Command c : cmds) {
-            if (c instanceof ConflictAddCommand) {
-                numberOfConflicts++;
-            }
-            c.executeCommand();
-        }
+        GuiHelper.runInEDT(() -> {
+        	for (Command c : cmds) {
+        		if (c instanceof ConflictAddCommand) {
+        			numberOfConflicts++;
+        		}
+        		c.executeCommand();
+        	}
+        });
         final String desc;
         if (revertType == RevertType.FULL) {
             desc = tr("Revert changeset {0}", String.valueOf(changesetId));
