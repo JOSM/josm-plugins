39cd641ee540019bf7ac7eedc88c310a768a076c
taylor.smock <taylor.smock@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2022-04-18 21:24:43 +0000

    geotools: Fix #21995 by removing Jackson and putting Jackson in its own plugin
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@35958 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ jackson/README.md
@@ -0,0 +1,9 @@
+README 
+======
+
+# License
+GPLv2 or later
+
+# Author
+Taylor Smock (taylor.smock)
+
---
+++ jackson/src/org/openstreetmap/josm/plugins/jackson/JacksonPlugin.java
@@ -0,0 +1,19 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.jackson;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * Jackson plugin, bringing this library to other JOSM plugins.
+ */
+public class JacksonPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code JacksonPlugin}.
+     * @param info plugin information
+     */
+    public JacksonPlugin(PluginInformation info) {
+        super(info);
+    }
+}
