99bfc0bc1c6046a43a6bb3205ad733e1a11365ed
taylor.smock <taylor.smock@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2023-07-20 14:05:19 +0000

    Move gson back to plugins (required for sonar CI job) and mark TODO as a Java 17 plugin
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@36100 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ gson/src/org/openstreetmap/josm/plugins/gson/GsonPlugin.java
@@ -0,0 +1,19 @@
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.gson;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+/**
+ * GSON plugin, bringing this library to other JOSM plugins.
+ */
+public class GsonPlugin extends Plugin {
+
+    /**
+     * Constructs a new {@code GsonPlugin}.
+     * @param info plugin information
+     */
+    public GsonPlugin(PluginInformation info) {
+        super(info);
+    }
+}
