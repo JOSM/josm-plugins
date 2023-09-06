7a002cec90e053bd340ad8acfa435fc8e9c240ae
taylor.smock <taylor.smock@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2023-09-06 20:40:20 +0000

    Update FlatLaf to 3.2 from 2.6
    * New macOS-style themes
    * Set `apple.awt.application.appearance` on macOS systems so title bar matches
      system settings
    
    git-svn-id: https://josm.openstreetmap.de/osmsvn/applications/editors/josm/plugins@36128 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- flatlaf/src/org/openstreetmap/josm/plugins/flatlaf/FlatLafPlugin.java
+++ flatlaf/src/org/openstreetmap/josm/plugins/flatlaf/FlatLafPlugin.java
@@ -5,12 +5,16 @@ import javax.swing.UIManager;
 
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
+import org.openstreetmap.josm.tools.PlatformManager;
+import org.openstreetmap.josm.tools.Utils;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
 /**
  * FlatLaf for JOSM
@@ -25,13 +29,21 @@ public class FlatLafPlugin extends Plugin {
     public FlatLafPlugin(PluginInformation info) {
         super(info);
         UIManager.getDefaults().put("ClassLoader", getClass().getClassLoader());
-        UIManager.installLookAndFeel("FlatLaf Darcula", FlatDarculaLaf.class.getName());
-        UIManager.installLookAndFeel("FlatLaf Dark", FlatDarkLaf.class.getName());
-        UIManager.installLookAndFeel("FlatLaf IntelliJ", FlatIntelliJLaf.class.getName());
-        UIManager.installLookAndFeel("FlatLaf Light", FlatLightLaf.class.getName());
+        // Load the built-in themes
+        FlatDarculaLaf.installLafInfo();
+        FlatDarkLaf.installLafInfo();
+        FlatIntelliJLaf.installLafInfo();
+        FlatLightLaf.installLafInfo();
+        FlatMacDarkLaf.installLafInfo();
+        FlatMacLightLaf.installLafInfo();
 
         // enable loading of FlatLaf.properties, FlatLightLaf.properties and FlatDarkLaf.properties from package
         FlatLaf.registerCustomDefaultsSource("org.openstreetmap.josm.plugins.flatlaf", getClass().getClassLoader());
+        if (PlatformManager.isPlatformOsx() && Utils.getSystemProperty("apple.awt.application.appearance") == null) {
+            // See https://www.formdev.com/flatlaf/macos/
+            // This makes the title bar match the system settings
+            Utils.updateSystemProperty("apple.awt.application.appearance", "system");
+        }
     }
 
 }
